395e5ebaed07b2631dd049a2ba8453339dbf9479
cv <34649812+cpvalente@users.noreply.github.com>
2021-03-25 14:43:34 +0100

    preview in iframe

--- src/features/viewers/PreviewContainer.jsx
+++ src/features/viewers/PreviewContainer.jsx
@@ -1,12 +1,16 @@
 import DefaultPresenter from './DefaultPresenter';
 import styles from './PreviewContainer.module.css';
+import { AspectRatio } from '@chakra-ui/react';
 
-export default function PreviewContainer(props) {
+export default function PreviewContainer() {
   return (
     <div className={styles.previewContainer}>
       <div className={styles.previewItem}>
         <div className={styles.preview}>
-          <DefaultPresenter data={props.data} preview/>
+          <AspectRatio maxW='250' ratio={16/10}>
+            <iframe src='http://localhost:3000/'></iframe>
+          </AspectRatio>
+          {/* <DefaultPresenter data={props.data} /> */}
         </div>
         <div className={styles.label}>Default Presenter</div>
       </div>
