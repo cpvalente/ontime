b846340ccbc8245af6a9680e18a5eba46f3d54a5
cv <34649812+cpvalente@users.noreply.github.com>
2021-04-22 22:43:16 +0200

    addendum to fix 13cc237c

--- client/src/features/viewers/PreviewContainer.jsx
+++ client/src/features/viewers/PreviewContainer.jsx
@@ -1,28 +1,31 @@
 import styles from './PreviewContainer.module.css';
 import IFrameLoader from './iframes/IFrameLoader';
 
+// get origin from URL
+const serverURL = `${window.location.origin}`;
+
 export default function PreviewContainer() {
   return (
     <div className={styles.previewContainer}>
       <div className={styles.previewItem}>
         <IFrameLoader
           title='Default Presenter'
-          src='http://localhost:3000/speaker'
+          src={`${serverURL}/speaker`}
         />
         <div className={styles.label}>Default Presenter</div>
       </div>
       <div className={styles.previewItem}>
-        <IFrameLoader title='Audience' src='http://localhost:3000/public' />
+        <IFrameLoader title='Audience' src={`${serverURL}/public`} />
         <div className={styles.label}>Audience</div>
       </div>
       <div className={styles.previewItem}>
-        <IFrameLoader title='Stage Manager' src='http://localhost:3000/sm' />
+        <IFrameLoader title='Stage Manager' src={`${serverURL}/sm`} />
         <div className={styles.label}>Stage Manager</div>
       </div>
       <div className={styles.previewItem}>
         <IFrameLoader
           title='Lower third'
-          src='http://localhost:3000/lower?key=242424'
+          src={`${serverURL}/lower?key=242424`}
         />
         <div className={styles.label}>Lower third</div>
       </div>
