f2f24d8517f2c5130f79906aee5fc14a9448a740
cv <34649812+cpvalente@users.noreply.github.com>
2021-05-12 23:27:31 +0200

    config distribute

--- server/app.js
+++ server/app.js
@@ -59,7 +59,7 @@ app.get('*', (req, res) => {
 // Implement route for errors
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).send('Something broke!');
+  res.status(500).send(err.stack);
 });
 
 // create HTTP server
@@ -73,7 +73,7 @@ global.timer = new EventTimer(server, config);
 global.timer.setupWithEventList(eventlist);
 
 // Start server
-server.listen(port, () =>
+server.listen(port, '0.0.0.0', () =>
   console.log(`HTTP Server is listening on port ${port}`)
 );
 
--- server/main.js
+++ server/main.js
@@ -1,13 +1,13 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
-const nodeapp = require('./server/app.js');
+const nodeapp = require('./app.js');
 
 function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
+      preload: path.join(__dirname, './preload.js'),
       nodeIntegration: true,
     },
   });
