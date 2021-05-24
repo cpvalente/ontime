7406d5447295dd8b02ac551054facd9fab5b853a
cv <34649812+cpvalente@users.noreply.github.com>
2021-05-24 23:15:53 +0200

    refract: delete unused code

--- server/controllers/eventController.js
+++ server/controllers/eventController.js
@@ -3,13 +3,13 @@ import { db, data } from '../app.js';
 
 // Create controller for GET request to 'event'
 // Returns ACK message
-export const getAll = async (req, res) => {
+export const getEvent = async (req, res) => {
   res.json(data.event);
 };
 
 // Create controller for POST request to 'event'
 // Returns ACK message
-export const post = async (req, res) => {
+export const postEvent = async (req, res) => {
   if (!req.body) {
     res.status(400).send('No object found in request');
     return;
@@ -17,109 +17,10 @@ export const post = async (req, res) => {
   // TODO: validate data
   try {
     data.event = { ...data.event, ...req.body };
-    db.write();
-    // data.event.push({ ...req.body }).write();
+    await db.write();
     res.sendStatus(200);
   } catch (error) {
     res.status(400).send(error);
     console.log(error);
   }
-};
-
-// Create controller for GET request to 'event/title'
-// Returns ACK message
-export const titleGet = async (req, res) => {
-  res.json(data.event.title);
-};
-
-// Create controller for POST request to 'event/title'
-// Returns ACK message
-export const titlePost = async (req, res) => {
-  if (!req.body) {
-    res.status(400).send('No object found in request');
-    return;
-  }
-
-  // TODO: validate data
-  try {
-    data.event.assign({ title: req.body.title }).write();
-    res.sendStatus(200);
-  } catch (error) {
-    res.status(400).send(error);
-  }
-};
-
-// Create controller for GET request to '/event/url'
-// Returns ACK message
-export const urlGet = async (req, res) => {
-  res.json(data.event.url);
-};
-
-// Create controller for POST request to '/event/url'
-// Returns ACK message
-export const urlPost = async (req, res) => {
-  if (!req.body) {
-    res.status(400).send('No object found in request');
-    return;
-  }
-
-  // TODO: validate data
-  try {
-    data.event.assign({ url: req.body.url }).write();
-    res.sendStatus(200);
-  } catch (error) {
-    res.status(400).send(error);
-  }
-};
-
-// Create controller for GET request to 'event/publicInfo'
-// Returns ACK message
-export const publicInfoGet = async (req, res) => {
-  res.json(data.event.publicInfo);
-};
-
-// Create controller for POST request to '/event/publicInfo'
-// Returns ACK message
-export const publicInfoPost = async (req, res) => {
-  if (!req.body) {
-    res.status(400).send('No object found in request');
-    return;
-  }
-
-  // TODO: validate data
-  try {
-    data.event.assign({ publicInfo: req.body.publicInfo }).write();
-    res.sendStatus(200);
-  } catch (error) {
-    res.status(400).send(error);
-  }
-};
-
-// Create controller for GET request to 'event/backstageInfo'
-// Returns ACK message
-export const backstageInfoGet = async (req, res) => {
-  res.json(data.event.backstageInfo);
-};
-
-// Create controller for POST request to '/event/info'
-// Returns ACK message
-export const backstageInfoPost = async (req, res) => {
-  if (!req.body) {
-    res.status(400).send('No object found in request');
-    return;
-  }
-
-  // TODO: validate data
-  try {
-    data.event.assign({ backstageInfo: req.body.backstageInfo }).write();
-    res.sendStatus(200);
-  } catch (error) {
-    res.status(400).send(error);
-  }
-};
-
-// Create controller for GET request to 'event/osc'
-// Returns ACK message
-export const osc = async (req, res) => {
-  res.send('Not yet implemented').status(500);
 };
--- server/routes/eventRouter.js
+++ server/routes/eventRouter.js
@@ -2,45 +2,10 @@ import express from 'express';
 export const router = express.Router();
 
 // import event controller
-import {
-  getAll,
-  post,
-  titleGet,
-  titlePost,
-  publicInfoGet,
-  publicInfoPost,
-  backstageInfoGet,
-  backstageInfoPost,
-  urlGet,
-  urlPost,
-} from '../controllers/eventController.js';
+import { getEvent, postEvent } from '../controllers/eventController.js';
 
-// create route between controller and '/settings' endpoint
-router.get('/', getAll);
+// create route between controller and 'GET /event' endpoint
+router.get('/', getEvent);
 
-// create route between controller and '/settings' endpoint
-router.post('/', post);
-
-// create route between controller and '/event/title' endpoint
-router.get('/title', titleGet);
-
-// create route between controller and '/event/title' endpoint
-router.post('/title', titlePost);
-
-// create route between controller and '/event/info' endpoint
-router.get('/publicInfo', publicInfoGet);
-
-// create route between controller and '/event/info' endpoint
-router.post('/publicInfo', publicInfoPost);
-
-// create route between controller and '/event/info' endpoint
-router.get('/backstageInfo', backstageInfoGet);
-
-// create route between controller and '/event/info' endpoint
-router.post('/backstageInfo', backstageInfoPost);
-
-// create route between controller and '/event/url' endpoint
-router.get('/url', urlGet);
-
-// create route between controller and '/event/url' endpoint
-router.post('/url', urlPost);
+// create route between controller and 'POST /event' endpoint
+router.post('/', postEvent);
