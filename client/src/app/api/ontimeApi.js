59341669061ce598c03d2d3ca11efc3a01a9ceca
cv <34649812+cpvalente@users.noreply.github.com>
2021-05-28 13:51:34 +0200

    get name from header
    - attempt getting name from header. will depend on security policies

--- client/src/app/api/ontimeApi.js
+++ client/src/app/api/ontimeApi.js
@@ -7,10 +7,21 @@ export const downloadEvents = async () => {
     method: 'GET',
     responseType: 'blob', // important
   }).then((response) => {
+    let headerLine = response.headers['Content-Disposition'];
+    console.log(response);
+    let filename = 'events.json';
+
+    // try and get the filename from the response
+    if (headerLine != null) {
+      let startFileNameIndex = headerLine.indexOf('"') + 1;
+      let endFileNameIndex = headerLine.lastIndexOf('"');
+      filename = headerLine.substring(startFileNameIndex, endFileNameIndex);
+    }
+
     const url = window.URL.createObjectURL(new Blob([response.data]));
     const link = document.createElement('a');
     link.href = url;
-    link.setAttribute('download', 'events.json');
+    link.setAttribute('download', filename);
     document.body.appendChild(link);
     link.click();
   });
