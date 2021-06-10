30c659276798cd4d78b87e360958425008569fbf
cv <34649812+cpvalente@users.noreply.github.com>
2021-06-10 23:31:08 +0200

    template testing

--- server/src/utils/time.js
+++ server/src/utils/time.js
@@ -1,3 +1,12 @@
+/**
+ * @description Converts milliseconds to string representing time
+ * @param {number} ms - time in milliseconds
+ * @param {boolean} showSeconds - wether to show the seconds
+ * @param {string} delim - character between HH MM SS
+ * @param {string} ifNull - what to return if value is null
+ * @returns {string} String representing time 00:12:02
+ */
+
 export const stringFromMillis = (
   ms,
   showSeconds = true,
@@ -5,14 +14,17 @@ export const stringFromMillis = (
   ifNull = '...'
 ) => {
   if (ms === null || isNaN(ms)) return ifNull;
+  const isNegative = ms < 0 ? '-' : '';
+  const millis = Math.abs(ms);
+
   const showWith0 = (value) => (value < 10 ? `0${value}` : value);
-  const hours = showWith0(Math.floor(((ms / (1000 * 60 * 60)) % 60) % 24));
-  const minutes = showWith0(Math.floor((ms / (1000 * 60)) % 60));
-  const seconds = showWith0(Math.floor((ms / 1000) % 60));
+  const hours = showWith0(Math.floor(((millis / (1000 * 60 * 60)) % 60) % 24));
+  const minutes = showWith0(Math.floor((millis / (1000 * 60)) % 60));
+  const seconds = showWith0(Math.floor((millis / 1000) % 60));
 
   return showSeconds
-    ? `${
+    ? `${isNegative}${
         parseInt(hours) ? `${hours}${delim}` : `00${delim}`
       }${minutes}${delim}${seconds}`
-    : `${parseInt(hours) ? `${hours}` : '00'}${delim}${minutes}`;
+    : `${isNegative}${parseInt(hours) ? `${hours}` : '00'}${delim}${minutes}`;
 };
