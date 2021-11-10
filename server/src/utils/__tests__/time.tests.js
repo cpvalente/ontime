e567ddeab4c21234df21418d897d2836f3f66a37
cv <34649812+cpvalente@users.noreply.github.com>
2021-11-10 20:53:36 +0100

    test function
    Add more tests to ensure the function remains safe

--- server/src/utils/__tests__/time.tests.js
+++ server/src/utils/__tests__/time.tests.js
@@ -1,16 +1,43 @@
-import { stringFromMillis } from '../time';
+import { stringFromMillis } from '../time.js';
 
-const t1 = { val: null, result: '...' };
-test('test stringFromMillis() on null values', () => {
-  expect(stringFromMillis(t1.val)).toBe(t1.result);
-});
+describe('test string to milis function', () => {
+  it('test with null values', () => {
+    const t = { val: null, result: '...' };
+    expect(stringFromMillis(t.val)).toBe(t.result);
+  });
 
-const t2 = { val: 3600000, result: '01:00:00' };
-test('test stringFromMillis() on valid millis', () => {
-  expect(stringFromMillis(t2.val)).toBe(t2.result);
-});
+  it('test with valid millis', () => {
+    const t = { val: 3600000, result: '01:00:00' };
+    expect(stringFromMillis(t.val)).toBe(t.result);
+  });
 
-const t3 = { val: -3600000, result: '-01:00:00' };
-test('test stringFromMillis() on negative millis', () => {
-  expect(stringFromMillis(t3.val)).toBe(t3.result);
+  it('test with negative millis', () => {
+    const t = { val: -3600000, result: '-01:00:00' };
+    expect(stringFromMillis(t.val)).toBe(t.result);
+  });
+
+  it('test  with 0', () => {
+    const t = { val: 0, result: '00:00:00' };
+    expect(stringFromMillis(t.val)).toBe(t.result);
+  });
+
+  it('test with -0', () => {
+    const t = { val: -0, result: '00:00:00' };
+    expect(stringFromMillis(t.val)).toBe(t.result);
+  });
+
+  it('test with 86400000 (24 hours)', () => {
+    const t = { val: 86400000, result: '00:00:00' };
+    expect(stringFromMillis(t.val)).toBe(t.result);
+  });
+
+  it('test with 86401000 (24 hours and 1 second)', () => {
+    const t = { val: 86401000, result: '00:00:01' };
+    expect(stringFromMillis(t.val)).toBe(t.result);
+  });
+
+  it('test with -86401000 (-24 hours and 1 second)', () => {
+    const t = { val: -86401000, result: '-00:00:01' };
+    expect(stringFromMillis(t.val)).toBe(t.result);
+  });
 });
