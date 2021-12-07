452c835f3abcd56bf2fa6ec6a5f13f5b204c98e4
cv <34649812+cpvalente@users.noreply.github.com>
2021-12-07 22:24:15 +0100

    Integration: create cleanURL function

---
+++ server/src/utils/url.js
@@ -0,0 +1,21 @@
+/**
+ * @description Cleans given url
+ * @param {string} url - URL to be checked
+ * @returns {string} Sanitized url
+ */
+export const cleanURL = (url)  => {
+
+  // trim whitespaces
+  let r = url.trim();
+
+  // clear any whitespaces
+  r = r.split(' ').join('%20');
+
+  // contain only allowed characters
+  r = r.replace(/([^\x00-\x7F]|[@\s<>\[\]{}|\\^%])+/g, '')
+
+  // starts with http://
+  if (!r.startsWith('http://')) r = `http://${r}`
+
+  return r;
+}
