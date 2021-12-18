6ab77f0f0249db8ff1721c862801c3a69b1b0c7c
cv <34649812+cpvalente@users.noreply.github.com>
2021-12-18 23:14:37 +0100

    feat/62-logging: create collapsible header component

---
+++ client/src/common/components/collapseBar/CollapseBar.jsx
@@ -0,0 +1,25 @@
+import PropTypes from "prop-types";
+import style from "../../../features/info/Info.module.scss";
+import {Icon} from "@chakra-ui/react";
+import {FiChevronUp} from "react-icons/fi";
+
+export default function CollapseBar(props) {
+  const {title = 'Collapse bar', isCollapsed = false, onClick}= props;
+
+  return(
+    <div className={style.header}>
+      {title}
+      <Icon
+        className={isCollapsed ? style.moreCollapsed : style.moreExpanded}
+        as={FiChevronUp}
+        onClick={onClick}
+      />
+    </div>
+  )
+}
+CollapseBar.propTypes = {
+  title: PropTypes.string,
+  isCollapsed: PropTypes.bool,
+  onClick: PropTypes.func,
+}
+
