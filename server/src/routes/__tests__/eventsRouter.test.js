8b4d3e54050838a6fd9340528f8feb1b8d787d48
cv <34649812+cpvalente@users.noreply.github.com>
2022-01-14 22:08:13 +0100

    hotfix/6.0.2 testing

--- server/src/routes/__tests__/eventsRouter.test.js
+++ server/src/routes/__tests__/eventsRouter.test.js
@@ -16,12 +16,22 @@ const testEvent = {
   id: 'superSpecial12',
 };
 
+const eventFromDb = {
+  title: 'Welcome to Ontime',
+  subtitle: 'Subtitles are useful',
+  presenter: 'cpvalente',
+  note: 'Maybe a running note for the operator?',
+  timeStart: 28800000,
+  timeEnd: 30600000,
+  isPublic: false,
+  type: 'event',
+  revision: 7,
+  id: '5946',
+};
+
 describe('When a POST request is sent', () => {
   test('POST /event should return a 201', async () => {
-    await supertest(server)
-      .post('/events')
-      .send(testEvent)
-      .expect(201)
+    await supertest(server).post('/events').send(testEvent).expect(201);
   });
 });
 
@@ -38,20 +48,20 @@ describe('When a GET request request is sent', () => {
   });
   test('GET /events/:eventId returns a valid object', async () => {
     await supertest(server)
-      .get(`/events/${testEvent.id}`)
+      .get(`/events/${eventFromDb.id}`)
       .expect(200)
       .then((response) => {
         expect(response.text.includes('<!doctype html>')).toBe(false);
         expect(response.body).toBeDefined();
-        expect(response.body.title).toBe(testEvent.title);
-        expect(response.body.subtitle).toBe(testEvent.subtitle);
-        expect(response.body.presenter).toBe(testEvent.presenter);
-        expect(response.body.note).toBe(testEvent.note);
-        expect(response.body.timeStart).toBe(testEvent.timeStart);
-        expect(response.body.timeEnd).toBe(testEvent.timeEnd);
-        expect(response.body.isPublic).toBe(testEvent.isPublic);
-        expect(response.body.type).toBe(testEvent.type);
-        expect(response.body.id).toBe(testEvent.id);
+        expect(response.body.title).toBe(eventFromDb.title);
+        expect(response.body.subtitle).toBe(eventFromDb.subtitle);
+        expect(response.body.presenter).toBe(eventFromDb.presenter);
+        expect(response.body.note).toBe(eventFromDb.note);
+        expect(response.body.timeStart).toBe(eventFromDb.timeStart);
+        expect(response.body.timeEnd).toBe(eventFromDb.timeEnd);
+        expect(response.body.isPublic).toBe(eventFromDb.isPublic);
+        expect(response.body.type).toBe(eventFromDb.type);
+        expect(response.body.id).toBe(eventFromDb.id);
       });
   });
 });
