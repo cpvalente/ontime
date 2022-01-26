5742ec878b4b6a18ef2180cad145a019d855f929
cv <34649812+cpvalente@users.noreply.github.com>
2022-01-26 21:33:17 +0100

    feat/table format data

--- client/src/features/table/OntimeTable.jsx
+++ client/src/features/table/OntimeTable.jsx
@@ -1,33 +1,39 @@
 import style from './Table.module.scss';
-import { IoSunnyOutline } from '@react-icons/all-files/io5/IoSunnyOutline';
+import { FiCheck } from '@react-icons/all-files/fi/FiCheck';
+import { stringFromMillis } from 'ontime-utils/time';
+import PublicIconBtn from '../../common/components/buttons/PublicIconBtn';
 
 export default function OnTimeTable({ columns, data }) {
   console.log(data);
   return (
     <div className={style.tableContainer}>
       <div className={style.rowHeader}>
-        <div style={{ width: '1%' }} className={style.indexColumn}>#</div>
+        <div style={{ width: '1%' }} className={style.indexColumn}>
+          #
+        </div>
         {columns.map((c) => (
           <div style={{ width: `${99 / 14}%` }} key={c.accessor} className={style.headerCell}>
             {c.header}
-            <IoSunnyOutline />
+            <FiCheck />
           </div>
         ))}
       </div>
       {data.map((d, index) => (
         <div className={style.row} key={d.id}>
-          <div className={style.indexColumn} style={{ width: '1%' }}>{index + 1}</div>
-          <div className={style.column} style={{ width: `${99 / 14}%`}}>
+          <div className={style.indexColumn} style={{ width: '1%' }}>
+            {index + 1}
+          </div>
+          <div className={style.column} style={{ width: `${99 / 14}%` }}>
             {d.type}
           </div>
           <div className={style.column} style={{ width: `${99 / 14}%` }}>
-            {d.timeStart}
+            {stringFromMillis(d.timeStart)}
           </div>
           <div className={style.column} style={{ width: `${99 / 14}%` }}>
-            {d.timeEnd}
+            {stringFromMillis(d.timeEnd)}
           </div>
           <div className={style.column} style={{ width: `${99 / 14}%` }}>
-            {d.duration}
+            {stringFromMillis(d.duration)}
           </div>
           <div className={style.column} style={{ width: `${99 / 14}%` }}>
             {d.title}
@@ -42,7 +48,7 @@ export default function OnTimeTable({ columns, data }) {
             {d.note}
           </div>
           <div className={style.column} style={{ width: `${99 / 14}%` }}>
-            {d.isPublic}
+            <PublicIconBtn active={d.isPublic} />
           </div>
           <div className={style.column} style={{ width: `${99 / 14}%` }}>
             ' - '
