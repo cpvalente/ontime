1e9cd09db95263a455369a983208003c80b6e57b
cv <34649812+cpvalente@users.noreply.github.com>
2022-03-06 20:46:11 +0100

    feat/table: ensure contrast between text and bg

--- client/src/features/table/tableRows/EventRow.jsx
+++ client/src/features/table/tableRows/EventRow.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Color from 'color';
 import style from '../Table.module.scss';
+
+/**
+ * Selects text colour to maintain accessible contrast
+ * @param bgColour
+ * @return {{backgroundColor, color: string}}
+ */
+const selCol = (bgColour) => {
+  if (bgColour != null && bgColour !== '') {
+    try {
+      const textColor = Color(bgColour).isLight() ? 'black' : 'white';
+      return { backgroundColor: bgColour, color: textColor };
+    } catch (error) {
+      console.log(`Unable to parse colour: ${bgColour}`);
+    }
+  }
+};
 
 export default function EventRow(props) {
   const { row, index, selectedId, delay } = props;
   const selected = row.original.id === selectedId;
+  const colours = selCol(row.original.colour);
+
   return (
     <tr {...row.getRowProps()} className={selected ? style.selected : ''}>
       <td className={style.indexColumn}>{index}</td>
@@ -20,11 +39,7 @@ export default function EventRow(props) {
         }
 
         return (
-          <td
-            key={key}
-            style={{ ...style, backgroundColor: row.original.colour || 'none' }}
-            {...restCellProps}
-          >
+          <td key={key} style={{ ...style, ...colours }} {...restCellProps}>
             {cell.render('Cell')}
           </td>
         );
