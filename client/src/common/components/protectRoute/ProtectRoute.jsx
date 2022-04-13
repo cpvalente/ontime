55811a38bf593c18b2e30b99f0800a4549855b6b
cv <34649812+cpvalente@users.noreply.github.com>
2022-04-13 18:16:32 +0200

    refact: easier login process

--- client/src/common/components/protectRoute/ProtectRoute.jsx
+++ client/src/common/components/protectRoute/ProtectRoute.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useCallback, useContext, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import style from './ProtectRoute.module.scss';
 import { PinInput, PinInputField } from '@chakra-ui/react';
@@ -13,53 +13,77 @@ export default function ProtectRoute({ children }) {
   const [failed, setFailed] = useState(false);
   const { auth, validate } = useContext(AppContext);
 
+  const handleValidation = useCallback(() => {
+    const r = validate(pin);
+    if (!r) {
+      setFailed(true);
+      setPin('');
+    }
+  }, [pin, validate]);
+
   // Set window title
   useEffect(() => {
     document.title = 'ontime';
   }, []);
 
-  const handleValidation = () => {
-    const r = validate(pin);
-    if (!r) {
-      setFailed(true);
-    }
-  };
-
-  return (
-    <>
-      {!isLocal && !auth ? (
-        <div className={style.container}>
-          ontime
-          <div className={failed ? style.pin__failed : style.pin}>
-            <PinInput
-              type='alphanumeric'
-              size='lg'
-              mask
-              onChange={(value) => {
-                setFailed(false);
-                setPin(value);
-              }}
-            >
-              <PinInputField />
-              <PinInputField />
-              <PinInputField />
-              <PinInputField />
-            </PinInput>
-            <IconButton
-              aria-label='Enter'
-              size='lg'
-              isRound
-              icon={<FiCheck />}
-              style={{ fontSize: '1.5em' }}
-              onClick={() => handleValidation()}
-            />
-          </div>
-        </div>
-      ) : (
-        children
-      )}
-    </>
+  // Handle keyboard shortcuts
+  const handleKeyPress = useCallback(
+    (e) => {
+      // handle held key
+      if (e.repeat) return;
+      // Space bar
+      if (e.keyCode === 13) {
+        handleValidation();
+      }
+    },
+    [handleValidation]
   );
+
+  useEffect(() => {
+    // attach the event listener
+    document.addEventListener('keydown', handleKeyPress);
+
+    // remove the event listener
+    return () => {
+      document.removeEventListener('keydown', handleKeyPress);
+    };
+  }, [handleKeyPress]);
+
+  if (isLocal || auth) {
+    return children;
+  } else {
+    return (
+      <div className={style.container}>
+        ontime
+        <div className={failed ? style.pin__failed : style.pin}>
+          <PinInput
+            type='alphanumeric'
+            size='lg'
+            mask
+            autoFocus
+            value={pin}
+            onChange={(value) => {
+              setFailed(false);
+              setPin(value);
+            }}
+          >
+            <PinInputField />
+            <PinInputField />
+            <PinInputField />
+            <PinInputField />
+          </PinInput>
+          <IconButton
+            aria-label='Enter'
+            size='lg'
+            isRound
+            icon={<FiCheck />}
+            style={{ fontSize: '1.5em' }}
+            onClick={() => handleValidation()}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 ProtectRoute.propTypes = {
