cc9eea5d9d4f4999e091fb6ec6dac8531a8263ba
cv <34649812+cpvalente@users.noreply.github.com>
2022-08-29 22:11:06 +0200

    feature(css-override): <Pip>

--- client/src/features/viewers/picture-in-picture/Pip.jsx
+++ client/src/features/viewers/picture-in-picture/Pip.jsx
@@ -7,7 +7,7 @@ import { formatDisplay } from 'common/utils/dateConfig';
 import { AnimatePresence, motion } from 'framer-motion';
 import PropTypes from 'prop-types';
 
-import style from './Pip.module.scss';
+import './Pip.scss';
 
 export default function Pip(props) {
   const { time, backstageEvents, selectedId, general } = props;
@@ -17,7 +17,7 @@ export default function Pip(props) {
   const [pageNumber, setPageNumber] = useState(0);
   const [currentPage, setCurrentPage] = useState(0);
 
-  // calculcate pip size
+  // calculate pip size
   useLayoutEffect(() => {
     const h = ref.current.clientHeight;
     const w = ref.current.clientWidth;
@@ -57,20 +57,20 @@ export default function Pip(props) {
   if (time.isNegative) stageTimer = `-${stageTimer}`;
 
   return (
-    <div className={style.container__gray}>
+    <div className='container pip'>
       <NavLogo />
 
-      <div className={style.eventTitle}>{general.title}</div>
+      <div className='event-title'>{general.title}</div>
 
-      <div className={style.todayContainer}>
-        <div className={style.todayHeaderBlock}>
-          <div className={style.label}>Today</div>
-          <div className={style.nav}>
+      <div className='today-container'>
+        <div className='today-header-block'>
+          <div className='label'>Today</div>
+          <div className='nav'>
             {pageNumber > 1 &&
             [...Array(pageNumber).keys()].map((i) => (
               <div
                 key={i}
-                className={i === currentPage ? style.navItemSelected : style.navItem}
+                className={i === currentPage ? 'nav-item nav-item--selected' : 'nav-item'}
               />
             ))}
           </div>
@@ -86,19 +86,19 @@ export default function Pip(props) {
         />
       </div>
 
-      <div className={style.pip} ref={ref}>
-        <Emptyimage className={style.empty} />
-        <span className={style.piptext}>{size}</span>
+      <div className='pip' ref={ref}>
+        <Emptyimage className='pip__empty' />
+        <span className='pip__text'>{size}</span>
       </div>
 
       <AnimatePresence>
         {showInfo && (
-          <motion.div className={style.infoContainer}>
-            <div className={style.label}>Info</div>
-            <div className={style.infoMessages}>
-              <div className={style.info}>{general.backstageInfo}</div>
+          <motion.div className='info-container'>
+            <div className='label'>Info</div>
+            <div className='info-message'>
+              {general.backstageInfo}
             </div>
-            <div className={style.qr}>
+            <div className='qr'>
               {general.url != null && general.url !== '' && (
                 <QRCode
                   value={general.url}
@@ -111,14 +111,14 @@ export default function Pip(props) {
         )}
       </AnimatePresence>
 
-      <div className={style.clockContainer}>
-        <div className={style.label}>Time Now</div>
-        <div className={style.clock}>{time.clock}</div>
+      <div className='clock-container'>
+        <div className='label'>Time Now</div>
+        <div className='clock'>{time.clock}</div>
       </div>
 
-      <div className={style.countdownContainer}>
-        <div className={style.label}>Stage Timer</div>
-        <div className={style.clock}>{stageTimer}</div>
+      <div className='timer-container'>
+        <div className='label'>Stage Timer</div>
+        <div className='timer'>{stageTimer}</div>
       </div>
     </div>
   );
