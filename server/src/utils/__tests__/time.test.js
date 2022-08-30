e51028e764e8f22deb03ce919e808e4e3d48b3a5
cv <34649812+cpvalente@users.noreply.github.com>
2022-08-30 22:17:03 +0200

    feat(excelDates): update tests

--- server/src/utils/__tests__/time.test.js
+++ server/src/utils/__tests__/time.test.js
@@ -5,8 +5,19 @@ describe('parseExcelDate', () => {
     const millis = parseExcelDate('1899-12-30T07:00:00.000Z');
     expect(millis).not.toBe(0);
   });
+
+  describe('parses a time string that passes validation', () => {
+    const validFields = ['10:00:00', '10:00'];
+    validFields.forEach((field) => {
+      it(`handles ${field}`, () => {
+        const millis = parseExcelDate(field);
+        expect(millis).not.toBe(0);
+      });
+    });
+  });
+
   describe('returns 0 on other strings', () => {
-    const invalidFields = ['10:00:00', 'test', ''];
+    const invalidFields = ['10', 'test', ''];
     invalidFields.forEach((field) => {
       it(`handles ${field}`, () => {
         const millis = parseExcelDate(field);
