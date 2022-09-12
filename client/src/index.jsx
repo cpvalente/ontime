1562dd40d3cab8bceb338c40335e86cae672f3cb
cv <34649812+cpvalente@users.noreply.github.com>
2022-09-12 20:37:41 +0200

    chore: upgrade chakra and deps

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,5 +1,5 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 
 import App from './App';
 
@@ -11,5 +11,5 @@ const root = createRoot(container);
 root.render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 );
