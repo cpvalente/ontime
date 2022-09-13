eb602bb03f909b45671ccd9022b0f7f44b5822c9
cv <34649812+cpvalente@users.noreply.github.com>
2022-09-13 20:32:21 +0200

    style: improve styling in menu

--- client/src/features/menu/EventListMenu.jsx
+++ client/src/features/menu/EventListMenu.jsx
@@ -85,14 +85,14 @@ const EventListMenu = ({ eventsHandler }) => {
           clickHandler={() => actionHandler('cursorUp')}
           icon={<IoCaretUp />}
           tooltip='Move cursor up Alt + ↑'
-          _hover={{ bg: 'pink.400' }}
+          _hover={{ bg: 'pink.400', color: 'white' }}
         />
         <TooltipActionBtn
           {...cursorBtnProps}
           clickHandler={() => actionHandler('cursorDown')}
           icon={<IoCaretDown />}
           tooltip='Move cursor down Alt + ↓'
-          _hover={{ bg: 'pink.400' }}
+          _hover={{ bg: 'pink.400', color: 'white' }}
         />
         <TooltipActionBtn
           {...cursorBtnProps}
@@ -101,7 +101,8 @@ const EventListMenu = ({ eventsHandler }) => {
           tooltip='Lock cursor to current'
           width='3em'
           backgroundColor={isCursorLocked && 'pink.400'}
-          _hover={{ bg: 'pink.300' }}
+          color={isCursorLocked && 'white'}
+          _hover={{ bg: 'pink.400', color: 'white' }}
           variant={isCursorLocked ? 'solid' : 'outline'}
         />
       </ButtonGroup>
