d078594ba0726874c7d77c166b6e1446df5ae17c
cv <34649812+cpvalente@users.noreply.github.com>
2022-10-18 22:20:35 +0200

    chore: update tests

--- client/src/features/menu/__tests__/MenuActionButtons.test.jsx
+++ client/src/features/menu/__tests__/MenuActionButtons.test.jsx
@@ -1,11 +1,17 @@
+import { QueryClientProvider } from '@tanstack/react-query';
 import { render, screen } from '@testing-library/react';
 import { vi } from 'vitest';
 
+import { queryClientMock } from '../../../__mocks__/QueryClient.mock';
 import MenuActionButtons from '../MenuActionButtons';
 
 const actionHandler = vi.fn();
 const renderInMock = () => {
-  render(<MenuActionButtons actionHandler={actionHandler} />);
+  render(
+    <QueryClientProvider client={queryClientMock}>
+      <MenuActionButtons actionHandler={actionHandler} />
+    </QueryClientProvider>,
+  );
 };
 
 test('check that menu bar renders correctly', () => {
