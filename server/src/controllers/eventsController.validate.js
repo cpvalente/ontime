7483fced650f29207744947f1597d488d0b97a3f
cv <34649812+cpvalente@users.noreply.github.com>
2022-10-22 22:31:38 +0200

    refactor: add validation to events post

--- server/src/controllers/eventsController.validate.js
+++ server/src/controllers/eventsController.validate.js
@@ -1,5 +1,14 @@
 import { body, param, validationResult } from 'express-validator';
 
+export const eventsPostValidator = [
+  body('type').isString().exists().isIn(['event', 'delay', 'block']),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) return res.status(422).json({ errors: errors.array() });
+    next();
+  },
+];
+
 export const eventsPutValidator = [
   body('id').isString().exists(),
   (req, res, next) => {
