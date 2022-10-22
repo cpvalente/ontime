e6367fa6f3a10bba55d39e7e78b823f276723893
cv <34649812+cpvalente@users.noreply.github.com>
2022-10-22 21:54:23 +0200

    refactor: tweaks on style + ts

--- client/src/declarations/declaration.d.ts
+++ client/src/declarations/declaration.d.ts
@@ -3,10 +3,13 @@ declare module '*.scss' {
   export default content;
 }
 
+type ListenerType = (event: 'string', args: unknown[]) => void;
+
 declare global {
   interface Window {
     ipcRenderer: {
       send: (channel: string, args?: string | object) => void;
+      on: (channel: string, listener: ListenerType) => void;
     };
     process: {
       type: string;
