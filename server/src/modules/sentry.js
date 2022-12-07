ee87ca8db4f2831400e62a31b16678f7b29db213
cv <34649812+cpvalente@users.noreply.github.com>
2022-12-07 18:15:28 +0100

    chore: report only in production

--- server/src/modules/sentry.js
+++ server/src/modules/sentry.js
@@ -4,8 +4,6 @@ let shouldReport;
 
 export function initSentry(environment) {
   shouldReport = environment === 'production';
-  process.exit(0);
-
   Sentry.init({
     dsn: 'https://ceb6abdce7374857bb50b65636cbaed1@o4504288369836032.ingest.sentry.io/4504288555565056',
     tracesSampleRate: 1.0,
