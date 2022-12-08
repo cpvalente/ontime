5d894c3f1e245d90b31eeff225f97bc7a87b8247
cv <34649812+cpvalente@users.noreply.github.com>
2022-12-08 22:02:52 +0100

    style: tweak switch styles

--- client/src/theme/ontimeSwitch.ts
+++ client/src/theme/ontimeSwitch.ts
@@ -2,9 +2,13 @@ export const ontimeSwitch = {
   container: {  },
   track: {
     background: '#2d2d2d', // $gray-1100
+    border: '1px solid transparent',
     _checked: {
       background: `#2B5ABC`, // $blue-700
     },
+    _focus: {
+      border: '1px solid #578AF4', // $blue-500
+    }
   },
   thumb: {},
 };
