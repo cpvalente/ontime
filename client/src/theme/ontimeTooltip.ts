96ac83f850f84907295394bfc9cd558ac5455918
cv <34649812+cpvalente@users.noreply.github.com>
2022-12-29 12:46:48 +0100

    style: small tweaks and clarifications

--- client/src/theme/ontimeTooltip.ts
+++ client/src/theme/ontimeTooltip.ts
@@ -1,12 +1,12 @@
 export const ontimeTooltip = {
   _light: {
-    backgroundColor: '#2d2d2d',
-    color: '#779BE7',
+    backgroundColor: '#2d2d2d', // $gray-1100
+    color: '#ececec', // $gray-100
     padding: '2px 8px',
   },
   _dark: {
-    backgroundColor: '#2d2d2d',
-    color: '#779BE7',
+    backgroundColor: '#2d2d2d', // $gray-1100
+    color: '#ececec', // $gray-100
     padding: '2px 8px',
   },
 };
