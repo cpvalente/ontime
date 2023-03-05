d8ba936383e789de8ee90afc30d5101815eacc5f
Fabian Posenau <fabian@fphome.de>
2023-03-05 22:17:53 +0100

    add edge cases

--- apps/client/src/common/utils/__tests__/dateConfig.test.js
+++ apps/client/src/common/utils/__tests__/dateConfig.test.js
@@ -305,6 +305,9 @@ describe('test forgivingStringToMillis()', () => {
       { value: '1h0m0s', expect: 1000 * 60 * 60 },
       { value: '23h0m0s', expect: 1000 * 60 * 60 * 23 },
       { value: '12h12m12s', expect: 12 * 1000 + 12 * 60 * 1000 + 12 * 1000 * 60 * 60 },
+      { value: '2m', expect: 2 * 60 * 1000 },
+      { value: '1h5s', expect: 1000 * 60 * 60 + 1000 * 5 },
+      { value: '1h2m', expect: 1000 * 60 * 60 + 1000 * 60 * 2 },
     ];
 
     for (const s of testData) {
