c059d6b708f1ce35e89d3ea1254fa69bdcdf90d4
Fabian Posenau <fabian@fphome.de>
2023-03-12 18:34:46 +0100

    grammar

--- apps/server/src/services/TimerService.ts
+++ apps/server/src/services/TimerService.ts
@@ -324,7 +324,7 @@ export class TimerService {
     if (this.timer.endAction === EndAction.Stop) {
       PlaybackService.stop();
     } else if (this.timer.endAction === EndAction.LoadNext) {
-      // we need to delay here because to put this action in the queue stack. otherwise it wont be executed properly
+      // we need to delay here to put this action in the queue stack. otherwise it won't be executed properly
       setTimeout(() => {
         PlaybackService.loadNext();
       }, 0);
