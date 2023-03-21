ea02a52afaeca9f6ba08db452a074c92dd4e0809
Fabian Posenau <fabian@fphome.de>
2023-03-21 19:12:26 +0100

    resolve wierd path issue

--- apps/server/src/config/config.js
+++ apps/server/src/config/config.js
@@ -1,12 +1,12 @@
 export const config = {
   database: {
     testdb: 'test-db',
-    directory: 'preloaded-db',
+    directory: 'db',
     filename: 'db.json',
     tablename: 'events',
   },
   styles: {
-    directory: 'external/styles',
+    directory: 'external-styles',
     filename: 'override.css',
   },
 };
