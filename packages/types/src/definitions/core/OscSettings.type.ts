56a03e94a03a071cb750e98192a49089b676bf9a
cv <34649812+cpvalente@users.noreply.github.com>
2023-03-22 22:08:31 +0100

    feat: parse and validate subscriptions

--- packages/types/src/definitions/core/OscSettings.type.ts
+++ packages/types/src/definitions/core/OscSettings.type.ts
@@ -1,7 +1,6 @@
-import { TimerLifeCycle } from './TimerLifecycle.type.js';
+import { TimerLifeCycleKey } from './TimerLifecycle.type.js';
 
-export type TimerLifeCycleKey = keyof typeof TimerLifeCycle;
-export type OscSubscriptionOptions = { id: string; message: string; enabled: boolean }
+export type OscSubscriptionOptions = { id: string; message: string; enabled: boolean };
 export type OscSubscription = { [key in TimerLifeCycleKey]: OscSubscriptionOptions[] };
 
 export interface OSCSettings {
