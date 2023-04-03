89608d47943819b8fecbfc967efe49f3dfb08038
cv <34649812+cpvalente@users.noreply.github.com>
2023-04-03 16:33:42 +0200

    chore: profile node

--- apps/server/src/modules/sentry.js
+++ apps/server/src/modules/sentry.js
@@ -1,4 +1,5 @@
 import * as Sentry from '@sentry/node';
+import { ProfilingIntegration } from '@sentry/profiling-node';
 
 let shouldReport;
 
@@ -7,6 +8,11 @@ export function initSentry(doReport) {
   Sentry.init({
     dsn: 'https://ceb6abdce7374857bb50b65636cbaed1@o4504288369836032.ingest.sentry.io/4504288555565056',
     tracesSampleRate: 1.0,
+    profilesSampleRate: 1.0, // Profiling sample rate is relative to tracesSampleRate
+    integrations: [
+      // Add profiling integration to list of integrations
+      new ProfilingIntegration(),
+    ],
   });
 }
 
