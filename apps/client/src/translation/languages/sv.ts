4dcfa0d0bafa50d20678f815bbe57d4b107545f6
cv <34649812+cpvalente@users.noreply.github.com>
2023-05-04 21:41:07 +0200

    feat: add nordic translations

---
+++ apps/client/src/translation/languages/sv.ts
@@ -0,0 +1,18 @@
+import { TranslationObject } from '../Translation.types';
+
+export const langSv: TranslationObject = {
+  'common.end_time': 'Sluttid',
+  'common.expected_finish': 'Förväntat slut',
+  'common.now': 'Nu',
+  'common.next': 'Nästa',
+  'common.public_message': 'Offentligt meddelande',
+  'common.start_time': 'Starttid',
+  'common.stage_timer': 'Timer för scenen',
+  'common.started_at': 'Började vid',
+  'common.time_now': 'Tid nu',
+  'countdown.ended': 'Evenemanget avslutades vid',
+  'countdown.running': 'Evenemang pågår',
+  'countdown.select_event': 'Välj ett evenemang att följa',
+  'countdown.to_start': 'Tid till start',
+  'countdown.waiting': '"Väntar på att evenemanget ska starta',
+};
