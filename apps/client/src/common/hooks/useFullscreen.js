cadfcc4e68e11b532fd0b7c22216c2896494b8dc
Carlos Valente <34649812+cpvalente@users.noreply.github.com>
2023-05-19 14:23:57 +0200

    fix: fullscreen toggle in safari

--- apps/client/src/common/hooks/useFullscreen.js
+++ apps/client/src/common/hooks/useFullscreen.js
@@ -1,17 +1,24 @@
 import { useCallback, useEffect, useState } from 'react';
 
-
 export default function useFullscreen() {
   const [isFullScreen, setFullScreen] = useState(document.fullscreenElement);
 
   useEffect(() => {
     const handleChange = () => {
-      setFullScreen(document.fullscreenElement);
+      if (document.webkitFullscreenElement === null) {
+        setFullScreen(false);
+      } else if (document.webkitFullscreenElement) {
+        setFullScreen(true);
+      } else {
+        setFullScreen(document.fullscreenElement);
+      }
     };
+    document.addEventListener('webkitfullscreenchange', handleChange, { passive: true });
     document.addEventListener('fullscreenchange', handleChange, { passive: true });
     document.addEventListener('resize', handleChange, { passive: true });
 
     return () => {
+      document.removeEventListener('webkitfullscreenchange', handleChange, { passive: true });
       document.removeEventListener('fullscreenchange', handleChange, { passive: true });
       document.removeEventListener('resize', handleChange, { passive: true });
     };
@@ -32,9 +39,9 @@ export default function useFullscreen() {
       if (document.exitFullscreen) {
         // Standard fullscreen API is supported
         document.exitFullscreen();
-      } else if (document.webkitCancelFullscreen) {
+      } else if (document.webkitExitFullscreen) {
         // iOS Safari fullscreen API is supported
-        document.webkitCancelFullscreen();
+        document.webkitExitFullscreen();
       }
     }
   }, []);
