67ce2c9fa8e413ee4a642113138512ef0f858ae6
Fabian Posenau <fabian@fphome.de>
2023-05-21 00:30:16 +0200

    convert time util to ts

--- apps/server/src/utils/time.ts
+++ apps/server/src/utils/time.ts
@@ -11,7 +11,7 @@ export const DAY_TO_MS = 86400000;
  * @param {string} string - time string "23:00:12"
  * @returns {boolean} string represents time
  */
-export const isTimeString = (string) => {
+export const isTimeString = (string: string): boolean => {
   // ^                   # Start of string
   // (?:                 # Try to match...
   //  (?:                #  Try to match...
@@ -31,7 +31,8 @@ export const isTimeString = (string) => {
  * @argument {string} date - excel string date
  * @returns {number} - time in milliseconds
  */
-export const dateToMillis = (date) => {
+
+export const dateToMillis = (date: Date): number => {
   const h = date.getHours();
   const m = date.getMinutes();
   const s = date.getSeconds();
@@ -44,7 +45,7 @@ export const dateToMillis = (date) => {
  * @param valueAsString
  * @return {number}
  */
-const parse = (valueAsString) => {
+const parse = (valueAsString: string): number => {
   const parsed = parseInt(valueAsString, 10);
   if (isNaN(parsed)) {
     return 0;
@@ -58,7 +59,7 @@ const parse = (valueAsString) => {
  * @param {boolean} fillLeft - autofill left = hours / right = seconds
  * @returns {number} - time string in millis
  */
-export const forgivingStringToMillis = (value, fillLeft = true) => {
+export const forgivingStringToMillis = (value: string, fillLeft = true): number => {
   let millis = 0;
 
   // split string at known separators    : , .
@@ -106,13 +107,48 @@ export const forgivingStringToMillis = (value, fillLeft = true) => {
  * @returns {number} - time in milliseconds
 
  */
-export const parseExcelDate = (excelDate) => {
+export const parseExcelDate = (excelDate: string): number => {
   // attempt converting to date object
   const date = new Date(excelDate);
-  if (date instanceof Date && !isNaN(date)) {
+  if (date instanceof Date && !isNaN(date.getTime())) {
     return dateToMillis(date);
   } else if (isTimeString(excelDate)) {
     return forgivingStringToMillis(excelDate);
   }
   return 0;
+};
+
+/**
+ * another go at simpler string formatting (counters) -- Copied from client code
+ * @description Converts seconds to string representing time
+ * @param {number | null} milliseconds - time in seconds
+ * @param {boolean} [hideZero] - whether to show hours in case its 00
+ * @returns {string} String representing absolute time 00:12:02
+ */
+export function formatDisplay(milliseconds: number | null, hideZero = false): string {
+  if (typeof milliseconds !== 'number') {
+    return hideZero ? '00:00' : '00:00:00';
+  }
+
+  // add an extra 0 if necessary
+  const format = (val: number) => `0${Math.floor(val)}`.slice(-2);
+
+  const s = Math.abs(millisToSeconds(milliseconds));
+  const hours = Math.floor((s / 3600) % 24);
+  const minutes = Math.floor((s % 3600) / 60);
+
+  if (hideZero && hours < 1) return [minutes, s % 60].map(format).join(':');
+  return [hours, minutes, s % 60].map(format).join(':');
+}
+
+/**
+ * @description Converts milliseconds to seconds -- Copied from client code
+ * @param {number | null} millis - time in seconds
+ * @returns {number} Amount in seconds
+ */
+export const millisToSeconds = (millis: number | null): number => {
+  if (millis === null) {
+    return 0;
+  }
+  return millis < 0 ? Math.ceil(millis / mts) : Math.floor(millis / mts);
 };
