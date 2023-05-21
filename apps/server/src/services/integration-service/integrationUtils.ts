240e6f2554c0f0f6dbafe2d99ac3b374dab7b72a
cv <34649812+cpvalente@users.noreply.github.com>
2023-05-21 21:50:43 +0200

    refactor: remove unused code

--- apps/server/src/services/integration-service/integrationUtils.ts
+++ apps/server/src/services/integration-service/integrationUtils.ts
@@ -2,22 +2,6 @@
 const placeholderRegex = /{{(.*?)}}/g;
 
 /**
- * Parses a templated string
- */
-export function parseTemplate(template: string, state: object): string {
-  let parsedTemplate = template;
-  let match;
-  while ((match = placeholderRegex.exec(template)) !== null) {
-    const variableName = match[1];
-    if (Object.hasOwn(state, variableName)) {
-      parsedTemplate = parsedTemplate.replace(match[0], state[variableName]);
-    }
-  }
-
-  return parsedTemplate;
-}
-
-/**
  * Parses a templated string to values in a nested object
  */
 export function parseTemplateNested(template: string, state: object): string {
