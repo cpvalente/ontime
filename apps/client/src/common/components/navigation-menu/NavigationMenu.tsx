97df2136857ff12c4a0f0c5af9b6f59cfeaa8bf4
asharonbaltazar <asharonbaltazar@outlook.com>
2023-06-04 16:28:01 -0400

    remove `isEditButtonHidden` prop from `NavigationMenu`

--- apps/client/src/common/components/navigation-menu/NavigationMenu.tsx
+++ apps/client/src/common/components/navigation-menu/NavigationMenu.tsx
@@ -16,11 +16,7 @@ import { useViewOptionsStore } from '../../stores/viewOptions';
 
 import style from './NavigationMenu.module.scss';
 
-interface NavigationMenuProps {
-  isEditBtnHidden?: boolean;
-}
-
-export default function NavigationMenu({ isEditBtnHidden = false }: NavigationMenuProps) {
+export default function NavigationMenu() {
   const location = useLocation();
 
   const { isFullScreen, toggleFullScreen } = useFullscreen();
@@ -66,11 +62,9 @@ export default function NavigationMenu({ isEditBtnHidden = false }: NavigationMe
         <button onClick={toggleMenu} aria-label='toggle menu' className={style.navButton}>
           <IoApps />
         </button>
-        {isEditBtnHidden === false && (
-          <button className={style.button} onClick={showEditFormDrawer}>
-            <IoPencilSharp />
-          </button>
-        )}
+        <button className={style.button} onClick={showEditFormDrawer}>
+          <IoPencilSharp />
+        </button>
         {showMenu && (
           <div className={style.menuContainer} data-testid='navigation-menu'>
             <div className={style.buttonsContainer}>
