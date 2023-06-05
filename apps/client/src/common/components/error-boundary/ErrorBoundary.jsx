52ac990d3bedb4faec7c88e1175e3811776a3723
cv <34649812+cpvalente@users.noreply.github.com>
2023-06-05 20:33:04 +0200

    refactor: avoid wildcard imports

--- apps/client/src/common/components/error-boundary/ErrorBoundary.jsx
+++ apps/client/src/common/components/error-boundary/ErrorBoundary.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/destructuring-assignment */
 import React from 'react';
+// skipcq: JS-C1003 - sentry does not expose itself as an ES Module.
 import * as Sentry from '@sentry/react';
 
 import style from './ErrorBoundary.module.scss';
