794fbc867e7b881db8f40dfa7bc8411c70b59fa2
cv <34649812+cpvalente@users.noreply.github.com>
2023-06-18 22:20:04 +0200

    style: small tweaks to components

--- apps/client/src/features/table/tableElements/SortableCell.tsx
+++ apps/client/src/features/table/tableElements/SortableCell.tsx
@@ -1,51 +1,20 @@
-import { ReactNode } from 'react';
-import { Tooltip } from '@chakra-ui/react';
+import { CSSProperties, ReactNode } from 'react';
 import { useSortable } from '@dnd-kit/sortable';
 import { CSS } from '@dnd-kit/utilities';
 import { Header } from '@tanstack/react-table';
 import { OntimeRundownEntry } from 'ontime-types';
 
 import { cx } from '../../../common/utils/styleUtils';
-import { tooltipDelayFast } from '../../../ontimeConfig';
 
 import styles from '../Cuesheet.module.scss';
 
 interface SortableCellProps {
-  column: object;
-}
-
-export default function SortableCell({ column }: SortableCellProps) {
-  const { style, ...restColumn } = column.getHeaderProps();
-  const { attributes, listeners, setNodeRef, transform, transition, isDragging } = useSortable({
-    id: column.id,
-  });
-
-  // build drag styles
-  const dragStyle = {
-    ...style,
-    transform: CSS.Translate.toString(transform),
-    transition,
-  };
-
-  return (
-    <th {...restColumn} ref={setNodeRef} style={dragStyle} className={isDragging ? styles.dragging : ''}>
-      <div {...attributes} {...listeners}>
-        <Tooltip label={column.Header} openDelay={tooltipDelayFast}>
-          {column.render('Header')}
-        </Tooltip>
-      </div>
-      <div {...column.getResizerProps()} className={styles.resizer} />
-    </th>
-  );
-}
-
-interface DraggableColumnHeaderProps {
   header: Header<OntimeRundownEntry, unknown>;
-  style: any;
+  style: CSSProperties;
   children: ReactNode;
 }
 
-export function DraggableColumnHeader({ header, style, children }: DraggableColumnHeaderProps) {
+export function SortableCell({ header, style, children }: SortableCellProps) {
   const { column, colSpan } = header;
 
   const { attributes, listeners, setNodeRef, transform, transition, isDragging } = useSortable({
@@ -60,7 +29,7 @@ export function DraggableColumnHeader({ header, style, children }: DraggableColu
     transition,
   };
 
-  const resizerClasses = cx([styles.resizer, header.column.getIsResizing() ? style.isResizing : null]);
+  const resizerClasses = cx([styles.resizer, header.column.getIsResizing() ? styles.isResizing : null]);
 
   return (
     <th ref={setNodeRef} style={dragStyle} colSpan={colSpan}>
