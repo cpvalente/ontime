09ed7740d92e7a06eb4912491a52975c0e96707e
cv <34649812+cpvalente@users.noreply.github.com>
2023-07-12 20:14:51 +0200

    refactor: create shared playback validation

--- packages/utils/index.ts
+++ packages/utils/index.ts
@@ -3,3 +3,4 @@ export { formatFromMillis } from './src/date-utils/formatFromMillis.js';
 export { isTimeString } from './src/date-utils/isTimeString.js';
 export { millisToString } from './src/date-utils/millisToString.js';
 export { generateId } from './src/generate-id/generateId.js';
+export { validatePlayback } from './src/validate-action/validatePlayback.js';
