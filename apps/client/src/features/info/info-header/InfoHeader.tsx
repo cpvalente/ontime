e49918e8bbf7ee2ddf3f3be248192a9d92bb1675
Carlos Valente <34649812+cpvalente@users.noreply.github.com>
2023-07-23 15:41:58 +0200

    style: show event title in info panel (#467)

---
+++ apps/client/src/features/info/info-header/InfoHeader.tsx
@@ -0,0 +1,14 @@
+import useEventData from '../../../common/hooks-query/useEventData';
+
+import style from '../Info.module.scss';
+
+export default function InfoHeader({ selected }: { selected: string }) {
+  const { data } = useEventData();
+
+  return (
+    <div className={style.panelHeader}>
+      <span className={style.title}>{data?.title || ''}</span>
+      <span className={style.selected}>{selected}</span>
+    </div>
+  );
+}
