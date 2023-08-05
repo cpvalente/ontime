2f299f5d430f3435b9c453ec4744d978c032790e
Zadat O <bundayy@outlook.com>
2023-08-05 19:39:23 +0100

    update the alias route functions

--- apps/client/src/common/utils/aliases.ts
+++ apps/client/src/common/utils/aliases.ts
@@ -31,40 +31,47 @@ export const validateAlias = (alias: string) => {
   return valid;
 };
 
+/**
+ * Gets the URL to send an alias to
+ * @param location
+ * @param data
+ * @param searchParams
+ */
 export const getAliasRoute = (location: Location, data: Alias[], searchParams: URLSearchParams) => {
   const currentURL = location.pathname.substring(1);
-  let redirectURL = '';
   // we need to check if the whole url here is an alias, so we can redirect
   const foundAlias = data.filter((d) => d.alias === currentURL && d.enabled)[0];
   if (foundAlias) {
-    redirectURL = generateURLFromAlias(foundAlias);
-    return redirectURL;
-  } else {
-    for (const d of data) {
-      const aliasOnPage = searchParams.get('alias');
-      if (aliasOnPage) {
-        // if the alias fits the alias on this page, but the URL is diferent, we redirect user to the new URL
-        // if we have the same alias and its enabled and its not empty
-        if (d.alias !== '' && d.enabled && d.alias === aliasOnPage) {
-          const newAliasPath = resolvePath(d.pathAndParams);
-          const urlParams = new URLSearchParams(newAliasPath.search);
-          urlParams.set('alias', d.alias);
-          // we confirm either the url parameters does not match or the url path doesnt
-          if (!isEqual(urlParams, searchParams) || newAliasPath.pathname !== location.pathname) {
-            // we then redirect to the alias route, since the view listening to this alias has an outdated URL
-            redirectURL = `${newAliasPath.pathname}?${urlParams.toString()}`;
-            return redirectURL;
-          }
+    return generateURLFromAlias(foundAlias);
+  }
+  const aliasOnPage = searchParams.get('alias');
+  for (const d of data) {
+    if (aliasOnPage) {
+      // if the alias fits the alias on this page, but the URL is diferent, we redirect user to the new URL
+      // if we have the same alias and its enabled and its not empty
+      if (d.alias !== '' && d.enabled && d.alias === aliasOnPage) {
+        const newAliasPath = resolvePath(d.pathAndParams);
+        const urlParams = new URLSearchParams(newAliasPath.search);
+        urlParams.set('alias', d.alias);
+        // we confirm either the url parameters does not match or the url path doesnt
+        if (!isEqual(urlParams, searchParams) || newAliasPath.pathname !== location.pathname) {
+          // we then redirect to the alias route, since the view listening to this alias has an outdated URL
+          return `${newAliasPath.pathname}?${urlParams}`;
         }
       }
     }
   }
-  return redirectURL;
+  return null;
 };
-const generateURLFromAlias = (aliasData: Alias) => {
+
+/**
+ * Generate URL from an alias
+ * @param aliasData
+ */
+export const generateURLFromAlias = (aliasData: Alias) => {
   const newAliasPath = resolvePath(aliasData.pathAndParams);
   const urlParams = new URLSearchParams(newAliasPath.search);
   urlParams.set('alias', aliasData.alias);
 
-  return `${newAliasPath.pathname}?${urlParams.toString()}`;
+  return `${newAliasPath.pathname}?${urlParams}`;
 };
