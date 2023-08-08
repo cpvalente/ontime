354f764a095abf2ab0d7045108c6615f28c1af55
cv <34649812+cpvalente@users.noreply.github.com>
2023-08-08 18:25:53 +0200

    refactor: extract and simplify progress bar logic

--- apps/client/src/features/rundown/event-block/composite/EventBlockProgressBar.tsx
+++ apps/client/src/features/rundown/event-block/composite/EventBlockProgressBar.tsx
@@ -1,4 +1,4 @@
-import { Playback } from 'ontime-types';
+import { MaybeNumber, Playback } from 'ontime-types';
 
 import { useTimer } from '../../../../common/hooks/useSocket';
 import { clamp } from '../../../../common/utils/math';
@@ -9,18 +9,26 @@ interface EventBlockProgressBarProps {
   playback?: Playback;
 }
 
+export function getPercentComplete(remaining: MaybeNumber, total: MaybeNumber): number {
+  if (remaining === null || total === null) {
+    return 0;
+  }
+
+  if (remaining <= 0) {
+    return 100;
+  }
+
+  if (remaining === total) {
+    return 0;
+  }
+
+  return clamp(100 - (remaining * 100) / total, 0, 100);
+}
+
 export default function EventBlockProgressBar(props: EventBlockProgressBarProps) {
   const { playback } = props;
   const timer = useTimer();
 
-  const now = Math.floor(Math.max((timer?.current ?? 1) / 1000, 0));
-  const complete = (timer?.duration ?? 1) / 1000;
-  const elapsed = clamp(100 - (now * 100) / complete, 0, 100);
-  const progress = `${elapsed}%`;
-
-  if ((timer?.current ?? 0) < 0) {
-    return <div className={`${style.progressBar} ${style.overtime}`} style={{ width: '100%' }} />;
-  }
-
+  const progress = `${getPercentComplete(timer.current, timer.duration)}%`;
   return <div className={`${style.progressBar} ${playback ? style[playback] : ''}`} style={{ width: progress }} />;
 }
