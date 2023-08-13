617f107fb19e830ab5d5c051580465350f8fb3f3
asharonbaltazar <asharonbaltazar@outlook.com>
2023-08-13 17:30:26 -0400

    create `swapOntimeEvents` and export in `index.ts`

--- packages/utils/src/rundown-utils/rundownUtils.ts
+++ packages/utils/src/rundown-utils/rundownUtils.ts
@@ -1,3 +1,5 @@
+import { OntimeEvent, OntimeRundown } from 'ontime-types';
+
 import { dayInMs } from '../timeConstants.js';
 
 /**
@@ -12,4 +14,44 @@ export const calculateDuration = (timeStart: number, timeEnd: number): number =>
     return timeEnd + dayInMs - timeStart;
   }
   return timeEnd - timeStart;
+};
+
+/**
+ * @description swaps two OntimeEvents in the rundown
+ * @param {OntimeRundown} rundown
+ * @param {number} fromEventIndex
+ * @param {number} toEventIndex
+ * @returns {OntimeRundown}
+ */
+export const swapOntimeEvents = (
+  rundown: OntimeRundown,
+  fromEventIndex: number,
+  toEventIndex: number,
+): OntimeRundown => {
+  const updatedRundown = [...rundown];
+
+  if (fromEventIndex < 0 || toEventIndex < 0) {
+    throw new Error('ID not found at index');
+  }
+
+  const fromEvent = updatedRundown.at(fromEventIndex) as OntimeEvent;
+  const toEvent = updatedRundown.at(toEventIndex) as OntimeEvent;
+
+  updatedRundown[fromEventIndex] = {
+    ...toEvent,
+    timeStart: fromEvent.timeStart,
+    timeEnd: fromEvent.timeEnd,
+    duration: fromEvent.duration,
+    delay: fromEvent.delay,
+  };
+
+  updatedRundown[toEventIndex] = {
+    ...fromEvent,
+    timeStart: toEvent.timeStart,
+    timeEnd: toEvent.timeEnd,
+    duration: toEvent.duration,
+    delay: toEvent.delay,
+  };
+
+  return updatedRundown;
 };
