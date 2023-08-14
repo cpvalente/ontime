d19c18f0cc2fa7ac0022842c02123c6a4d229be0
cv <34649812+cpvalente@users.noreply.github.com>
2023-08-14 20:30:48 +0200

    chore: update tests

--- packages/utils/src/rundown-utils/rundownUtils.test.ts
+++ packages/utils/src/rundown-utils/rundownUtils.test.ts
@@ -83,46 +83,8 @@ describe('calculateDuration()', () => {
   describe('Handles edge cases', () => {
     it('handles events that go over midnight', () => {
       const duration = calculateDuration(51, 50);
-      expect(duration).not.toBe(-50);
       expect(duration).toBe(dayInMs - 1);
     });
-    it('when both are equal', () => {
-      const testStart = 1;
-      const testEnd = 1;
-      const val = calculateDuration(testStart, testEnd);
-      expect(val).toBe(testEnd - testStart);
-    });
-
-    it('handles no difference', () => {
-      const duration1 = calculateDuration(0, 0);
-      const duration2 = calculateDuration(dayInMs, dayInMs);
-      expect(duration1).toBe(0);
-      expect(duration2).toBe(0);
-    });
-  });
-});
-
-describe('calculateDuration()', () => {
-  describe('Given start and end values', () => {
-    it('is the difference between end and start', () => {
-      const duration = calculateDuration(10, 20);
-      expect(duration).toBe(10);
-    });
-  });
-
-  describe('Handles edge cases', () => {
-    it('handles events that go over midnight', () => {
-      const duration = calculateDuration(51, 50);
-      expect(duration).not.toBe(-50);
-      expect(duration).toBe(dayInMs - 1);
-    });
-    it('when both are equal', () => {
-      const testStart = 1;
-      const testEnd = 1;
-      const val = calculateDuration(testStart, testEnd);
-      expect(val).toBe(testEnd - testStart);
-    });
-
     it('handles no difference', () => {
       const duration1 = calculateDuration(0, 0);
       const duration2 = calculateDuration(dayInMs, dayInMs);
