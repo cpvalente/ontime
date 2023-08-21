40562885c63ac14ba2a361aebca64eccb65bd3d4
asharonbaltazar <asharonbaltazar@outlook.com>
2023-08-21 16:01:10 -0400

    change `editId` to an array of `idsToEdit`; add `editMode` property

--- apps/client/src/common/stores/appModeStore.ts
+++ apps/client/src/common/stores/appModeStore.ts
@@ -5,66 +5,44 @@ export enum AppMode {
   Edit = 'edit',
 }
 
+export enum EditMode {
+  Range = 'range',
+  Singe = 'single',
+  None = 'none',
+}
+
 const appModeKey = 'ontime-app-mode';
 
 function getModeFromSession() {
   return localStorage.getItem(appModeKey) === AppMode.Run ? AppMode.Run : AppMode.Edit;
 }
 
-async function persistModeToSession(mode: AppMode) {
+function persistModeToSession(mode: AppMode) {
   localStorage.setItem(appModeKey, mode);
 }
 
 type AppModeStore = {
   mode: AppMode;
   cursor: string | null;
-  editId: string | null;
+  editMode: EditMode;
+  idsToEdit: string[];
   setMode: (mode: AppMode) => void;
-  setCursor: (id: string | null, isEditable?: boolean) => void;
-  setEditId: (id: string | null) => void;
+  setIdsToEdit: (id: string[]) => void;
+  clearIdsToEdit: () => void;
 };
 
 export const useAppMode = create<AppModeStore>()((set) => ({
   mode: getModeFromSession(),
   cursor: null,
-  editId: null,
-  setMode: (mode: AppMode) =>
-    set((state) => {
-      persistModeToSession(mode);
-      return mode === AppMode.Edit
-        ? {
-            editId: state.cursor,
-            mode: mode,
-          }
-        : {
-            editId: null,
-            mode: mode,
-          };
-    }),
-  setCursor: (id: string | null, isEditable?: boolean) =>
-    set((state) => {
-      if (isEditable) {
-        return state.mode === AppMode.Edit
-          ? {
-              cursor: id,
-              editId: id,
-            }
-          : {
-              cursor: id,
-            };
-      } else {
-        return { cursor: id, editId: null };
-      }
-    }),
-  setEditId: (id: string | null) =>
-    set((state) => {
-      return state.mode === AppMode.Edit
-        ? {
-            cursor: id,
-            editId: id,
-          }
-        : {
-            editId: id,
-          };
-    }),
+  editMode: EditMode.None,
+  idsToEdit: [],
+  setMode: (mode: AppMode) => {
+    persistModeToSession(mode);
+
+    return set(() => {
+      return { mode };
+    });
+  },
+  setIdsToEdit: (ids) => set(() => ({ idsToEdit: ids })),
+  clearIdsToEdit: () => set(() => ({ idsToEdit: [] })),
 }));
