edfe9a5c6d136ba492595cb24775d01886ba611e
arc-alex <ac@omnivox.dk>
2023-09-08 16:02:48 +0200

    add load function

--- apps/server/src/services/dumpService.ts
+++ apps/server/src/services/dumpService.ts
@@ -3,9 +3,24 @@ import { logger } from '../classes/Logger.js';
 import fs from 'fs/promises';
 import path from 'path';
 
-let lastStore: Partial<RuntimeStore> = {};
 
-export async function dump(store: Partial<RuntimeStore>) {
-    logger.info('DUMP', path.normalize(__dirname + '/../../dump'));
-    fs.writeFile(path.normalize(__dirname + '/../../dump/test.json'), JSON.stringify(store), 'utf-8')
+export async function dump(store: Partial<RuntimeStore>, dumpPath: string = __dirname + '/../../dump/test.json'): Promise<void> {
+    fs.writeFile(path.normalize(dumpPath), JSON.stringify(store), 'utf-8').
+        catch((err) => { logger.error('DUMP', 'failde to dump state, ' + err) });
+}
+
+export async function load(dumpPath: string = __dirname + '/../../dump/test.json'): Promise<Partial<RuntimeStore>> {
+    let data = await fs.readFile(path.normalize(dumpPath), 'utf-8');
+    try {
+        let s: Partial<RuntimeStore> = JSON.parse(data);
+        return s;
+    } catch (err) {
+        logger.info('DUMP', 'fFaild to parse state file, ' + err);
+    }
+    return null;
+    //         .catch ((err) => {
+    //     logger.info('DUMP', 'unable load to dump state file, ' + err);
+    //     return null;
+    // });
+
 }
