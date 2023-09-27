30905757f38d9106afb349d128253cd529877581
cv <34649812+cpvalente@users.noreply.github.com>
2023-09-27 14:31:14 +0200

    chore: increase size limits on uploads

--- apps/client/src/features/modals/upload-modal/uploadUtils.ts
+++ apps/client/src/features/modals/upload-modal/uploadUtils.ts
@@ -10,9 +10,15 @@ export function validateFile(file: File): ValidationStatus {
     status.isValid = false;
   }
 
-  // Limit file size to 1MB
-  if (file.size > 1000000) {
+  // Limit file size of a project file to around 1MB
+  if (file.name.endsWith('.json') && file.size > 1_000_000) {
     status.errors.push('File size limit (1MB) exceeded');
+    status.isValid = false;
+  }
+
+  // Limit file size of an excel file to around 10MB
+  if (file.name.endsWith('.xlsx') && file.size > 10_000_000) {
+    status.errors.push('File size limit (10MB) exceeded');
     status.isValid = false;
   }
 
@@ -23,8 +29,6 @@ export function validateFile(file: File): ValidationStatus {
   }
   return status;
 }
-
-export type MaybeFile = null | 'ontime' | 'excel';
 
 export function isExcelFile(file: File | null) {
   return file?.name.endsWith('.xlsx');
