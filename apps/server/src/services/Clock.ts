88ab42880134e09de55a7d1ade481a7f1a263e24
arc-alex <ac@omnivox.dk>
2023-10-28 18:03:11 +0200

    first test

--- apps/server/src/services/Clock.ts
+++ apps/server/src/services/Clock.ts
@@ -1,3 +1,5 @@
+import easymidi from 'easymidi';
+
 enum Source {
   System = 'system',
   MIDI = 'MIDI',
@@ -6,6 +8,19 @@ enum Source {
 class Clock {
   private static instance: Clock;
   private readonly source: Source;
+  private readonly midiIn;
+  private midiState = {
+    rate: 0,
+    hourMsbit: 0,
+    hourLsbits: 0,
+    minuteMsbits: 0,
+    minuteLsbits: 0,
+    secondMsbits: 0,
+    secondLsbits: 0,
+    frameMsbit: 0,
+    frameLsbits: 0,
+    total: 0,
+  };
 
   constructor(source?: Source) {
     if (Clock.instance) {
@@ -15,6 +30,51 @@ class Clock {
     Clock.instance = this;
 
     this.source = source || Source.System;
+    if (source === Source.MIDI) {
+      this.midiIn = new easymidi.Input('loopMIDI Port');
+      this.midiIn.on('mtc', (mtc) => {
+        switch (mtc.type) {
+          case 0: {
+            this.midiState.frameLsbits = mtc.value & 0b00001111;
+            this.midiState.total =
+              ((this.midiState.frameMsbit + this.midiState.frameLsbits) / this.midiState.rate) * 1000 +
+              (this.midiState.secondMsbits + this.midiState.secondLsbits) * 1000 +
+              (this.midiState.minuteMsbits + this.midiState.minuteLsbits) * 60000 +
+              (this.midiState.hourMsbit + this.midiState.hourLsbits) * 3600000;
+            break;
+          }
+          case 1: {
+            this.midiState.frameMsbit = (mtc.value & 0b00000001) << 4;
+            break;
+          }
+          case 2: {
+            this.midiState.secondLsbits = mtc.value & 0b00001111;
+            break;
+          }
+          case 3: {
+            this.midiState.secondMsbits = (mtc.value & 0b00000011) << 4;
+            break;
+          }
+          case 4: {
+            this.midiState.minuteLsbits = mtc.value & 0b00001111;
+            break;
+          }
+          case 5: {
+            this.midiState.minuteMsbits = (mtc.value & 0b00000011) << 4;
+            break;
+          }
+          case 6: {
+            this.midiState.hourLsbits = mtc.value & 0b00001111;
+            break;
+          }
+          case 7: {
+            this.midiState.hourMsbit = (mtc.value & 0b00000001) << 4;
+            this.midiState.rate = [24, 25, 29.97, 30][(mtc.value >> 1) & 0b00000011];
+            break;
+          }
+        }
+      });
+    }
   }
 
   /**
@@ -25,7 +85,6 @@ class Clock {
       case Source.System:
         return this.getSystemTime();
       case Source.MIDI:
-        // @ts-expect-error -- not implemented
         return this.getMidiTime();
       default:
         throw new Error('Invalid time source');
@@ -50,8 +109,11 @@ class Clock {
    * Get current time from MIDI
    */
   getMidiTime() {
-    throw new Error('Not implemented');
+    return this.midiState.total;
+    // return this.getSystemTime();
+
+    // throw new Error('Not implemented');
   }
 }
 
-export const clock = new Clock();
+export const clock = new Clock(Source.MIDI);
