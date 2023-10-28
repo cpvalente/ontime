22ee644da2645693e86c0c37257d768b5379749b
cv <34649812+cpvalente@users.noreply.github.com>
2023-10-28 20:11:42 +0200

    chore: update dependencies

--- apps/server/vite.config.ts
+++ apps/server/vite.config.ts
@@ -3,5 +3,8 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     globals: true,
+    typecheck: {
+      tsconfig: './tsconfig.test.json',
+    },
   },
 });
