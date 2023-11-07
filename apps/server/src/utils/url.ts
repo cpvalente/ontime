4ea7f2857949d8bda999f0743c26b42906be5c52
cv <34649812+cpvalente@users.noreply.github.com>
2023-11-07 20:18:42 +0100

    chore: update test

--- apps/server/src/utils/url.ts
+++ apps/server/src/utils/url.ts
@@ -11,16 +11,7 @@ export const cleanURL = (url: string): string => {
   sanitised = sanitised.split(' ').join('%20');
 
   // contain only allowed characters
-  sanitised = sanitised.replace(/%40/g, '@');
-  sanitised = sanitised.replace(/%5B/g, '[');
-  sanitised = sanitised.replace(/%5D/g, ']');
-  sanitised = sanitised.replace(/%7B/g, '{');
-  sanitised = sanitised.replace(/%7D/g, '}');
-  sanitised = sanitised.replace(/%7C/g, '|');
-  sanitised = sanitised.replace(/%5C/g, '\\');
-  sanitised = sanitised.replace(/%5E/g, '^');
-  sanitised = sanitised.replace(/%60/g, '`');
-  sanitised = sanitised.replace(/%20/g, '+');
+  sanitised = sanitised.replace(/([@\s<>[\]{}|\\^])+/g, '');
 
   // starts with http://
   if (!sanitised.startsWith('http://')) sanitised = `http://${sanitised}`;
