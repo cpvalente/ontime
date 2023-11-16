bd2aee5f92ad858dfcddcbb1bf14df490c1a198b
Carlos Valente <carlosvalente@pm.me>
2023-11-16 22:11:51 +0100

    refactor: optimistic mutations

--- apps/client/src/common/hooks-query/useRundown.ts
+++ apps/client/src/common/hooks-query/useRundown.ts
@@ -1,38 +1,31 @@
 import { useQuery } from '@tanstack/react-query';
-import { GetRundownCached, OntimeRundown } from 'ontime-types';
+import { GetRundownCached } from 'ontime-types';
 
 import { queryRefetchInterval } from '../../ontimeConfig';
-import { RUNDOWN_TABLE } from '../api/apiConstants';
+import { RUNDOWN } from '../api/apiConstants';
 import { fetchCachedRundown } from '../api/eventsApi';
 
 const cachedRundownPlaceholder = { rundown: [], revision: -1 };
 
+// TODO: can we leverage structural sharing to see if data has changed?
 export default function useRundown() {
   const { data, status, isError, refetch } = useQuery<GetRundownCached>({
-    queryKey: RUNDOWN_TABLE,
+    queryKey: RUNDOWN,
     queryFn: fetchCachedRundown,
     placeholderData: cachedRundownPlaceholder,
     retry: 5,
+    select: (data) => data.rundown,
     retryDelay: (attempt) => attempt * 2500,
     refetchInterval: queryRefetchInterval,
     networkMode: 'always',
-    structuralSharing: (oldData: GetRundownCached | undefined, newData: GetRundownCached) => {
-      if (oldData === undefined) {
-        cachedRundownPlaceholder;
-      }
-      const hasDataChanged = oldData?.revision === newData.revision;
-      return hasDataChanged ? oldData : newData;
-    },
+    // structuralSharing: (oldData: GetRundownCached | undefined, newData: GetRundownCached) => {
+    //   if (oldData === undefined) {
+    //     cachedRundownPlaceholder;
+    //   }
+    //   const hasDataChanged = oldData?.revision === newData.revision;
+    //   return hasDataChanged ? oldData : newData;
+    // },
   });
 
-  const rundown: OntimeRundown = data?.rundown ?? [];
-
-  return { data: rundown, status, isError, refetch };
+  return { data, status, isError, refetch };
 }
-
-/**
-
-
- + structuralSharing: (oldData, newData) => customCheck(oldData, newData) ? oldData : replaceEqualDeep(oldData, newData)
-
- */
