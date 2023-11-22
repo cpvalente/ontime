7eea57c55ad0b53928c05783a01f904976c8a77f
asharonbaltazar <asharonbaltazar@outlook.com>
2023-11-22 18:59:10 -0500

    use `defaultValue`s in `ParamInput`

--- apps/client/src/common/components/view-params-editor/ParamInput.tsx
+++ apps/client/src/common/components/view-params-editor/ParamInput.tsx
@@ -11,14 +11,19 @@ interface EditFormInputProps {
 
 export default function ParamInput({ paramField }: EditFormInputProps) {
   const [searchParams] = useSearchParams();
-  const { id, type } = paramField;
+  const { id, type, defaultValue } = paramField;
 
   if (type === 'option') {
     const optionFromParams = searchParams.get(id);
-    const defaultOptionValue = optionFromParams || undefined;
+    const defaultOptionValue = optionFromParams || defaultValue;
 
     return (
-      <Select placeholder='Select an option' variant='ontime' name={id} defaultValue={defaultOptionValue}>
+      <Select
+        placeholder={defaultValue ? undefined : 'Select an option'}
+        variant='ontime'
+        name={id}
+        defaultValue={defaultOptionValue}
+      >
         {Object.entries(paramField.values).map(([key, value]) => (
           <option key={key} value={key}>
             {value}
@@ -36,12 +41,12 @@ export default function ParamInput({ paramField }: EditFormInputProps) {
   }
 
   if (type === 'number') {
-    const defaultNumberValue = searchParams.get(id) ?? '';
+    const defaultNumberValue = searchParams.get(id) ?? defaultValue;
 
     return <Input type='number' step='any' variant='ontime-filled' name={id} defaultValue={defaultNumberValue} />;
   }
 
-  const defaultStringValue = searchParams.get(id) ?? '';
+  const defaultStringValue = searchParams.get(id) ?? defaultValue;
 
   return <Input variant='ontime-filled' name={id} defaultValue={defaultStringValue} />;
 }
