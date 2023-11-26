e5014c16537fb96f244be4bc2d1f2751b46050f6
Ary <arylmoraesn@gmail.com>
2023-11-26 11:53:16 -0700

    default db loading

--- apps/server/src/setup.ts
+++ apps/server/src/setup.ts
@@ -80,8 +80,8 @@ export const resolveDbDirectory = join(
 export const resolveDbPath = join(resolveDbDirectory, config.database.filename);
 
 export const pathToStartDb = isTest
-  ? join(currentDirectory, '../', config.database.testdb, config.database.filename)
-  : join(currentDirectory, '/preloaded-db/', config.database.filename);
+  ? join(currentDirectory, '../', config.database.testdb, 'db.json')
+  : join(currentDirectory, '/preloaded-db/', 'db.json');
 
 // path to public styles
 export const resolveStylesDirectory = join(externalsStartDirectory, config.styles.directory);
