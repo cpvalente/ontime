a15c28c5fd2b0627ef016009c25094d537af3f32
arc-alex <ac@omnivox.dk>
2023-12-07 13:15:46 +0100

    add test for getAccessibleColour

--- apps/client/src/common/utils/__tests__/styleUtils.test.ts
+++ apps/client/src/common/utils/__tests__/styleUtils.test.ts
@@ -1,4 +1,4 @@
-import { cx } from '../styleUtils';
+import { cx, getAccessibleColour } from '../styleUtils';
 
 import style from './styleUtils.module.scss';
 
@@ -11,5 +11,26 @@ describe('cx()', () => {
     const falsyStuff = false;
     const merged = cx([undefined, false, 0, null, falsyStuff ? style.test : null]);
     expect(merged).toMatchSnapshot();
+  });
+});
+
+describe('getAccessibleColour()', () => {
+  it('handles named colours', () => {
+    const colour = 'red';
+    const { backgroundColor, color } = getAccessibleColour(colour);
+    expect(backgroundColor).toBe('#FF0000FF');
+    expect(color).toBe('#fffffa');
+  });
+  it('handles hex colours', () => {
+    const colour = '#0F0';
+    const { backgroundColor, color } = getAccessibleColour(colour);
+    expect(backgroundColor).toBe('#00FF00FF');
+    expect(color).toBe('black');
+  });
+  it('handles transparens', () => {
+    const colour = '#0F08';
+    const { backgroundColor, color } = getAccessibleColour(colour);
+    expect(backgroundColor).toBe('#0C940CFF');
+    expect(color).toBe('#fffffa');
   });
 });
