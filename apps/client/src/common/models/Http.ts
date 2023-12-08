1164975c91575301b88aa33aa6728dde0cf757f3
arc-alex <ac@omnivox.dk>
2023-12-08 17:14:03 +0100

    missing retryCount in httpPlaceholder

--- apps/client/src/common/models/Http.ts
+++ apps/client/src/common/models/Http.ts
@@ -2,6 +2,7 @@ import { HttpSettings } from 'ontime-types';
 
 export const httpPlaceholder: HttpSettings = {
   enabledOut: false,
+  retryCount: 0,
   subscriptions: {
     onLoad: [],
     onStart: [],
