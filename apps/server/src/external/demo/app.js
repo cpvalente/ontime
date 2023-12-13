87d428c0608d0410a5021fed819eb90481c3a24b
arc-alex <ac@omnivox.dk>
2023-12-13 12:10:58 +0100

    dynamic port, reconnect socket, msTo string without Date

--- apps/server/src/external/demo/app.js
+++ apps/server/src/external/demo/app.js
@@ -1,39 +1,71 @@
+/*eslint-env browser*/
 /**
  * This is a very minimal example for a websocket client
  * You could use this as a starting point to creating your own interfaces
  *
  * This example does not handle disconnections
  */
-/* eslint-disable */
 
-// this would need to resolve to the hostname of where ontime is running
-const socket = new WebSocket(`ws://${window.location.hostname}:4001/ws`);
-console.log(`ws://${window.location.hostname}:4001/ws`);
+const mts = 1000; // millis to seconds
+const mtm = 1000 * 60; // millis to minutes
+const mth = 1000 * 60 * 60; // millis to hours
 
-function leftPad(number) {
-  return number.toString().padStart(2, '0');
-}
+const leftPad = (number) => {
+  return Math.floor(number).toString().padStart(2, '0');
+};
 
-socket.addEventListener('message', (event) => {
-  const data = JSON.parse(event.data);
+let reconnectTimeout;
+const reconnectInterval = 1000;
+let reconnectAttempts = 0;
 
-  // all objects from ontime are structured with type and payload
-  const { type, payload } = data;
+console.log(window.location)
 
-  // we only need to read message type of ontime
-  if (type === 'ontime') {
-    // destructure known data from ontime
-    // see https://cpvalente.gitbook.io/ontime/control-and-feedback/websocket-api
-    const { timer, playback } = payload;
-    console.log('Got message from ontime:', timer);
-    const timerElement = document.getElementById('timer');
-    if (playback == 'stop') {
-      timerElement.innerText = '--:--:--';
-    } else {
-      const now = new Date(timer.current);
-      timerElement.innerText = `${leftPad(now.getUTCHours())}:${leftPad(now.getMinutes())}:${leftPad(
-        now.getSeconds(),
-      )}`;
+const connectSocket = () => {
+  const websocket = new WebSocket(`ws://${window.location.hostname}:${window.location.port}/ws`);
+
+  websocket.onopen = () => {
+    clearTimeout(reconnectTimeout);
+    reconnectAttempts = 0;
+    console.info('WebSocket connected');
+  };
+
+  websocket.onclose = () => {
+    console.warn('WebSocket disconnected');
+    reconnectTimeout = setTimeout(() => {
+      console.warn(`WebSocket: attempting reconnect ${reconnectAttempts}`);
+      if (websocket && websocket.readyState === WebSocket.CLOSED) {
+        reconnectAttempts += 1;
+        connectSocket();
+      }
+    }, reconnectInterval);
+  };
+  websocket.onerror = (error) => {
+    console.error('WebSocket error:', error);
+  };
+
+  websocket.onmessage = (event) => {
+    const data = JSON.parse(event.data);
+
+    // all objects from ontime are structured with type and payload
+    const { type, payload } = data;
+
+    // we only need to read message type of ontime
+    if (type === 'ontime') {
+      // destructure known data from ontime
+      // see https://cpvalente.gitbook.io/ontime/control-and-feedback/websocket-api
+      const { timer, playback } = payload;
+      const timerElement = document.getElementById('timer');
+      if (playback == 'stop') {
+        timerElement.innerText = '--:--:--';
+      } else {
+        const millis = Math.abs(timer.current);
+        const isNegative = timer.current < 0;
+        timerElement.innerText = `${isNegative ? '-' : ''}${leftPad(millis / mth)}:${leftPad(
+          (millis % mth) / mtm,
+        )}:${leftPad((millis % mtm) / mts)}`;
+      }
     }
-  }
-});
+  };
+};
+
+connectSocket();
