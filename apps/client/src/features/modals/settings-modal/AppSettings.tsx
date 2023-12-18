26a3991128d21047c854a8492082cd41df98439f
Carlos Valente <34649812+cpvalente@users.noreply.github.com>
2023-12-18 15:07:48 +0100

    chore: add italian to languages (#656)

--- apps/client/src/features/modals/settings-modal/AppSettings.tsx
+++ apps/client/src/features/modals/settings-modal/AppSettings.tsx
@@ -119,6 +119,7 @@ export default function AppSettingsModal() {
           <option value='en'>English</option>
           <option value='fr'>French</option>
           <option value='de'>German</option>
+          <option value='it'>Italian</option>
           <option value='no'>Norwegian</option>
           <option value='pt'>Portuguese</option>
           <option value='es'>Spanish</option>
