7aa942a14a9443076d9706203ed08185ebf2cde1
Carlos Valente <carlosvalente@pm.me>
2023-12-27 19:38:58 +0100

    refactor: migrate logs

--- apps/client/src/features/info/Info.tsx
+++ apps/client/src/features/info/Info.tsx
@@ -1,15 +1,11 @@
 import { useInfoPanel } from '../../common/hooks/useSocket';
-import { useEditorSettings } from '../../common/stores/editorSettings';
 
 import InfoHeader from './info-header/InfoHeader';
 import CollapsableInfo from './CollapsableInfo';
-import InfoLogger from './InfoLogger';
-import InfoNif from './InfoNif';
 import InfoTitles from './InfoTitles';
 
 export default function Info() {
   const data = useInfoPanel();
-  const showNif = useEditorSettings((state) => state.eventSettings.showNif);
 
   const titlesNow = {
     title: data.eventNow?.title || '',
@@ -34,19 +30,11 @@ export default function Info() {
   return (
     <>
       <InfoHeader selected={selected} />
-      {showNif && (
-        <CollapsableInfo title='Network Info'>
-          <InfoNif />
-        </CollapsableInfo>
-      )}
       <CollapsableInfo title='Playing Now'>
         <InfoTitles data={titlesNow} />
       </CollapsableInfo>
       <CollapsableInfo title='Playing Next'>
         <InfoTitles data={titlesNext} />
-      </CollapsableInfo>
-      <CollapsableInfo title='Log'>
-        <InfoLogger />
       </CollapsableInfo>
     </>
   );
