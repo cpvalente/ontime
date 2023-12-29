4525e42730cbdef6ed9500b7e290b4942a66aebb
Ary <arylmoraesn@gmail.com>
2023-12-29 12:00:42 -0700

    chore: another wrong import block

--- apps/server/src/utils/getFileListFromFolder.ts
+++ apps/server/src/utils/getFileListFromFolder.ts
@@ -1,7 +1,6 @@
 import { ProjectFile } from 'ontime-types';
 
 import { readdirSync, statSync } from 'fs';
-
 import { extname } from 'path';
 
 interface Options {
@@ -13,12 +12,12 @@ export const getFileListFromFolder = (folderPath: string, options: Options = {})
 
   // Filter files if allowedExtensions is provided
   if (options.allowedExtensions && options.allowedExtensions.length > 0) {
-    files = files.filter(file => {
+    files = files.filter((file) => {
       const ext = extname(file).toLowerCase();
       return options.allowedExtensions.includes(ext);
     });
   }
-  
+
   return files.map((file) => {
     const filePath = `${folderPath}/${file}`;
     const stats = statSync(filePath);
