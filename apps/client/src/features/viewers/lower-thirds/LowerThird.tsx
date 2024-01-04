0c2e692f69536a6628d1f360152d3ad7a9a76c3e
arc-alex <ac@omnivox.dk>
2024-01-04 14:54:58 +0100

    fix: memoize options

--- apps/client/src/features/viewers/lower-thirds/LowerThird.tsx
+++ apps/client/src/features/viewers/lower-thirds/LowerThird.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import { Message, OntimeEvent, ViewSettings } from 'ontime-types';
 
@@ -47,26 +47,113 @@ interface LowerProps {
   lower: Message;
 }
 
+const defaultOptions: LowerOptions = {
+  width: 45,
+  trigger: triggerType.Event,
+  topSrc: srcKeys.Title,
+  bottomSrc: srcKeys.Subtitle,
+  topColour: '000000ff',
+  bottomColour: '000000ff',
+  topBg: '00000000',
+  bottomBg: '00000000',
+  topSize: '65px',
+  bottomSize: '40px',
+  transition: 3,
+  delay: 3,
+  key: 'ffffffff',
+  lineColour: 'ff0000ff',
+  lineHeight: '0.4em',
+};
+
 export default function LowerThird(props: LowerProps) {
   const { eventNow, lower, viewSettings } = props;
   const [searchParams] = useSearchParams();
-  const options: LowerOptions = {
-    width: 45,
-    trigger: triggerType.Event,
-    topSrc: srcKeys.Title,
-    bottomSrc: srcKeys.Subtitle,
-    topColour: '000000ff',
-    bottomColour: '000000ff',
-    topBg: '00000000',
-    bottomBg: '00000000',
-    topSize: '65px',
-    bottomSize: '40px',
-    transition: 3,
-    delay: 3,
-    key: 'ffffffff',
-    lineColour: 'ff0000ff',
-    lineHeight: '0.4em',
-  };
+
+  const options = useMemo(() => {
+    const newOptions = { ...defaultOptions };
+
+    const _width = searchParams.get('width');
+    if (_width) {
+      const width = Number(_width);
+      if (!Number.isNaN(width) && width != newOptions.width) {
+        newOptions.width = width;
+      }
+    }
+
+    const trigger = searchParams.get('trigger') as triggerType;
+    if (trigger && Object.values(triggerType).includes(trigger) && trigger != newOptions.trigger) {
+      newOptions.trigger = trigger;
+    }
+
+    const _topSrc = searchParams.get('top-src');
+    if (_topSrc && _topSrc in srcKeys && _topSrc != newOptions.topSrc) {
+      const topSrc = _topSrc as srcKeys;
+      newOptions.topSrc = topSrc;
+    }
+
+    const _bottomSrc = searchParams.get('bottom-src');
+    if (_bottomSrc && _bottomSrc in srcKeys && _bottomSrc != newOptions.bottomSrc) {
+      const bottomSrc = _bottomSrc as srcKeys;
+      newOptions.bottomSrc = bottomSrc;
+    }
+
+    const topColour = searchParams.get('top-colour');
+    if (topColour && topColour != newOptions.topColour) {
+      newOptions.topColour = topColour;
+    }
+
+    const bottomColour = searchParams.get('bottom-colour');
+    if (bottomColour && bottomColour != newOptions.bottomColour) {
+      newOptions.bottomColour = bottomColour;
+    }
+
+    const topBg = searchParams.get('top-bg');
+    if (topBg && topBg != newOptions.topBg) {
+      newOptions.topBg = topBg;
+    }
+
+    const bottomBg = searchParams.get('bottom-bg');
+    if (bottomBg && bottomBg != newOptions.bottomBg) {
+      newOptions.bottomBg = bottomBg;
+    }
+
+    const topSize = searchParams.get('top-size');
+    if (topSize && topSize != newOptions.topSize) {
+      newOptions.topSize = topSize;
+    }
+
+    const bottomSize = searchParams.get('bottom-size');
+    if (bottomSize && bottomSize != newOptions.bottomSize) {
+      newOptions.bottomSize = bottomSize;
+    }
+
+    const _transition = searchParams.get('transition');
+    if (_transition) {
+      const transition = Number(_transition);
+      if (!Number.isNaN(transition) && transition != newOptions.transition) {
+        newOptions.transition = transition;
+      }
+    }
+
+    const _delay = searchParams.get('delay');
+    if (_delay) {
+      const delay = Number(_delay);
+      if (!Number.isNaN(delay) && delay != newOptions.delay) {
+        newOptions.delay = delay;
+      }
+    }
+
+    const key = searchParams.get('key');
+    if (key && key != newOptions.key) {
+      newOptions.key = key;
+    }
+
+    const lineColour = searchParams.get('line-colour');
+    if (lineColour && lineColour != newOptions.lineColour) {
+      newOptions.lineColour = lineColour;
+    }
+    return newOptions;
+  }, [searchParams]);
 
   useRuntimeStylesheet(viewSettings?.overrideStyles && overrideStylesURL);
 
@@ -75,87 +162,6 @@ export default function LowerThird(props: LowerProps) {
   useEffect(() => {
     document.title = 'ontime - Lower Third';
   }, []);
-
-  const _width = searchParams.get('width');
-  if (_width) {
-    const width = Number(_width);
-    if (!Number.isNaN(width) && width != options.width) {
-      options.width = width;
-    }
-  }
-
-  const trigger = searchParams.get('trigger') as triggerType;
-  if (trigger && Object.values(triggerType).includes(trigger) && trigger != options.trigger) {
-    options.trigger = trigger;
-  }
-
-  const _topSrc = searchParams.get('top-src');
-  if (_topSrc && _topSrc in srcKeys && _topSrc != options.topSrc) {
-    const topSrc = _topSrc as srcKeys;
-    options.topSrc = topSrc;
-  }
-
-  const _bottomSrc = searchParams.get('bottom-src');
-  if (_bottomSrc && _bottomSrc in srcKeys && _bottomSrc != options.bottomSrc) {
-    const bottomSrc = _bottomSrc as srcKeys;
-    options.bottomSrc = bottomSrc;
-  }
-
-  const topColour = searchParams.get('top-colour');
-  if (topColour && topColour != options.topColour) {
-    options.topColour = topColour;
-  }
-
-  const bottomColour = searchParams.get('bottom-colour');
-  if (bottomColour && bottomColour != options.bottomColour) {
-    options.bottomColour = bottomColour;
-  }
-
-  const topBg = searchParams.get('top-bg');
-  if (topBg && topBg != options.topBg) {
-    options.topBg = topBg;
-  }
-
-  const bottomBg = searchParams.get('bottom-bg');
-  if (bottomBg && bottomBg != options.bottomBg) {
-    options.bottomBg = bottomBg;
-  }
-
-  const topSize = searchParams.get('top-size');
-  if (topSize && topSize != options.topSize) {
-    options.topSize = topSize;
-  }
-
-  const bottomSize = searchParams.get('bottom-size');
-  if (bottomSize && bottomSize != options.bottomSize) {
-    options.bottomSize = bottomSize;
-  }
-
-  const _transition = searchParams.get('transition');
-  if (_transition) {
-    const transition = Number(_transition);
-    if (!Number.isNaN(transition) && transition != options.transition) {
-      options.transition = transition;
-    }
-  }
-
-  const _delay = searchParams.get('delay');
-  if (_delay) {
-    const delay = Number(_delay);
-    if (!Number.isNaN(delay) && delay != options.delay) {
-      options.delay = delay;
-    }
-  }
-
-  const key = searchParams.get('key');
-  if (key && key != options.key) {
-    options.key = key;
-  }
-
-  const lineColour = searchParams.get('line-colour');
-  if (lineColour && lineColour != options.lineColour) {
-    options.lineColour = lineColour;
-  }
 
   const topText = options.topSrc == srcKeys.LowerMsg ? lower.text : eventNow ? eventNow[options.topSrc] : '';
 
