b1ba47a80fd7d8155139f3b83ccc51f102c8d0b8
Alex Christoffer Rasmussen <ac@omnivox.dk>
2024-01-09 22:57:06 +0100

    Fix/external path (#702)
    * leave styles as is but move other external to `external` folder

--- apps/server/src/setup.ts
+++ apps/server/src/setup.ts
@@ -68,7 +68,8 @@ export const currentDirectory = dirname(__dirname);
 
 const testDbStartDirectory = isTest ? '../' : getAppDataPath();
 export const externalsStartDirectory = isProduction ? getAppDataPath() : join(currentDirectory, 'external');
-
+//TODO: we only need one when they are all in the same folder
+export const resolveExternalsDirectory = join(isProduction ? getAppDataPath() : currentDirectory, 'external');
 // path to public db
 export const resolveDbDirectory = join(
   testDbStartDirectory,
@@ -80,11 +81,26 @@ export const pathToStartDb = isTest
   ? join(currentDirectory, '../', config.database.testdb, config.database.filename)
   : join(currentDirectory, '/preloaded-db/', config.database.filename);
 
+//TODO: move all static files to the external directory
 // path to public styles
 export const resolveStylesDirectory = join(externalsStartDirectory, config.styles.directory);
 export const resolveStylesPath = join(resolveStylesDirectory, config.styles.filename);
 
 export const pathToStartStyles = join(currentDirectory, '/external/styles/', config.styles.filename);
+
+// path to public demo
+export const resolveDemoDirectory = join(
+  externalsStartDirectory,
+  isProduction ? '/external/' : '', //move to external folde in production
+  config.demo.directory,
+);
+export const resolveDemoPath = config.demo.filename.map((file) => {
+  return join(resolveDemoDirectory, file);
+});
+
+export const pathToStartDemo = config.demo.filename.map((file) => {
+  return join(currentDirectory, '/external/demo/', file);
+});
 
 // path to restore file
 export const resolveRestoreFile = join(getAppDataPath(), config.restoreFile);
