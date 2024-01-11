0ac94a12d9aac5c5a15e4d5c3984ae3f02356ab6
Carlos Valente <carlosvalente@pm.me>
2024-01-11 21:21:17 +0100

    chore: update tests

--- apps/server/src/classes/simple-timer/__tests__/SimpleTimer.test.ts
+++ apps/server/src/classes/simple-timer/__tests__/SimpleTimer.test.ts
@@ -4,13 +4,12 @@ describe('SimpleTimer', () => {
     let timer: SimpleTimer;
 
     describe('normal timer flow', () => {
-        const emit = vi.fn()
         const initialTime = 1000;
-        timer = new SimpleTimer(emit);
+        timer = new SimpleTimer();
 
         test("setting the timer duration", () => {
-            timer.setTime(initialTime);
-            expect(emit).toHaveBeenCalledWith({
+            const newState = timer.setTime(initialTime);
+            expect(newState).toStrictEqual({
                 duration: initialTime,
                 current: null,
                 playback: 'stop',
@@ -18,8 +17,8 @@ describe('SimpleTimer', () => {
         })
 
         test("setting the timer to play", () => {
-            timer.play({ timeNow: 0 })
-            expect(emit).toHaveBeenCalledWith({
+            const newState = timer.play(0)
+            expect(newState).toStrictEqual({
                 duration: initialTime,
                 current: 0,
                 playback: 'play',
@@ -27,22 +26,22 @@ describe('SimpleTimer', () => {
         })
 
         test("updating the timer", () => {
-            timer.update({ timeNow: 100 })
-            expect(emit).toHaveBeenCalledWith({
+            let newState = timer.update(100)
+            expect(newState).toStrictEqual({
                 duration: initialTime,
                 current: 100,
                 playback: 'play',
             });
 
-            timer.update({ timeNow: 500 })
-            expect(emit).toHaveBeenCalledWith({
+            newState = timer.update(500)
+            expect(newState).toStrictEqual({
                 duration: initialTime,
                 current: 500,
                 playback: 'play',
             });
 
-            timer.update({ timeNow: 1500 })
-            expect(emit).toHaveBeenCalledWith({
+            newState = timer.update(1500)
+            expect(newState).toStrictEqual({
                 duration: initialTime,
                 current: 1500,
                 playback: 'play',
@@ -50,36 +49,36 @@ describe('SimpleTimer', () => {
         })
 
         test("pausing the time doesnt affect the current", () => {
-            timer.pause({ timeNow: 1500 })
-            expect(emit).toHaveBeenCalledWith({
+            let newState = timer.pause(1500)
+            expect(newState).toStrictEqual({
                 duration: initialTime,
                 current: 1500,
                 playback: 'pause',
             });
 
-            timer.update({ timeNow: 1600 })
-            expect(emit).toHaveBeenCalledWith({
+            newState = timer.update(1600)
+            expect(newState).toStrictEqual({
                 duration: initialTime,
                 current: 1500,
                 playback: 'pause',
             });
 
-            timer.update({ timeNow: 1700 })
-            expect(emit).toHaveBeenCalledWith({
+            newState = timer.update(1700)
+            expect(newState).toStrictEqual({
                 duration: initialTime,
                 current: 1500,
                 playback: 'pause',
             });
 
-            timer.play({ timeNow: 1700 })
-            expect(emit).toHaveBeenCalledWith({
+            newState = timer.play(1700)
+            expect(newState).toStrictEqual({
                 duration: initialTime,
                 current: 1500,
                 playback: 'play',
             });
 
-            timer.update({ timeNow: 1800 })
-            expect(emit).toHaveBeenCalledWith({
+            newState = timer.update(1800)
+            expect(newState).toStrictEqual({
                 duration: initialTime,
                 current: 1600,
                 playback: 'play',
@@ -87,8 +86,8 @@ describe('SimpleTimer', () => {
         })
 
         test("stopping the timer clears the running data", () => {
-            timer.stop()
-            expect(emit).toHaveBeenCalledWith({
+            const newState = timer.stop()
+            expect(newState).toStrictEqual({
                 duration: initialTime,
                 current: null,
                 playback: 'stop',
