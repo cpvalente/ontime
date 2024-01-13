5c815ba7189993097b0eaa000f48367876a35bcd
arc-alex <ac@omnivox.dk>
2024-01-13 14:36:47 +0100

    feat: add a way for HTTP to send an nested object

--- apps/server/src/routes/apiRouter.ts
+++ apps/server/src/routes/apiRouter.ts
@@ -14,9 +14,19 @@ router.get('/', (_req, res) => {
 
 // any GET request in /api is sent to the integration controller
 router.get('/*', (req, res) => {
-  const action = req.path.substring(1);
+  let action = req.path.substring(1);
+  const actionArray = action.split('/');
+  
   const params = { payload: req.query };
-
+  
+  //TODO: this can be better
+  if (actionArray.length > 1) {
+    action = actionArray.shift();
+    params.payload = actionArray.reduceRight(
+      (parm, key, index) => (index === actionArray.length - 1 ? { [key]: params.payload } : { [key]: parm }),
+      {},
+    );
+  }
   try {
     const reply = dispatchFromAdapter(action, params, 'http');
     res.status(202).json(reply);
