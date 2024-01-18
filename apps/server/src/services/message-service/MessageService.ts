946ab9d7f0eb7dda1565ced0a334efd5908f4a0f
arc-alex <ac@omnivox.dk>
2024-01-18 17:20:00 +0100

    refactor: we only use the patch functions

--- apps/server/src/services/message-service/MessageService.ts
+++ apps/server/src/services/message-service/MessageService.ts
@@ -56,7 +56,7 @@ class MessageService {
   }
 
   /**
-   * @description patches the Message object
+   * @description patches the External Message object
    */
   setExternalMessage(payload: Partial<Message>) {
     this.externalMessage = { ...this.externalMessage, ...payload };
@@ -65,27 +65,7 @@ class MessageService {
   }
 
   /**
-   * @description sets message on stage timer screen
-   */
-  setExternalText(payload: string) {
-    if (this.externalMessage.text !== payload) {
-      this.externalMessage.text = payload;
-      this.throttledSet('externalMessage', this.externalMessage);
-    }
-    return this.getAll();
-  }
-
-  /**
-   * @description sets message visibility on stage timer screen
-   */
-  setExternalVisibility(status: boolean) {
-    this.externalMessage.visible = status;
-    this.throttledSet('externalMessage', this.externalMessage);
-    return this.getAll();
-  }
-
-  /**
-   * @description patches the TimerMessage object
+   * @description patches the Timer Message object
    */
   setTimerMessage(payload: Partial<TimerMessage>) {
     this.timerMessage = { ...this.timerMessage, ...payload };
@@ -94,25 +74,7 @@ class MessageService {
   }
 
   /**
-   * @description sets message on stage timer screen
-   */
-  setTimerText(payload: string) {
-    this.timerMessage.text = payload;
-    this.throttledSet('timerMessage', this.timerMessage);
-    return this.getAll();
-  }
-
-  /**
-   * @description sets message visibility on stage timer screen
-   */
-  setTimerVisibility(status: boolean) {
-    this.timerMessage.visible = status;
-    this.throttledSet('timerMessage', this.timerMessage);
-    return this.getAll();
-  }
-
-  /**
-   * @description sets message on public screen
+   * @description patches the Public Message object
    */
   setPublicMessage(payload: Partial<Message>) {
     this.publicMessage = { ...this.publicMessage, ...payload };
@@ -121,75 +83,11 @@ class MessageService {
   }
 
   /**
-   * @description sets message on public screen
-   */
-  setPublicText(payload: string) {
-    this.publicMessage.text = payload;
-    this.throttledSet('publicMessage', this.publicMessage);
-    return this.getAll();
-  }
-
-  /**
-   * @description sets message visibility on public screen
-   */
-  setPublicVisibility(status: boolean) {
-    this.publicMessage.visible = status;
-    this.throttledSet('publicMessage', this.publicMessage);
-    return this.getAll();
-  }
-
-  /**
-   * @description sets message on public screen
+   * @description patches the Lower Message object
    */
   setLowerMessage(payload: Partial<Message>) {
     this.lowerMessage = { ...this.lowerMessage, ...payload };
     this.throttledSet('lowerMessage', this.lowerMessage);
-    return this.getAll();
-  }
-
-  /**
-   * @description sets message on lower third screen
-   */
-  setLowerText(payload: string) {
-    this.lowerMessage.text = payload;
-    this.throttledSet('lowerMessage', this.lowerMessage);
-    return this.getAll();
-  }
-
-  /**
-   * @description sets message visibility on lower third screen
-   */
-  setLowerVisibility(status: boolean) {
-    this.lowerMessage.visible = status;
-    this.throttledSet('lowerMessage', this.lowerMessage);
-    return this.getAll();
-  }
-
-  /**
-   * @description set state of timer blink, toggles if parameters are offered
-   */
-
-  setTimerBlink(status?: boolean) {
-    if (typeof status === 'undefined') {
-      this.timerMessage.blink = !this.timerMessage.blink;
-    } else {
-      this.timerMessage.blink = status;
-    }
-    this.throttledSet('timerMessage', this.timerMessage);
-    return this.getAll();
-  }
-
-  /**
-   * @description set state of timer blackout, toggles if parameters are offered
-   */
-
-  setTimerBlackout(status?: boolean) {
-    if (typeof status === 'undefined') {
-      this.timerMessage.blackout = !this.timerMessage.blackout;
-    } else {
-      this.timerMessage.blackout = status;
-    }
-    this.throttledSet('timerMessage', this.timerMessage);
     return this.getAll();
   }
 
@@ -221,19 +119,5 @@ export function isPartialTimerMessage(obj: any): obj is Partial<TimerMessage> {
     (typeof obj.visible === 'boolean' || obj.visible === undefined) &&
     (typeof obj.blink === 'boolean' || obj.timerBlink === undefined) &&
     (typeof obj.blackout === 'boolean' || obj.timerBlackout === undefined)
-  );
-}
-
-/**
- * Asserts whether an object is a valid TimerMessage patch
- * @param obj - object to evaluate
- * @returns boolean
- */
-export function isPartialMessage(obj: any): obj is Partial<Message> {
-  return (
-    obj &&
-    typeof obj === 'object' &&
-    (typeof obj.text === 'string' || obj.text === undefined) &&
-    (typeof obj.visible === 'boolean' || obj.visible === undefined)
   );
 }
