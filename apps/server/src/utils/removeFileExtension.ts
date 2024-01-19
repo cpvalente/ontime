cc9eab173f1e4d8eb853e68999f604e484c2cab0
Ary <arylmoraesn@gmail.com>
2024-01-19 15:44:50 -0700

    feat: removed regex for removeFileExtension

--- apps/server/src/utils/removeFileExtension.ts
+++ apps/server/src/utils/removeFileExtension.ts
@@ -1,7 +1,9 @@
+import { parse } from 'path';
+
 /**
  * @description Takes a filename and removes the extension
  * @param {string} filename - filename with extension
  */
 export const removeFileExtension = (filename: string): string => {
-  return filename.replace(/\.[^/.]+$/, '');
+  return parse(filename).name;
 };
