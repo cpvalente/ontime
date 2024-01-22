b3bdb74e1b0de1e7a27493b3f3d223e2cad5b78e
Carlos Valente <carlosvalente@pm.me>
2024-01-22 13:56:07 +0100

    chore: fail tests that contain unhandled errors

---
+++ e2e/tests/fixtures/override.ts
@@ -0,0 +1,14 @@
+import { expect, test as base } from '@playwright/test';
+
+// fail tests which contain errors
+export const test = base.extend({
+  page: async ({ page }, use) => {
+    const messages = [];
+    page.on('pageerror', (exception) => {
+      console.log(`Uncaught exception: "${exception.message}"`);
+      messages.push(exception);
+    });
+    await use(page);
+    expect(messages).toEqual([]);
+  },
+});
