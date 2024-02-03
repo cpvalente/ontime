9ce3893a47770dd9d83af830c76e454df2144e85
Carlos Valente <carlosvalente@pm.me>
2024-02-03 12:03:55 +0100

    refactor: increase ID size

--- packages/utils/src/generate-id/generateId.test.ts
+++ packages/utils/src/generate-id/generateId.test.ts
@@ -1,28 +1,6 @@
 import { generateId } from './generateId.js';
 
-test('generate a valid 5 digit id', () => {
+test('generate a valid 6 digit id', () => {
   const id = generateId();
-  expect(id.length).toBe(5);
-});
-
-test('generate 100 with less than 110 attempts', () => {
-  const ids = new Set<string>();
-  let attempts = 1;
-  while (ids.size < 100) {
-    ids.add(generateId());
-    attempts++;
-  }
-
-  expect(attempts).toBeLessThan(105);
-});
-
-test('generate 1000 with less than 1020 attempts', () => {
-  const ids = new Set<string>();
-  let attempts = 1;
-  while (ids.size < 1000) {
-    ids.add(generateId());
-    attempts++;
-  }
-
-  expect(attempts).toBeLessThan(1020);
+  expect(id.length).toBe(6);
 });
