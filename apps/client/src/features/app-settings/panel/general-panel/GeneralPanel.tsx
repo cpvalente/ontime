10d3986832610a49f77baef2a217b8574b6f445c
Bianca Procopio <biancahprocopio@gmail.com>
2024-03-03 11:06:21 -0700

    Move app settings (#765)
    * refactor: migrate app settings
    
    ---------
    
    Co-authored-by: Carlos Valente <carlosvalente@pm.me>

---
+++ apps/client/src/features/app-settings/panel/general-panel/GeneralPanel.tsx
@@ -0,0 +1,12 @@
+import * as Panel from '../PanelUtils';
+
+import GeneralPanelForm from './GeneralPanelForm';
+
+export default function GeneralPanel() {
+  return (
+    <>
+      <Panel.Header>Settings</Panel.Header>
+      <GeneralPanelForm />
+    </>
+  );
+}
