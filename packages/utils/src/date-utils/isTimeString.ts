7203830b7e44821d3cf83637877133e856df970c
arc-alex <ac@omnivox.dk>
2024-03-22 14:49:31 +0100

    chore: comment not correct anymore

--- packages/utils/src/date-utils/isTimeString.ts
+++ packages/utils/src/date-utils/isTimeString.ts
@@ -4,16 +4,6 @@
  * @returns {boolean} string represents time
  */
 export const isTimeString = (text: string): boolean => {
-  // ^                   # Start of string
-  // (?:                 # Try to match...
-  //  (?:                #  Try to match...
-  //   ([01]?\d|2[0-3]): #   HH:
-  //  )?                 #  (optionally).
-  //  ([0-5]?\d):        #  MM: (required)
-  // )?                  # (entire group optional, so either HH:MM:, MM: or nothing)
-  // ([0-5]?\d)          # SS (required)
-  // $                   # End of string
-
   const regex = /^(?:(?:([01]?\d|2[0-3])[:,.])?([0-5]?\d)[:,.])?([0-5]?\d)?(\s)?([APap][Mm])?$/;
   return regex.test(text);
 };
