a8add1791788c3d70b26fb8efca0ae42b6b72435
arc-alex <ac@omnivox.dk>
2024-03-30 14:28:42 +0100

    set immutable tag for static route

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -83,6 +83,10 @@ app.use(
   expressStaticGzip(reactAppPath, {
     enableBrotli: true,
     orderPreference: ['br'],
+    // when we build the client all the react subfiles will get a hashed name we can the immutable tag
+    // as the contents of a build file will never change without also changing its name
+    // so the client dose not need to revalidate the file contetnts with the server
+    serveStatic: { etag: false, lastModified: false, immutable: true, maxAge: '1y' },
   }),
 );
 
