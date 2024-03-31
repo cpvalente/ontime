b9d7fa3b89096dbc31e0584b5d747e9814a3953c
kellhogs <fabianpos99@gmail.com>
2024-04-01 00:54:57 +0200

    idk what im doing

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -182,7 +182,6 @@ export const startServer = async () => {
       playback: SimplePlayback.Stop,
       direction: SimpleDirection.CountDown,
     },
-    clientList: [],
   });
 
   // initialise rundown service
