ca9fc3d6501f67a9e0b89fab1bfae20b0a015a0d
Carlos Valente <carlosvalente@pm.me>
2024-04-08 21:19:01 +0200

    chore: add feature test

---
+++ e2e/tests/features/207-view-params.spec.ts
@@ -0,0 +1,13 @@
+import { test, expect } from '@playwright/test';
+
+test('View params configures timer view', async ({ page }) => {
+  await page.goto('http://localhost:4001/timer');
+
+  await expect(page.getByText('TIME NOW')).toBeInViewport();
+
+  await page.getByTestId('navigation__toggle-settings').click();
+  await page.locator('label').filter({ hasText: 'Hide Time NowHides the Time' }).locator('span').nth(2).click();
+  await page.getByRole('button', { name: 'Save' }).click();
+
+  await expect(page.getByText('TIME NOW')).not.toBeInViewport();
+});
