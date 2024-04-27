75b5118b0f6239b973be9b71b0608e2c66123bdd
Carlos Valente <carlosvalente@pm.me>
2024-04-27 10:04:36 +0200

    refactor: user alerts on destructive actions

--- apps/client/src/features/rundown/rundown-header/RundownMenu.tsx
+++ apps/client/src/features/rundown/rundown-header/RundownMenu.tsx
@@ -1,5 +1,14 @@
-import { useCallback } from 'react';
-import { Button } from '@chakra-ui/react';
+import { useCallback, useRef } from 'react';
+import {
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogOverlay,
+  Button,
+  useDisclosure,
+} from '@chakra-ui/react';
 import { IoTrash } from '@react-icons/all-files/io5/IoTrash';
 
 import { useEventAction } from '../../../common/hooks/useEventAction';
@@ -12,22 +21,48 @@ export default function RundownMenu() {
   const appMode = useAppMode((state) => state.mode);
   const { deleteAllEvents } = useEventAction();
 
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const cancelRef = useRef<HTMLButtonElement | null>(null);
+
   const deleteAll = useCallback(() => {
     deleteAllEvents();
     clearSelectedEvents();
     setCursor(null);
-  }, [clearSelectedEvents, deleteAllEvents, setCursor]);
+    onClose();
+  }, [clearSelectedEvents, deleteAllEvents, onClose, setCursor]);
 
   return (
-    <Button
-      size='sm'
-      variant='ontime-outlined'
-      leftIcon={<IoTrash />}
-      onClick={deleteAll}
-      color='#FA5656'
-      isDisabled={appMode === 'run'}
-    >
-      Clear rundown
-    </Button>
+    <>
+      <Button
+        size='sm'
+        variant='ontime-outlined'
+        leftIcon={<IoTrash />}
+        onClick={onOpen}
+        color='#FA5656'
+        isDisabled={appMode === 'run'}
+      >
+        Clear rundown
+      </Button>
+      <AlertDialog variant='ontime' isOpen={isOpen} leastDestructiveRef={cancelRef} onClose={onClose}>
+        <AlertDialogOverlay>
+          <AlertDialogContent>
+            <AlertDialogHeader fontSize='lg' fontWeight='bold'>
+              Clear rundown
+            </AlertDialogHeader>
+            <AlertDialogBody>
+              You will lose all data in your rundown. <br /> Are you sure?
+            </AlertDialogBody>
+            <AlertDialogFooter>
+              <Button ref={cancelRef} onClick={onClose} variant='ontime-ghosted-white'>
+                Cancel
+              </Button>
+              <Button colorScheme='red' onClick={deleteAll} ml={4}>
+                Delete all
+              </Button>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialogOverlay>
+      </AlertDialog>
+    </>
   );
 }
