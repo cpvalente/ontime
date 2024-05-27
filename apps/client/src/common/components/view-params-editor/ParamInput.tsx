e19f4ecb5bc4a02900e423a69636b035c552ce0b
arc-alex <ac@omnivox.dk>
2024-05-27 15:19:15 +0200

    remove chakra-react-select and replace with checkbox menu

--- apps/client/src/common/components/view-params-editor/ParamInput.tsx
+++ apps/client/src/common/components/view-params-editor/ParamInput.tsx
@@ -1,6 +1,18 @@
+import { useRef } from 'react';
 import { useSearchParams } from 'react-router-dom';
-import { Input, InputGroup, InputLeftElement, Select, Switch, Tag } from '@chakra-ui/react';
-import { chakraComponents, Select as MultiSelect } from 'chakra-react-select';
+import {
+  Button,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  Menu,
+  MenuButton,
+  MenuItemOption,
+  MenuList,
+  MenuOptionGroup,
+  Select,
+  Switch,
+} from '@chakra-ui/react';
 
 import { isStringBoolean } from '../../../features/viewers/common/viewUtils';
 
@@ -14,6 +26,8 @@ export default function ParamInput(props: EditFormInputProps) {
   const [searchParams] = useSearchParams();
   const { paramField } = props;
   const { id, type, defaultValue } = paramField;
+
+  const inputRef = useRef<HTMLInputElement | null>(null);
 
   if (type === 'option') {
     const optionFromParams = searchParams.get(id);
@@ -36,30 +50,37 @@ export default function ParamInput(props: EditFormInputProps) {
   }
 
   if (type === 'multi-option') {
-    const optionFromParams = (searchParams.get(id) ?? '').split(',').map((value) => {
-      const selectedOption = paramField.values[value.toLocaleLowerCase()];
-      if (selectedOption) {
-        return { value: selectedOption.value, label: selectedOption.label, colour: selectedOption.colour };
-      }
-      return undefined;
-    });
-
+    const optionFromParams = (searchParams.get(id) ?? '').toLocaleLowerCase().split(',');
     //TODO:
-    // const defaultOptionValue = optionFromParams || defaultValue;
+    const defaultOptionValue = optionFromParams || defaultValue?.toLocaleLowerCase().split(',');
 
+    //TODO: , turned into '%2C' for url kan we comeup with something better
     return (
-      <MultiSelect
-        placeholder={defaultValue ? undefined : 'Select options'}
-        // variant='ontime'
-        useBasicStyles
-        tagVariant='solid'
-        name={id}
-        isMulti
-        defaultValue={optionFromParams}
-        options={Object.values(paramField.values)}
-        components={customFieldsOptions}
-        delimiter=',' //TODO: this gest turned into '%2C' for url kan we comeup with something better
-      />
+      <>
+        <input name={id} hidden ref={inputRef}></input>
+        <Menu closeOnSelect={false} variant='ontime'>
+          <MenuButton as={Button}>{paramField.title}</MenuButton>
+          <MenuList>
+            <MenuOptionGroup
+              title='Select fields to display'
+              type='checkbox'
+              defaultValue={defaultOptionValue}
+              onChange={(value) => {
+                if (inputRef.current) inputRef.current.value = typeof value === 'object' ? value.join() : value;
+              }}
+            >
+              {Object.values(paramField.values).map((option) => {
+                const { value, label, colour } = option;
+                return (
+                  <MenuItemOption backgroundColor={colour} color='black' value={value} key={value}>
+                    {label}
+                  </MenuItemOption>
+                );
+              })}
+            </MenuOptionGroup>
+          </MenuList>
+        </Menu>
+      </>
     );
   }
 
@@ -99,21 +120,3 @@ export default function ParamInput(props: EditFormInputProps) {
     </InputGroup>
   );
 }
-
-//FIXME: ???
-const customFieldsOptions = {
-  // @ts-expect-error: react type problems
-  Option: ({ children, ...props }) => (
-    // @ts-expect-error: react type problems
-    <chakraComponents.Option {...props}>
-      <Tag style={{ backgroundColor: props.data.colour, color: ' #101010' }}>{children}</Tag>
-    </chakraComponents.Option>
-  ),
-  // @ts-expect-error: react type problems
-  MultiValueContainer: ({ children, ...props }) => (
-    // @ts-expect-error: react type problems
-    <chakraComponents.MultiValueContainer {...props}>
-      <Tag style={{ backgroundColor: props.data.colour, color: ' #101010' }}>{children}</Tag>
-    </chakraComponents.MultiValueContainer>
-  ),
-};
