a6de9cd14ec89651d5660fdd9fc626173e2cb4f2
arc-alex <ac@omnivox.dk>
2024-05-31 22:10:39 +0200

    test move

--- e2e/tests/features/209-rundown-shortcuts.spec.ts
+++ e2e/tests/features/209-rundown-shortcuts.spec.ts
@@ -38,3 +38,27 @@ test('Copy Past', async ({ page }) => {
   await expect(page.getByTestId('entry-2').getByTestId('block__title')).toHaveValue('test');
   await expect(page.getByTestId('entry-2').locator('#event-block')).toContainText('4.1');
 });
+
+test('Move', async ({ page }) => {
+  await page.goto('http://localhost:4001/rundown');
+
+  // clear rundown
+  await page.getByRole('button', { name: 'Clear rundown' }).click();
+  await page.getByRole('button', { name: 'Delete all' }).click();
+
+  //create events
+  await page.getByRole('button', { name: 'Create Event' }).click();
+  await page.getByRole('button', { name: 'Event' }).nth(4).click();
+  await page.getByRole('button', { name: 'Event', exact: true }).nth(1).click();
+
+  //copy move down
+  await page.getByTestId('entry-1').locator('#event-block').getByText('1').click();
+  await page.getByTestId('entry-1').locator('#event-block div').filter({ hasText: '1' }).press('Alt+Control+ArrowDown');
+  await expect(page.getByTestId('entry-2').locator('#event-block')).toContainText('1');
+
+  //copy move up
+  await page.getByTestId('entry-3').locator('#event-block').getByText('3').click();
+  await page.getByTestId('entry-3').locator('#event-block div').filter({ hasText: '3' }).press('Alt+Control+ArrowUp');
+  await page.getByTestId('entry-2').locator('div').filter({ hasText: /^3$/ }).press('Alt+Control+ArrowUp');
+  await expect(page.getByTestId('entry-1').locator('#event-block')).toContainText('3');
+});
