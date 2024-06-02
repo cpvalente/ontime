72db09f7189481a4608b62ccfa41266f33484a5c
arc-alex <ac@omnivox.dk>
2024-06-02 20:43:36 +0200

    test add block, delay and event

--- e2e/tests/features/209-rundown-shortcuts.spec.ts
+++ e2e/tests/features/209-rundown-shortcuts.spec.ts
@@ -62,3 +62,66 @@ test('Move', async ({ page }) => {
   await page.getByTestId('entry-2').locator('div').filter({ hasText: /^3$/ }).press('Alt+Control+ArrowUp');
   await expect(page.getByTestId('entry-1').locator('#event-block')).toContainText('3');
 });
+
+test('Add block', async ({ page }) => {
+  await page.goto('http://localhost:4001/rundown');
+
+  // clear rundown
+  await page.getByRole('button', { name: 'Clear rundown' }).click();
+  await page.getByRole('button', { name: 'Delete all' }).click();
+
+  //create events
+  await page.getByRole('button', { name: 'Create Event' }).click();
+  await page.getByTestId('entry-1').click();
+  await page.getByTestId('block__title').press('Escape');
+
+  //add block below
+  await page.getByTestId('entry-1').locator('#event-block div').filter({ hasText: '1' }).press('Alt+B');
+  await expect(page.getByPlaceholder('Block title')).toBeVisible();
+
+  //add block above
+  await page.getByTestId('entry-1').locator('#event-block div').filter({ hasText: '1' }).press('Alt+Shift+B');
+  await expect(page.getByTestId('entry-0').getByTestId('block__title')).toBeVisible();
+});
+
+test('Add delay', async ({ page }) => {
+  await page.goto('http://localhost:4001/rundown');
+
+  // clear rundown
+  await page.getByRole('button', { name: 'Clear rundown' }).click();
+  await page.getByRole('button', { name: 'Delete all' }).click();
+
+  //create events
+  await page.getByRole('button', { name: 'Create Event' }).click();
+  await page.getByTestId('entry-1').click();
+  await page.getByTestId('block__title').press('Escape');
+
+  //add delay below
+  await page.getByTestId('entry-1').locator('#event-block div').filter({ hasText: '1' }).press('Alt+D');
+  await expect(page.getByTestId('delay-input')).toBeVisible();
+
+  //add delay above
+  await page.getByTestId('entry-1').locator('#event-block div').filter({ hasText: '1' }).press('Alt+Shift+D');
+  await expect(page.getByTestId('entry-0').getByTestId('delay-input')).toBeVisible();
+});
+
+test('Add event', async ({ page }) => {
+  await page.goto('http://localhost:4001/rundown');
+
+  // clear rundown
+  await page.getByRole('button', { name: 'Clear rundown' }).click();
+  await page.getByRole('button', { name: 'Delete all' }).click();
+
+  //create events
+  await page.getByRole('button', { name: 'Create Event' }).click();
+  await page.getByTestId('entry-1').click();
+  await page.getByTestId('block__title').press('Escape');
+
+  //add event below
+  await page.getByTestId('entry-1').locator('#event-block div').filter({ hasText: '1' }).press('Alt+E');
+  await expect(page.getByTestId('entry-2').locator('#event-block').getByText('2')).toBeVisible();
+
+  //add event above
+  await page.getByTestId('entry-1').locator('#event-block div').filter({ hasText: '1' }).press('Alt+Shift+E');
+  await expect(page.getByTestId('entry-1').locator('#event-block')).toContainText('0.1');
+});
