39f1cd6e5eabbf5cf32a245c19483afacf600721
Joel Wetzell <jwetzell@yahoo.com>
2024-06-03 21:54:14 -0500

    add basic cli app for bundling a headless ontime

---
+++ apps/cli/main.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+// NOTE: for now the following needs to be in place: ./server/index.cjs, ./client, ./external
+
+const ontimeServer = require('./server/index.cjs');
+const { initAssets, startServer, startIntegrations, shutdown } = ontimeServer;
+
+async function startOntime() {
+  await initAssets();
+
+  await startServer();
+
+  await startIntegrations();
+}
+
+startOntime();
+
+process.on('SIGINT',()=>{
+  shutdown()
+})
