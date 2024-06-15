40e54bb02b56369eac2738d74a695e9b7f14d22e
root <root@cma-multimedia.eu.org>
2024-06-15 12:14:29 +0200

    PL translatio

---
+++ apps/client/src/translation/languages/pl.ts
@@ -0,0 +1,20 @@
+export const langEn = {
+  'common.end_time': 'End Time',
+  'common.expected_finish': 'Expected Finish',
+  'common.minutes': 'min',
+  'common.now': 'Now',
+  'common.next': 'Next',
+  'common.public_message': 'Public message',
+  'common.start_time': 'Start Time',
+  'common.stage_timer': 'Stage Timer',
+  'common.started_at': 'Started At',
+  'common.time_now': 'Time now',
+  'countdown.ended': 'Event ended at',
+  'countdown.running': 'Event running',
+  'countdown.select_event': 'Select an event to follow',
+  'countdown.to_start': 'Time to start',
+  'countdown.waiting': 'Waiting for event start',
+  'countdown.overtime': 'in overtime',
+};
+
+export type TranslationObject = Record<keyof typeof langEn, string>;
