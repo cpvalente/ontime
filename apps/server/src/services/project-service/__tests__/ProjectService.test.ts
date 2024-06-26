843cae732c7e053952606103138bea393c926a61
Carlos Valente <carlosvalente@pm.me>
2024-06-26 22:30:15 +0200

    refactor: duplicate project

--- apps/server/src/services/project-service/__tests__/ProjectService.test.ts
+++ apps/server/src/services/project-service/__tests__/ProjectService.test.ts
@@ -1,24 +1,30 @@
-import { deleteProjectFile } from '../ProjectService.js';
+import { deleteProjectFile, duplicateProjectFile } from '../ProjectService.js';
 import { appStateProvider } from '../../app-state-service/AppStateService.js';
-import { doesProjectExist, getPathToProject } from '../projectServiceUtils.js';
+import { doesProjectExist } from '../projectServiceUtils.js';
 import { Mock } from 'vitest';
-import { deleteFile } from '../../../utils/parserUtils.js';
 
-vi.mock('./appStateProvider');
-vi.mock('./fileSystem');
+// stop the database loading from initiating
+vi.mock('../../../setup/loadDb.js', () => {
+  return {
+    switchDb: vi.fn(),
+  };
+});
+
 vi.mock('../../app-state-service/AppStateService.js', () => ({
   appStateProvider: {
     isLastLoadedProject: vi.fn(),
   },
 }));
+
 vi.mock('../projectServiceUtils.js', () => ({
   doesProjectExist: vi.fn(),
   getPathToProject: vi.fn(),
 }));
-vi.mock('../../../utils/parserUtils.js', () => ({
-  deleteFile: vi.fn(),
-}));
 
+/**
+ * tests only assert errors since the
+ * controller depend on these to send the right responses
+ */
 describe('deleteProjectFile', () => {
   it('throws an error if trying to delete the currently loaded project', async () => {
     (appStateProvider.isLastLoadedProject as Mock).mockResolvedValue(true);
@@ -30,16 +36,21 @@ describe('deleteProjectFile', () => {
     (doesProjectExist as Mock).mockReturnValue(false);
     await expect(deleteProjectFile('nonexistentProject')).rejects.toThrow('Project file not found');
   });
+});
 
-  it('deletes the project file successfully', async () => {
-    (appStateProvider.isLastLoadedProject as Mock).mockResolvedValue(false);
-    (doesProjectExist as Mock).mockReturnValue(true);
-    (getPathToProject as Mock).mockReturnValue('/path/to/project');
-    (deleteFile as Mock).mockResolvedValue(undefined);
+describe('duplicateProjectFile', () => {
+  it('throws an error if origin project does not exist', async () => {
+    (doesProjectExist as Mock).mockReturnValue(false);
+    await expect(duplicateProjectFile('does not exist', 'doesnt matter')).rejects.toThrow('Project file not found');
+  });
 
-    await deleteProjectFile('existingProject');
-
-    expect(getPathToProject).toHaveBeenCalledWith('existingProject');
-    expect(deleteFile).toHaveBeenCalledWith('/path/to/project');
+  it('throws an error if new file name is already a project', async () => {
+    // current project exists
+    (doesProjectExist as Mock).mockReturnValueOnce(true);
+    // new project exists
+    (doesProjectExist as Mock).mockReturnValueOnce(true);
+    expect(duplicateProjectFile('nonexistentProject', 'existingproject')).rejects.toThrow(
+      'Project file with name existingproject already exists',
+    );
   });
 });
