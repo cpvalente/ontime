25aefe322bb496feb94f92beb58861cb5c6fa8ab
Gyuris Gellért <jobel@ujevangelizacio.hu>
2024-08-27 13:06:23 +0200

    Hungarian translation (#1187)
    * Add Hungarian translation

--- apps/client/src/features/app-settings/panel/general-panel/GeneralPanelForm.tsx
+++ apps/client/src/features/app-settings/panel/general-panel/GeneralPanelForm.tsx
@@ -148,6 +148,7 @@ export default function GeneralPanelForm() {
                 <option value='en'>English</option>
                 <option value='fr'>French</option>
                 <option value='de'>German</option>
+                <option value='hu'>Hungarian</option>
                 <option value='it'>Italian</option>
                 <option value='no'>Norwegian</option>
                 <option value='pt'>Portuguese</option>
