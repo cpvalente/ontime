e87d0bf21e939496d1dcc25639e21ab8aa6f303b
Carlos Valente <carlosvalente@pm.me>
2024-10-13 15:05:03 +0200

    refactor: network interfaces can be copied or followed

--- apps/client/src/features/app-settings/panel/network-panel/NetworkInterfaces.tsx
+++ apps/client/src/features/app-settings/panel/network-panel/NetworkInterfaces.tsx
@@ -1,19 +1,33 @@
+import { IoArrowUp } from '@react-icons/all-files/io5/IoArrowUp';
+
 import { serverPort } from '../../../../common/api/constants';
-import AppLink from '../../../../common/components/app-link/AppLink';
+import CopyTag from '../../../../common/components/copy-tag/CopyTag';
 import useInfo from '../../../../common/hooks-query/useInfo';
+import { openLink } from '../../../../common/utils/linkUtils';
 
 import style from './NetworkInterfaces.module.scss';
 
 export default function InfoNif() {
   const { data } = useInfo();
 
+  const handleClick = (address: string) => openLink(address);
+
   return (
     <div className={style.interfaces}>
-      {data?.networkInterfaces?.map((nif) => (
-        <AppLink key={nif.address} href={`http://${nif.address}:${serverPort}`}>
-          {`${nif.name} - ${nif.address}`}
-        </AppLink>
-      ))}
+      {data?.networkInterfaces?.map((nif) => {
+        const address = `http://${nif.address}:${serverPort}`;
+
+        return (
+          <CopyTag
+            key={nif.name}
+            copyValue={address}
+            onClick={() => handleClick(address)}
+            label='Copy IP or navigate to address'
+          >
+            {`${nif.name} - ${nif.address}`} <IoArrowUp className={style.goIcon} />
+          </CopyTag>
+        );
+      })}
     </div>
   );
 }
