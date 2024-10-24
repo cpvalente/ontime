1f130fcdca7b00fb91b41fc68cb8217359fdc5ed
arc-alex <ac@omnivox.dk>
2024-10-24 13:16:44 +0200

    use prediction in event progress

--- apps/client/src/features/rundown/event-block/composite/EventBlockProgressBar.tsx
+++ apps/client/src/features/rundown/event-block/composite/EventBlockProgressBar.tsx
@@ -1,12 +1,13 @@
+import { useClientTimePrediction } from '../../../../common/hooks/useClientTimePrediction';
 import { useTimer } from '../../../../common/hooks/useSocket';
 import { getProgress } from '../../../../common/utils/getProgress';
 
 import style from './EventBlockProgressBar.module.scss';
 
 export default function EventBlockProgressBar() {
-  const timer = useTimer();
-
-  const progress = getProgress(timer.current, timer.duration);
+  const { duration } = useTimer();
+  const clientCurrent = useClientTimePrediction();
+  const progress = getProgress(clientCurrent, duration);
 
   return <div className={style.progressBar} style={{ width: `${progress}%` }} />;
 }
