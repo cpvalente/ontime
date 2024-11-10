4686ad4f0d832d24b0367bce33ed3cc47eaf48c3
asharonbaltazar <asharonbaltazar@outlook.com>
2024-11-10 14:24:13 -0500

    feat: use toggle for frozen mode

--- apps/client/src/features/rundown/rundown-header/RundownHeader.tsx
+++ apps/client/src/features/rundown/rundown-header/RundownHeader.tsx
@@ -1,6 +1,5 @@
 import { Button, ButtonGroup } from '@chakra-ui/react';
 
-import { useEventAction } from '../../../common/hooks/useEventAction';
 import { AppMode, useAppMode } from '../../../common/stores/appModeStore';
 
 import RundownMenu from './RundownMenu';
@@ -8,39 +7,25 @@ import RundownMenu from './RundownMenu';
 import style from './RundownHeader.module.scss';
 
 export default function RundownHeader() {
-  const { toggleFreezeEvents } = useEventAction();
-
   const appMode = useAppMode((state) => state.mode);
   const setAppMode = useAppMode((state) => state.setMode);
-
-  const toggleAppMode = async (appMode: AppMode) => {
-    if (appMode === AppMode.Freeze) {
-      await toggleFreezeEvents(true);
-    } else {
-      await toggleFreezeEvents(false);
-    }
-    setAppMode(appMode);
-  };
-
-  const setRunMode = () => toggleAppMode(AppMode.Run);
-  const setEditMode = () => toggleAppMode(AppMode.Edit);
-  const setFreezeMode = () => toggleAppMode(AppMode.Freeze);
 
   return (
     <div className={style.header}>
       <ButtonGroup isAttached>
-        <Button size='sm' variant={appMode === AppMode.Run ? 'ontime-filled' : 'ontime-subtle'} onClick={setRunMode}>
+        <Button
+          size='sm'
+          variant={appMode === AppMode.Run ? 'ontime-filled' : 'ontime-subtle'}
+          onClick={() => setAppMode(AppMode.Run)}
+        >
           Run
-        </Button>
-        <Button size='sm' variant={appMode === AppMode.Edit ? 'ontime-filled' : 'ontime-subtle'} onClick={setEditMode}>
-          Edit
         </Button>
         <Button
           size='sm'
-          variant={appMode === AppMode.Freeze ? 'ontime-filled' : 'ontime-subtle'}
-          onClick={setFreezeMode}
+          variant={appMode === AppMode.Edit ? 'ontime-filled' : 'ontime-subtle'}
+          onClick={() => setAppMode(AppMode.Edit)}
         >
-          Freeze
+          Edit
         </Button>
       </ButtonGroup>
       <RundownMenu />
--- apps/client/src/features/rundown/rundown-header/RundownMenu.tsx
+++ apps/client/src/features/rundown/rundown-header/RundownMenu.tsx
@@ -7,18 +7,24 @@ import {
   AlertDialogHeader,
   AlertDialogOverlay,
   Button,
+  Switch,
   useDisclosure,
 } from '@chakra-ui/react';
 import { IoTrash } from '@react-icons/all-files/io5/IoTrash';
 
 import { useEventAction } from '../../../common/hooks/useEventAction';
+import { useFrozen } from '../../../common/hooks/useSocket';
 import { useAppMode } from '../../../common/stores/appModeStore';
 import { useEventSelection } from '../useEventSelection';
+
+import style from './RundownHeader.module.scss';
 
 export default function RundownMenu() {
   const clearSelectedEvents = useEventSelection((state) => state.clearSelectedEvents);
   const appMode = useAppMode((state) => state.mode);
-  const { deleteAllEvents } = useEventAction();
+  const { frozen } = useFrozen();
+
+  const { deleteAllEvents, toggleFreezeEvents } = useEventAction();
 
   const { isOpen, onOpen, onClose } = useDisclosure();
   const cancelRef = useRef<HTMLButtonElement | null>(null);
@@ -30,38 +36,44 @@ export default function RundownMenu() {
   }, [clearSelectedEvents, deleteAllEvents, onClose]);
 
   return (
-    <>
-      <Button
-        size='sm'
-        variant='ontime-outlined'
-        leftIcon={<IoTrash />}
-        onClick={onOpen}
-        color='#FA5656'
-        isDisabled={appMode !== 'edit'}
-      >
-        Clear rundown
-      </Button>
+    <div className={style.controls}>
+      <Switch variant='ontime' isChecked={frozen} onChange={() => toggleFreezeEvents(!frozen)}>
+        Freeze
+      </Switch>
 
-      <AlertDialog variant='ontime' isOpen={isOpen} leastDestructiveRef={cancelRef} onClose={onClose}>
-        <AlertDialogOverlay>
-          <AlertDialogContent>
-            <AlertDialogHeader fontSize='lg' fontWeight='bold'>
-              Clear rundown
-            </AlertDialogHeader>
-            <AlertDialogBody>
-              You will lose all data in your rundown. <br /> Are you sure?
-            </AlertDialogBody>
-            <AlertDialogFooter>
-              <Button ref={cancelRef} onClick={onClose} variant='ontime-ghosted-white'>
-                Cancel
-              </Button>
-              <Button colorScheme='red' onClick={deleteAll} ml={4}>
-                Delete all
-              </Button>
-            </AlertDialogFooter>
-          </AlertDialogContent>
-        </AlertDialogOverlay>
-      </AlertDialog>
-    </>
+      <div>
+        <Button
+          size='sm'
+          variant='ontime-outlined'
+          leftIcon={<IoTrash />}
+          onClick={onOpen}
+          color='#FA5656'
+          isDisabled={appMode !== 'edit' || frozen}
+        >
+          Clear rundown
+        </Button>
+
+        <AlertDialog variant='ontime' isOpen={isOpen} leastDestructiveRef={cancelRef} onClose={onClose}>
+          <AlertDialogOverlay>
+            <AlertDialogContent>
+              <AlertDialogHeader fontSize='lg' fontWeight='bold'>
+                Clear rundown
+              </AlertDialogHeader>
+              <AlertDialogBody>
+                You will lose all data in your rundown. <br /> Are you sure?
+              </AlertDialogBody>
+              <AlertDialogFooter>
+                <Button ref={cancelRef} onClick={onClose} variant='ontime-ghosted-white'>
+                  Cancel
+                </Button>
+                <Button colorScheme='red' onClick={deleteAll} ml={4}>
+                  Delete all
+                </Button>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialogOverlay>
+        </AlertDialog>
+      </div>
+    </div>
   );
 }
