1b68eef358fb2d5c7e09491fb9c98b31fa8c32c1
asharonbaltazar <asharonbaltazar@outlook.com>
2024-11-14 11:21:34 -0500

    feat: allow `Freezable` to be extended as components

--- apps/client/src/features/rundown/freezable/Freezable.tsx
+++ apps/client/src/features/rundown/freezable/Freezable.tsx
@@ -1,22 +1,32 @@
 import { ReactElement } from 'react';
+import { Box, BoxProps } from '@chakra-ui/react';
 import { IoSnowSharp } from '@react-icons/all-files/io5/IoSnowSharp';
 
 import { useFrozen } from '../../../common/hooks/useSocket';
+import { cx } from '../../../common/utils/styleUtils';
 
 import style from './Freezable.module.scss';
 
-interface FreezableProps {
+interface FreezableProps extends Omit<BoxProps, 'children'> {
   children: (props: { frozen: boolean; FrozenIcon: () => JSX.Element | null }) => ReactElement;
 }
 
-export default function Freezable({ children }: FreezableProps) {
+export default function Freezable({ children, className, ...props }: FreezableProps) {
   const { frozen } = useFrozen();
 
   const FrozenIcon = () => (frozen ? <IoSnowSharp /> : null);
 
   if (frozen) {
-    return <div className={style.frozen}>{children({ frozen, FrozenIcon })}</div>;
+    return (
+      <Box className={cx([style.frozen, className])} {...props}>
+        {children({ frozen, FrozenIcon })}
+      </Box>
+    );
   }
 
-  return children({ frozen, FrozenIcon });
+  return (
+    <Box className={className} {...props}>
+      {children({ frozen, FrozenIcon })}
+    </Box>
+  );
 }
