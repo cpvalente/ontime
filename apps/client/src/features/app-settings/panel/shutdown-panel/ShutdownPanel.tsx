b64b154330c0ab4450dffdc60cf64af429c54bfa
Carlos Valente <carlosvalente@pm.me>
2024-12-13 09:50:32 +0100

    refactor: disable shutdown

--- apps/client/src/features/app-settings/panel/shutdown-panel/ShutdownPanel.tsx
+++ apps/client/src/features/app-settings/panel/shutdown-panel/ShutdownPanel.tsx
@@ -11,7 +11,7 @@ import {
 } from '@chakra-ui/react';
 
 import { useElectronEvent } from '../../../../common/hooks/useElectronEvent';
-import { isLocalhost } from '../../../../externals';
+import { isLocalhost, isOntimeCloud } from '../../../../externals';
 import * as Panel from '../../panel-utils/PanelUtils';
 
 export default function ShutdownPanel() {
@@ -24,18 +24,28 @@ export default function ShutdownPanel() {
     onClose();
   };
 
+  const canShutdown = isElectron || isLocalhost;
+
   return (
     <>
       <Panel.Header>Shutdown Ontime</Panel.Header>
       <Panel.Section>
-        <Panel.Paragraph>
-          This will shutdown the Ontime server. <br />
-          The runtime state will be lost, but your project is kept for next time.
-        </Panel.Paragraph>
+        {isOntimeCloud ? (
+          <Panel.Highlight>
+            For security reasons, shutting down the server must be done from the Ontime Cloud dashboard.
+          </Panel.Highlight>
+        ) : (
+          <Panel.Paragraph>
+            This will shutdown the Ontime server. <br />
+            The runtime state will be lost, but your project is kept for next time.
+          </Panel.Paragraph>
+        )}
         <Button colorScheme='red' onClick={onOpen} maxWidth='350px' isDisabled={!(isElectron || isLocalhost)}>
           Shutdown ontime
         </Button>
-        <Panel.Description>Note: Ontime can only be shutdown from the machine it is running in.</Panel.Description>
+        {!canShutdown && (
+          <Panel.Description>Note: Ontime can only be shutdown from the machine it is running in.</Panel.Description>
+        )}
         <AlertDialog variant='ontime' isOpen={isOpen} leastDestructiveRef={cancelRef} onClose={onClose}>
           <AlertDialogOverlay>
             <AlertDialogContent>
@@ -49,7 +59,7 @@ export default function ShutdownPanel() {
                 <Button ref={cancelRef} onClick={onClose} variant='ontime-ghosted-white'>
                   Cancel
                 </Button>
-                <Button colorScheme='red' onClick={sendShutdown} ml={4}>
+                <Button colorScheme='red' onClick={sendShutdown} disabled={!canShutdown}>
                   Shutdown
                 </Button>
               </AlertDialogFooter>
