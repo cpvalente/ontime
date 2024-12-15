ed65a4db678a56093c3884f1a3525a4a7795992d
Carlos Valente <carlosvalente@pm.me>
2024-12-15 10:57:11 +0100

    refactor: smaller settings

--- apps/client/src/views/cuesheet/cuesheet-table-settings/CuesheetTableSettings.tsx
+++ apps/client/src/views/cuesheet/cuesheet-table-settings/CuesheetTableSettings.tsx
@@ -1,15 +1,15 @@
 import { memo, ReactNode } from 'react';
-import { Button, Checkbox, Switch } from '@chakra-ui/react';
+import { Button, Checkbox } from '@chakra-ui/react';
 import { Column } from '@tanstack/react-table';
 import { OntimeRundownEntry } from 'ontime-types';
 
-import { useCuesheetSettings } from '../store/cuesheetSettingsStore';
+import * as Editor from '../../../features/editors/editor-utils/EditorUtils';
 
 import style from './CuesheetTableSettings.module.scss';
 
 // reusable button styles
 const buttonProps = {
-  size: 'sm',
+  size: 'xs',
   variant: 'ontime-subtle',
 };
 
@@ -22,26 +22,12 @@ interface CuesheetTableSettingsProps {
 
 function CuesheetTableSettings(props: CuesheetTableSettingsProps) {
   const { columns, handleResetResizing, handleResetReordering, handleClearToggles } = props;
-  const {
-    followSelected,
-    showIndexColumn,
-    toggleFollow,
-    showPrevious,
-    togglePreviousVisibility,
-    showDelayBlock,
-    hideSeconds,
-    showDelayedTimes,
-    toggleIndexColumn,
-    toggleDelayedTimes,
-    toggleDelayVisibility,
-    toggleSecondsVisibility,
-  } = useCuesheetSettings();
 
   return (
     <div className={style.tableSettings}>
-      <div className={style.leftPanel}>
-        <div className={style.sectionTitle}>Toggle column visibility</div>
-        <div className={style.options}>
+      <div>
+        <Editor.Label className={style.sectionTitle}>Toggle column visibility</Editor.Label>
+        <div className={style.row}>
           {columns.map((column) => {
             const columnHeader = column.columnDef.header;
             const visible = column.getIsVisible();
@@ -57,47 +43,20 @@ function CuesheetTableSettings(props: CuesheetTableSettingsProps) {
             );
           })}
         </div>
-        <div className={style.sectionTitle}>Table Options</div>
-        <div className={style.options}>
-          <label className={style.option}>
-            <Switch variant='ontime' size='sm' isChecked={followSelected} onChange={() => toggleFollow()} />
-            Follow selected event
-          </label>
-          <label className={style.option}>
-            <Switch variant='ontime' size='sm' isChecked={showPrevious} onChange={() => togglePreviousVisibility()} />
-            Show past events
-          </label>
-          <label className={style.option}>
-            <Switch variant='ontime' size='sm' isChecked={showIndexColumn} onChange={() => toggleIndexColumn()} />
-            Show Event Order
-          </label>
-        </div>
-        <div className={style.sectionTitle}>Delay Flow</div>
-        <div className={style.options}>
-          <label className={style.option}>
-            <Switch variant='ontime' size='sm' isChecked={showDelayedTimes} onChange={() => toggleDelayedTimes()} />
-            Show delayed times
-          </label>
-          <label className={style.option}>
-            <Switch variant='ontime' size='sm' isChecked={showDelayBlock} onChange={() => toggleDelayVisibility()} />
-            Show delay blocks
-          </label>
-          <label className={style.option}>
-            <Switch variant='ontime' size='sm' isChecked={hideSeconds} onChange={() => toggleSecondsVisibility()} />
-            Hide seconds
-          </label>
-        </div>
       </div>
-      <div className={style.rightPanel}>
-        <Button onClick={handleClearToggles} {...buttonProps}>
-          Show All
-        </Button>
-        <Button onClick={handleResetResizing} {...buttonProps}>
-          Reset Resizing
-        </Button>
-        <Button onClick={handleResetReordering} {...buttonProps}>
-          Reset Reordering
-        </Button>
+      <div className={style.column}>
+        <Editor.Label className={style.sectionTitle}>Reset Options</Editor.Label>
+        <div className={style.row}>
+          <Button onClick={handleClearToggles} {...buttonProps}>
+            Show All
+          </Button>
+          <Button onClick={handleResetResizing} {...buttonProps}>
+            Reset Resizing
+          </Button>
+          <Button onClick={handleResetReordering} {...buttonProps}>
+            Reset Reordering
+          </Button>
+        </div>
       </div>
     </div>
   );
