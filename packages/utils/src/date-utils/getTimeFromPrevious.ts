3002680c9d6f6c8dee71a9ce3ac3a5a75801ce9d
arc-alex <ac@omnivox.dk>
2024-12-18 20:16:18 +0100

    refactor: `getTimeFromPrevious`

--- packages/utils/src/date-utils/getTimeFromPrevious.ts
+++ packages/utils/src/date-utils/getTimeFromPrevious.ts
@@ -1,45 +1,28 @@
-import { checkIsNextDay } from './checkIsNextDay.js';
 import { dayInMs } from './conversionUtils.js';
 
 /**
  * Utility returns the time elapsed (gap or overlap) from the previous
  * It uses deconstructed parameters to simplify implementation in UI
+ * @param currentStart the start time of the current event in ms
+ * @param currentDayOffset total number of days the current event is offset by
+ * @param previousStart the start time of the previous event in ms
+ * @param previousDuration the duration of the previous event in ms
+ * @param previousDayOffset total number of days the previous event is offset by
  */
 export function getTimeFromPrevious(
   currentStart: number,
+  currentDayOffset: number,
   previousStart?: number,
-  previousEnd?: number,
   previousDuration?: number,
+  previousDayOffset?: number,
 ): number {
   // there is no previous event
-  if (previousStart === undefined || previousEnd === undefined || previousDuration === undefined) {
+  if (previousStart === undefined || previousDuration === undefined || previousDayOffset === undefined) {
     return 0;
   }
 
-  // event is linked to previous
-  if (currentStart === previousEnd) {
-    return 0;
-  }
+  const currentStartDayAdjusted = currentDayOffset * dayInMs + currentStart;
+  const previousEndDayAdjusted = previousDayOffset * dayInMs + previousStart + previousDuration;
 
-  // event is the day after
-  if (checkIsNextDay(previousStart, currentStart, previousDuration)) {
-    // time from previous is difference between normalised start and previous end
-    return currentStart + dayInMs - previousEnd;
-  }
-
-  // event has a gap from previous
-  if (currentStart > previousEnd) {
-    // time from previous is difference between start and previous end
-    return currentStart - previousEnd;
-  }
-
-  // event overlaps with previous
-  const overlap = previousEnd - currentStart;
-  if (overlap > 0) {
-    // time is a negative number indicating the amount of overlap
-    return -overlap;
-  }
-
-  // we need to make sure we return a number, but there are no business cases for this
-  return 0;
+  return currentStartDayAdjusted - previousEndDayAdjusted;
 }
