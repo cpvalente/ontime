4cbe25a845cbe5b168f87a03d0be001e44e35308
Carlos Valente <carlosvalente@pm.me>
2024-12-20 09:36:03 +0100

    refactor: simplify update functions

--- apps/client/src/declarations/declaration.d.ts
+++ apps/client/src/declarations/declaration.d.ts
@@ -6,6 +6,9 @@ declare module '*.scss' {
 type ListenerType = (event: 'string', args: unknown[]) => void;
 
 declare global {
+  /**
+   * Register the electron properties
+   */
   interface Window {
     ipcRenderer: {
       send: (channel: string, args?: string | object) => void;
@@ -17,6 +20,19 @@ declare global {
   }
 }
 
+/**
+ * We pass a custom property to the table meta to allow field update
+ */
+declare module '@tanstack/react-table' {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  interface TableMeta<TData extends RowData> {
+    handleUpdate: (rowIndex: number, accessor: string, payload: string, isCustom: boolean) => void;
+  }
+}
+
+/**
+ * Allow passing CSS Properties
+ */
 declare module 'react' {
   interface CSSProperties {
     [key: `--${string}`]: string | number;
