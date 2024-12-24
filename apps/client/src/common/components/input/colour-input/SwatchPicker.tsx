bad7093442a561eaa8eb2ef6b497e39f4adbe728
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2024-12-25 00:07:39 +0530

    refactor: icon color util

--- apps/client/src/common/components/input/colour-input/SwatchPicker.tsx
+++ apps/client/src/common/components/input/colour-input/SwatchPicker.tsx
@@ -1,5 +1,6 @@
 import { useCallback } from 'react';
 import { IoEyedrop } from '@react-icons/all-files/io5/IoEyedrop';
+import Color from 'color';
 
 import PopoverPicker from '../../../../common/components/input/popover-picker/PopoverPicker';
 import { debounce } from '../../../../common/utils/debounce';
@@ -13,21 +14,12 @@ interface SwatchPickerProps {
   onChange: (name: string) => void;
 }
 
-function isLightColor(color: string) {
-  const hex = color.replace('#', '');
-  const c_r = parseInt(hex.substring(0, 0 + 2), 16);
-  const c_g = parseInt(hex.substring(2, 2 + 2), 16);
-  const c_b = parseInt(hex.substring(4, 4 + 2), 16);
-  const brightness = (c_r * 299 + c_g * 587 + c_b * 114) / 1000;
-  return brightness > 155;
-}
-
 export default function SwatchPicker(props: SwatchPickerProps) {
   const { color, onChange, isSelected } = props;
 
   const classes = cx([style.swatch, isSelected ? style.selected : null, style.selectable]);
 
-  const iconColor = isLightColor(color) && isSelected ? '#000000' : '#FFFFFF';
+  const iconColor = Color(color).isLight() && isSelected ? '#000000' : '#FFFFFF';
 
   const debouncedOnChange = useCallback(
     debounce((newValue: string) => {
