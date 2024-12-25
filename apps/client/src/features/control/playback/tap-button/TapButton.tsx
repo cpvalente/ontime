1afb27b3606b0f0468c7f5ee7d0236579be12e91
arc-alex <ac@omnivox.dk>
2024-12-25 13:39:16 +0100

    remove props that are part of the default `HTMLButtonElement`

--- apps/client/src/features/control/playback/tap-button/TapButton.tsx
+++ apps/client/src/features/control/playback/tap-button/TapButton.tsx
@@ -6,30 +6,17 @@ import { cx } from '../../../../common/utils/styleUtils';
 import style from './TapButton.module.scss';
 
 interface TapButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  disabled?: boolean;
   aspect?: 'normal' | 'square' | 'fill' | 'tight';
-  free?: boolean;
-  onClick: () => void;
   theme?: Playback | 'neutral';
   active?: boolean;
-  className?: string;
 }
 
 const TapButton = forwardRef((props: PropsWithChildren<TapButtonProps>, ref: ForwardedRef<HTMLButtonElement>) => {
-  const {
-    children,
-    disabled,
-    onClick,
-    theme = 'neutral',
-    aspect = 'normal',
-    active,
-    className,
-    ...elementProps
-  } = props;
+  const { children, theme = 'neutral', aspect = 'normal', active, className, ...elementProps } = props;
   const classes = cx([style.tapButton, className, style[theme], style[aspect], active ? style.active : null]);
 
   return (
-    <button className={classes} disabled={disabled} type='button' onClick={onClick} ref={ref} {...elementProps}>
+    <button className={classes} type='button' ref={ref} {...elementProps}>
       {children}
     </button>
   );
