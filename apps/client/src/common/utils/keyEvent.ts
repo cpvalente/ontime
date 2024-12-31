8da51380d68d3e9f82942e596f7ffb67bc342613
Carlos Valente <carlosvalente@pm.me>
2024-12-31 10:57:42 +0100

    refactor: extract utility

--- apps/client/src/common/utils/keyEvent.ts
+++ apps/client/src/common/utils/keyEvent.ts
@@ -7,3 +7,11 @@ export function isKeyEnter<T>(event: KeyboardEvent<T>): boolean {
 export function isKeyEscape<T>(event: KeyboardEvent<T>): boolean {
   return event.key === 'Escape';
 }
+
+export function preventEscape<T>(event: KeyboardEvent<T>, callback?: () => void): void {
+  if (isKeyEscape(event)) {
+    event.preventDefault();
+    event.stopPropagation();
+    callback?.();
+  }
+}
