13eadeb0afd80cda98477ffbac9726fc084c3997
Alex Christoffer Rasmussen <ac@omnivox.dk>
2025-01-06 20:26:06 +0100

    fix download (#1434)
    * fix download
    
    * update test db
    
    * create download test

--- apps/client/src/common/api/db.ts
+++ apps/client/src/common/api/db.ts
@@ -12,7 +12,7 @@ const dbPath = `${apiEntryUrl}/db`;
  * HTTP request to the current DB
  */
 export function getDb(filename: string): Promise<AxiosResponse<DatabaseModel>> {
-  return axios.post(`${dbPath}/download/`, { filename });
+  return axios.post(`${dbPath}/download`, { filename });
 }
 
 /**
