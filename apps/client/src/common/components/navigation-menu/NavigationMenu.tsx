0dde755e686f05c5fedec2b9ef2a85f563e04d57
asharonbaltazar <asharonbaltazar@outlook.com>
2025-01-06 17:17:03 -0500

    chore: increase font size for menu

--- apps/client/src/common/components/navigation-menu/NavigationMenu.tsx
+++ apps/client/src/common/components/navigation-menu/NavigationMenu.tsx
@@ -46,7 +46,7 @@ function NavigationMenu(props: NavigationMenuProps) {
         <DrawerRoot placement='start' onOpenChange={onClose} open={isOpen} data-testid='navigation__menu'>
           <DrawerBackdrop />
           <DrawerContent>
-            <DrawerHeader>
+            <DrawerHeader fontSize='xl' fontWeight='semibold'>
               Ontime
               <DrawerCloseTrigger />
             </DrawerHeader>
