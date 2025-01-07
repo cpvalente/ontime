35b7d6d80d9ce75cab7515a65bf183b7e3987480
asharonbaltazar <asharonbaltazar@outlook.com>
2025-01-06 20:19:32 -0500

    chore: fix type

--- apps/client/src/features/editors/editor-utils/EditorUtils.tsx
+++ apps/client/src/features/editors/editor-utils/EditorUtils.tsx
@@ -1,6 +1,6 @@
 import type { HTMLAttributes, LabelHTMLAttributes } from 'react';
+import { IconBaseProps } from 'react-icons';
 import { IoArrowUp } from 'react-icons/io5';
-import { type IconBaseProps } from '@react-icons/all-files/lib';
 
 import { cx } from '../../../common/utils/styleUtils';
 
