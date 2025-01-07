818f445c931871f192506a54979915079e6662d6
arc-alex <ac@omnivox.dk>
2025-01-07 12:11:19 +0100

    refactor: getTimeFromPrevious

--- packages/utils/src/date-utils/getTimeFromPrevious.ts
+++ packages/utils/src/date-utils/getTimeFromPrevious.ts
@@ -1,4 +1,5 @@
-import { checkIsNextDay } from './checkIsNextDay.js';
+import type { OntimeEvent } from 'ontime-types';
+
 import { dayInMs } from './conversionUtils.js';
 
 /**
@@ -6,40 +7,28 @@ import { dayInMs } from './conversionUtils.js';
  * It uses deconstructed parameters to simplify implementation in UI
  */
 export function getTimeFromPrevious(
-  currentStart: number,
-  previousStart?: number,
-  previousEnd?: number,
-  previousDuration?: number,
+  current: Pick<OntimeEvent, 'timeStart' | 'dayOffset'>,
+  previous?: Pick<OntimeEvent, 'timeStart' | 'duration' | 'dayOffset'> | null,
 ): number {
   // there is no previous event
-  if (previousStart === undefined || previousEnd === undefined || previousDuration === undefined) {
+  if (!previous) {
     return 0;
   }
+
+  const currentStartDayAdjusted = current.timeStart + current.dayOffset * dayInMs;
+  const previousEndDayAdjusted = previous.timeStart + previous.duration + previous.dayOffset * dayInMs;
 
   // event is linked to previous
-  if (currentStart === previousEnd) {
+  if (currentStartDayAdjusted === previousEndDayAdjusted) {
     return 0;
-  }
-
-  // event is the day after
-  if (checkIsNextDay(previousStart, currentStart, previousDuration)) {
-    // time from previous is difference between normalised start and previous end
-    return currentStart + dayInMs - previousEnd;
   }
 
   // event has a gap from previous
-  if (currentStart > previousEnd) {
+  if (currentStartDayAdjusted > previousEndDayAdjusted) {
     // time from previous is difference between start and previous end
-    return currentStart - previousEnd;
+    return currentStartDayAdjusted - previousEndDayAdjusted;
   }
 
   // event overlaps with previous
-  const overlap = previousEnd - currentStart;
-  if (overlap > 0) {
-    // time is a negative number indicating the amount of overlap
-    return -overlap;
-  }
-
-  // we need to make sure we return a number, but there are no business cases for this
-  return 0;
+  return currentStartDayAdjusted - previousEndDayAdjusted;
 }
