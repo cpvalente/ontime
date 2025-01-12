8141ac77f4787dbe0f719b8dd6f039edbd76f4ae
Carlos Valente <carlosvalente@pm.me>
2025-01-12 09:01:30 +0100

    refactor: create mock data utilities

---
+++ apps/server/src/stores/__mocks__/runtimeState.mocks.ts
@@ -0,0 +1,46 @@
+import { TimerPhase, Playback } from 'ontime-types';
+import { deepmerge } from 'ontime-utils';
+import { RuntimeState } from '../runtimeState.js';
+
+const baseState: RuntimeState = {
+  clock: 0,
+  currentBlock: {
+    block: null,
+    startedAt: null,
+  },
+  eventNow: null,
+  publicEventNow: null,
+  eventNext: null,
+  publicEventNext: null,
+  runtime: {
+    selectedEventIndex: null,
+    numEvents: 0,
+    offset: 0,
+    plannedStart: 0,
+    plannedEnd: 0,
+    actualStart: null,
+    expectedEnd: null,
+  },
+  timer: {
+    addedTime: 0,
+    current: null,
+    duration: null,
+    elapsed: null,
+    expectedFinish: null,
+    finishedAt: null,
+    phase: TimerPhase.None,
+    playback: Playback.Stop,
+    secondaryTimer: null,
+    startedAt: null,
+  },
+  _timer: {
+    forceFinish: null,
+    totalDelay: 0,
+    pausedAt: null,
+    secondaryTarget: null,
+  },
+};
+
+export function makeRuntimeStateData(patch?: Partial<RuntimeState>) {
+  return deepmerge(baseState, patch);
+}
