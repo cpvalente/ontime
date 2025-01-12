7d9c2a9cfdb0721eee0b1387c5c83036145df6a3
Carlos Valente <carlosvalente@pm.me>
2025-01-12 09:01:30 +0100

    refactor: create mock data utilities

---
+++ apps/server/src/services/rundown-service/__mocks__/rundown.mocks.ts
@@ -0,0 +1,30 @@
+import { SupportedEvent, OntimeEvent, OntimeDelay } from 'ontime-types';
+
+const baseEvent = {
+  type: SupportedEvent.Event,
+  skip: false,
+};
+
+/**
+ * Utility to create a Ontime event
+ */
+export function makeOntimeEvent(patch: Partial<OntimeEvent>): OntimeEvent {
+  return {
+    ...baseEvent,
+    ...patch,
+  } as OntimeEvent;
+}
+
+/**
+ * Utility to create a delay event
+ */
+export function makeOntimeDelay(duration: number): OntimeDelay {
+  return { id: 'delay', type: SupportedEvent.Delay, duration };
+}
+
+/**
+ * Utility to generate a rundown of OntimeEvents form partial objects
+ */
+export function prepareTimedEvents(events: Partial<OntimeEvent>[]): OntimeEvent[] {
+  return events.map(makeOntimeEvent);
+}
