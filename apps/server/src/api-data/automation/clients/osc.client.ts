5e870d3071a51060dba5d6c80fcd11b173ed46d4
arc-alex <ac@omnivox.dk>
2025-01-12 13:48:48 +0100

    switch to osc-mini for TX

--- apps/server/src/api-data/automation/clients/osc.client.ts
+++ apps/server/src/api-data/automation/clients/osc.client.ts
@@ -1,10 +1,13 @@
 import { LogOrigin, OSCOutput } from 'ontime-types';
 
-import { Client, Message } from 'node-osc';
+import { type OscPacketInput, toBuffer as oscPacketToBuffer } from 'osc-min';
+import * as dgram from 'node:dgram';
 
 import { logger } from '../../../classes/Logger.js';
 import { type RuntimeState } from '../../../stores/runtimeState.js';
 import { parseTemplateNested, stringToOSCArgs } from '../automation.utils.js';
+
+const udp = dgram.createSocket('udp4');
 
 /**
  * Expose possibility to send a message using OSC protocol
@@ -15,25 +18,23 @@ export function emitOSC(output: OSCOutput, state: RuntimeState) {
 }
 
 /** Parses the state and prepares payload to be emitted */
-function preparePayload(output: OSCOutput, state: RuntimeState): Message {
+function preparePayload(output: OSCOutput, state: RuntimeState): OscPacketInput {
   // check for templates in the address
   const parsedAddress = parseTemplateNested(output.address, state);
-  const message = new Message(parsedAddress);
 
   // check for templates in the arguments
   const parsedArguments = output.args ? parseTemplateNested(output.args, state) : undefined;
   // check we have the correct type
-  message.append(stringToOSCArgs(parsedArguments));
-  return message;
+  const oscArguments = stringToOSCArgs(parsedArguments);
+  return { address: parsedAddress, args: oscArguments };
 }
 
 /** Emits message over transport */
-function emit(targetIP: string, targetPort: number, message: Message) {
+function emit(targetIP: string, targetPort: number, packet: OscPacketInput) {
   logger.info(LogOrigin.Rx, `Sending OSC: ${targetIP}:${targetPort}`);
 
-  const oscClient = new Client(targetIP, targetPort);
-  oscClient.send(message, () => {
-    oscClient.close();
-  });
+  const dataView = oscPacketToBuffer(packet).buffer;
+  const buffer = new Uint8Array(dataView, 0, dataView.byteLength);
+  udp.send(buffer, 0, buffer.byteLength, targetPort, targetIP);
   return;
 }
