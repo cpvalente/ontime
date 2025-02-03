3490e7d39567ad8adbf7e4f9e1d3d6c9e0f90140
arc-alex <ac@omnivox.dk>
2025-02-03 12:35:39 +0100

    remove menu

--- apps/client/src/features/app-settings/panel/feature-settings-panel/FeatureSettingsPanel.tsx
+++ apps/client/src/features/app-settings/panel/feature-settings-panel/FeatureSettingsPanel.tsx
@@ -3,13 +3,11 @@ import type { PanelBaseProps } from '../../panel-list/PanelList';
 import * as Panel from '../../panel-utils/PanelUtils';
 
 import CustomFields from './custom-fields/CustomFields';
-import ReportSettings from './ReportSettings';
 import UrlPresetsForm from './UrlPresetsForm';
 
 export default function FeatureSettingsPanel({ location }: PanelBaseProps) {
   const customFieldsRef = useScrollIntoView<HTMLDivElement>('custom', location);
   const urlPresetsRef = useScrollIntoView<HTMLDivElement>('urlpresets', location);
-  const reportRef = useScrollIntoView<HTMLDivElement>('report', location);
 
   return (
     <>
@@ -20,10 +18,6 @@ export default function FeatureSettingsPanel({ location }: PanelBaseProps) {
 
       <div ref={urlPresetsRef}>
         <UrlPresetsForm />
-      </div>
-
-      <div ref={reportRef}>
-        <ReportSettings />
       </div>
     </>
   );
