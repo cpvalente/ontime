af2dca2b63bf24773c4103a023d4d1b918b6fa8b
Carlos Valente <carlosvalente@pm.me>
2025-02-17 17:39:38 +0100

    chore: upgrade zustand

--- apps/client/src/common/hooks/useClientPath.ts
+++ apps/client/src/common/hooks/useClientPath.ts
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
+import { useShallow } from 'zustand/shallow';
 
 import { useClientStore } from '../stores/clientStore';
 import { socketSendJson } from '../utils/socket';
@@ -9,10 +10,12 @@ import { useIsOnline } from './useSocket';
 export const useClientPath = () => {
   const navigate = useNavigate();
   const { pathname, search } = useLocation();
-  const { redirect, setRedirect } = useClientStore((store) => ({
-    redirect: store.redirect,
-    setRedirect: store.setRedirect,
-  }));
+  const { redirect, setRedirect } = useClientStore(
+    useShallow((store) => ({
+      redirect: store.redirect,
+      setRedirect: store.setRedirect,
+    })),
+  );
   const isOnline = useIsOnline();
 
   // notify of client path changes
