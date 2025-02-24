e0f2830072fcc105c8e13207213a49123a66bbc3
Carlos Valente <carlosvalente@pm.me>
2025-02-24 22:19:36 +0100

    refactor: review studio design

---
+++ apps/client/src/views/studio/StudioClock.tsx
@@ -0,0 +1,68 @@
+import { useViewportSize } from '@mantine/hooks';
+
+import { cx } from '../../common/utils/styleUtils';
+import { formatTime } from '../../common/utils/time';
+import SuperscriptTime from '../../features/viewers/common/superscript-time/SuperscriptTime';
+
+import { getLargeClockData } from './studioClock.utils';
+
+import './StudioClock.scss';
+
+const activeIndicators = [...Array(12).keys()];
+const secondsIndicators = [...Array(60).keys()];
+
+interface StudioClockProps {
+  onAir: boolean;
+  clock: number;
+  hideCards: boolean;
+}
+
+export default function StudioClock({ onAir, clock, hideCards }: StudioClockProps) {
+  const { width } = useViewportSize();
+
+  // if we are on mobile and have to show the cards
+  if (width < 800 && !hideCards) {
+    return <StudioClockMobile clock={clock} onAir={onAir} />;
+  }
+
+  const { seconds, display, meridian } = getLargeClockData(clock);
+
+  return (
+    <div className='studio__clock'>
+      <div className='clock-container'>
+        {secondsIndicators.map((i) => {
+          return (
+            <div
+              key={i}
+              className={cx(['tick', i <= seconds && 'tick--active'])}
+              style={{ transform: `rotate(${180 + i * 6}deg) translateY(var(--half-size))` }}
+            />
+          );
+        })}
+        {activeIndicators.map((i) => (
+          <div
+            key={i}
+            className='tick tick--active'
+            style={{
+              transform: `rotate(${180 + i * 30}deg) translateX(var(--smaller-half-size))`,
+            }}
+          />
+        ))}
+        <div className={cx(['ampm', Boolean(meridian) && 'ampm--active'])}>{meridian}</div>
+        <div className='time time--large'>{display}</div>
+        <div className={cx(['on-air', onAir && 'on-air--active'])}>ON AIR</div>
+      </div>
+    </div>
+  );
+}
+
+function StudioClockMobile({ clock, onAir }: Omit<StudioClockProps, 'hideCards'>) {
+  const displayClock = formatTime(clock);
+
+  return (
+    <div className='studio__clock studio__clock--small'>
+      <SuperscriptTime className='time time--small' time={displayClock} />
+      <div className={cx(['on-air', onAir && 'on-air--active'])}>ON AIR</div>
+    </div>
+  );
+}
---
+++ apps/client/src/views/studio/StudioTimers.tsx
@@ -0,0 +1,147 @@
+import { OntimeEvent, Playback, Runtime, TimerPhase, TimerState, ViewSettings } from 'ontime-types';
+import { millisToString } from 'ontime-utils';
+
+import { cx } from '../../common/utils/styleUtils';
+import { useTranslation } from '../../translation/TranslationProvider';
+import { getTimerColour } from '../utils/presentation.utils';
+
+import { getFormattedEventData, getFormattedScheduleTimes } from './studioTimers.utils';
+
+import './StudioTimers.scss';
+
+interface StudioTimersProps {
+  runtime: Runtime;
+  time: TimerState;
+  eventNow: OntimeEvent | null;
+  eventNext: OntimeEvent | null;
+  auxTimer: number;
+  timerMessage: string;
+  secondaryMessage: string;
+  viewSettings: ViewSettings;
+}
+
+export default function StudioTimers({
+  runtime,
+  time,
+  eventNow,
+  eventNext,
+  auxTimer,
+  timerMessage,
+  secondaryMessage,
+  viewSettings,
+}: StudioTimersProps) {
+  const { getLocalizedString } = useTranslation();
+
+  const schedule = getFormattedScheduleTimes(runtime);
+  const event = getFormattedEventData(eventNow, time);
+  const eventNextTitle = eventNext?.title || '-';
+  const formattedAuxTimer = millisToString(auxTimer);
+  const formattedTimerMessage = timerMessage || '-';
+  const formattedSecondaryMessage = secondaryMessage || '-';
+
+  // gather presentation styles
+  const timerColour = getTimerColour(viewSettings, time.phase === TimerPhase.Warning, time.phase === TimerPhase.Danger);
+
+  return (
+    <div className='studio__timers'>
+      <div className='card' id='card-schedule'>
+        <div className='card__row'>
+          <div>
+            <div className='label'>{getLocalizedString('common.started_at')}</div>
+            <div className='runtime-timer'>{schedule.actualStart}</div>
+          </div>
+          <div>
+            <div className='label center'>Offset</div>
+            <div
+              className={cx([
+                'runtime-timer',
+                'center',
+                !eventNow && 'muted',
+                runtime.offset <= 0 ? 'behind' : 'ahead',
+              ])}
+            >
+              {schedule.offset}
+            </div>
+          </div>
+          <div>
+            <div className='label right'>{getLocalizedString('common.projected_end')}</div>
+            <div className='runtime-timer right'>{schedule.expectedEnd}</div>
+          </div>
+        </div>
+      </div>
+
+      <div className='card' id='card-event-now'>
+        <div className='card__row'>
+          <div>
+            <div className='label'>{getLocalizedString('common.now')}</div>
+            <div className='title'>{event.title}</div>
+          </div>
+          <div>
+            <div className='label right'>{getLocalizedString('common.next')}</div>
+            <div className='title right'>{eventNextTitle}</div>
+          </div>
+        </div>
+
+        <div className='card__row'>
+          <div>
+            <div className='label'>{getLocalizedString('common.started_at')}</div>
+            <div className='runtime-timer'>{event.startedAt}</div>
+          </div>
+          <div>
+            <div className='label' />
+            <div
+              className={cx([
+                'event-timer',
+                time.phase === TimerPhase.Overtime && 'event-timer--finished',
+                time.playback === Playback.Pause && 'event-timer--paused',
+              ])}
+              style={{
+                '--phase-color': timerColour,
+              }}
+              data-phase={time.phase}
+            >
+              {event.timer}
+            </div>
+          </div>
+          <div>
+            <div className='label right'>{getLocalizedString('common.projected_end')}</div>
+            <div className='runtime-timer right'>{event.expectedEnd}</div>
+          </div>
+        </div>
+      </div>
+
+      <div className='card' id='card-aux'>
+        <div className='card__row'>
+          <div>
+            <div className='label'>Aux 1</div>
+            <div className='extra'>{formattedAuxTimer}</div>
+          </div>
+
+          <div>
+            <div className='label center'>Aux 2</div>
+            <div className='extra center'>NOT YET</div>
+          </div>
+
+          <div>
+            <div className='label right'>Aux 3</div>
+            <div className='extra right'>NOT YET</div>
+          </div>
+        </div>
+      </div>
+
+      <div className='card' id='card-timer-message'>
+        <div>
+          <div className='label'>Timer message</div>
+          <div className={cx(['extra', !formattedTimerMessage && 'muted'])}>{formattedTimerMessage}</div>
+        </div>
+      </div>
+
+      <div className='card' id='card-secondary-message'>
+        <div>
+          <div className='label'>Secondary message</div>
+          <div className={cx(['extra', !formattedSecondaryMessage && 'muted'])}>{formattedSecondaryMessage}</div>
+        </div>
+      </div>
+    </div>
+  );
+}
