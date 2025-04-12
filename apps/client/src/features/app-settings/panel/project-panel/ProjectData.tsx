2bd10fae514de161e50d260e7889c16f06b13f66
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2025-04-12 17:21:16 +0530

    refactor: add custom field input under project data

--- apps/client/src/features/app-settings/panel/project-panel/ProjectData.tsx
+++ apps/client/src/features/app-settings/panel/project-panel/ProjectData.tsx
@@ -1,5 +1,5 @@
-import { ChangeEvent, useEffect, useRef } from 'react';
-import { useForm } from 'react-hook-form';
+import { ChangeEvent, Fragment, useEffect, useRef } from 'react';
+import { useFieldArray, useForm } from 'react-hook-form';
 import { IoDownloadOutline, IoTrash } from 'react-icons/io5';
 import { Button, Input, Textarea } from '@chakra-ui/react';
 import { type ProjectData } from 'ontime-types';
@@ -25,6 +25,7 @@ export default function ProjectData() {
     formState: { isSubmitting, isValid, isDirty, errors },
     setError,
     watch,
+    control,
     setValue,
   } = useForm({
     defaultValues: data,
@@ -32,6 +33,11 @@ export default function ProjectData() {
     resetOptions: {
       keepDirtyValues: true,
     },
+  });
+
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: 'custom',
   });
 
   // reset form values if data changes
@@ -75,6 +81,10 @@ export default function ProjectData() {
     setValue('projectLogo', null, {
       shouldDirty: true,
     });
+  };
+
+  const handleAddCustom = () => {
+    append({ title: '', value: '' });
   };
 
   const onSubmit = async (formData: ProjectData) => {
@@ -231,6 +241,42 @@ export default function ProjectData() {
               {...register('backstageUrl')}
             />
           </label>
+          <Panel.ListItem>
+            <Panel.Field title='Custom data' description='' />
+            <Button variant='ontime-subtle' onClick={handleAddCustom}>
+              +
+            </Button>
+          </Panel.ListItem>
+          {fields.length === 0 && <p>Project has no metadata currently</p>}
+          {fields.length > 0 &&
+            fields.map((field, idx) => (
+              <Panel.InlineElements key={field.id}>
+                <Panel.Paragraph>{idx + 1}.</Panel.Paragraph>
+                <label>
+                  Title
+                  <Input
+                    variant='ontime-filled'
+                    size='sm'
+                    placeholder={field.title}
+                    autoComplete='off'
+                    {...register(`custom.${idx}.title`)}
+                  />
+                </label>
+                <label>
+                  Value
+                  <Input
+                    variant='ontime-filled'
+                    size='sm'
+                    placeholder={field.value}
+                    autoComplete='off'
+                    {...register(`custom.${idx}.value`)}
+                  />
+                </label>
+                <Button variant='ontime-ghosted' onClick={() => remove(idx)}>
+                  X
+                </Button>
+              </Panel.InlineElements>
+            ))}
         </Panel.Section>
       </Panel.Card>
     </Panel.Section>
