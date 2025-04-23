4b6316f0478cb77f92c6f7150cfaefcc2daaaf40
arc-alex <ac@omnivox.dk>
2025-04-23 08:40:02 +0200

    rename variables

--- apps/client/src/features/control/playback/add-time/AddTime.tsx
+++ apps/client/src/features/control/playback/add-time/AddTime.tsx
@@ -17,29 +17,29 @@ interface AddTimeProps {
 
 export default function AddTime(props: AddTimeProps) {
   const { playback } = props;
-  const [time, setTime] = useLocalStorage({ key: 'add-time', defaultValue: 300_000 }); // 5 minutes
+  const [timeInMs, setTime] = useLocalStorage({ key: 'add-time', defaultValue: 300_000 }); // 5 minutes
 
   const handleTimeChange = (_field: string, value: string) => {
-    const newTime = parseUserTime(value);
+    const newTimeInMs = parseUserTime(value);
     // cap add time to 1 hour
-    setTime(Math.min(newTime, MILLIS_PER_HOUR));
+    setTime(Math.min(newTimeInMs, MILLIS_PER_HOUR));
   };
 
   const handleAddTime = (direction: 'add' | 'remove') => {
     // API expects input in milliseconds
     if (direction === 'add') {
-      setPlayback.addTime(time);
+      setPlayback.addTime(timeInMs);
     } else {
-      setPlayback.addTime(-1 * time);
+      setPlayback.addTime(-1 * timeInMs);
     }
   };
 
   const canAddTime = playback === Playback.Play || playback === Playback.Pause;
-  const doDisableButtons = !canAddTime || time === 0;
+  const doDisableButtons = !canAddTime || timeInMs === 0;
 
   return (
     <div className={style.addTime}>
-      <TimeInput name='addtime' submitHandler={handleTimeChange} time={time} placeholder='Add time' />
+      <TimeInput name='addtime' submitHandler={handleTimeChange} time={timeInMs} placeholder='Add time' />
       <div className={style.addButtons}>
         <Tooltip label='Remove time' openDelay={tooltipDelayMid} shouldWrapChildren>
           <TapButton onClick={() => handleAddTime('remove')} disabled={doDisableButtons} className={style.tallButtons}>
