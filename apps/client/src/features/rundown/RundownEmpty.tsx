66ee8674ee17a132bf618d80b3715b9f3752ca50
Carlos Valente <carlosvalente@pm.me>
2025-05-17 14:58:46 +0200

    feat: create block from rundown empty

--- apps/client/src/features/rundown/RundownEmpty.tsx
+++ apps/client/src/features/rundown/RundownEmpty.tsx
@@ -1,12 +1,13 @@
 import { IoAdd } from 'react-icons/io5';
 import { Button } from '@chakra-ui/react';
+import { SupportedEntry } from 'ontime-types';
 
 import Empty from '../../common/components/state/Empty';
 
 import style from './Empty.module.scss';
 
 interface RundownEmptyProps {
-  handleAddNew: () => void;
+  handleAddNew: (type: SupportedEntry) => void;
 }
 
 export default function RundownEmpty(props: RundownEmptyProps) {
@@ -14,10 +15,16 @@ export default function RundownEmpty(props: RundownEmptyProps) {
 
   return (
     <div className={style.empty}>
-      <Empty style={{ marginTop: '7vh', marginBottom: '1.5rem' }} />
-      <Button onClick={handleAddNew} variant='ontime-filled' leftIcon={<IoAdd />}>
-        Create Event
-      </Button>
+      <Empty style={{ marginTop: '5vh', marginBottom: '3rem' }} />
+      <div className={style.inline}>
+        <Button onClick={() => handleAddNew(SupportedEntry.Event)} variant='ontime-filled' leftIcon={<IoAdd />}>
+          Create Event
+        </Button>
+
+        <Button onClick={() => handleAddNew(SupportedEntry.Block)} variant='ontime-filled' leftIcon={<IoAdd />}>
+          Create Block
+        </Button>
+      </div>
     </div>
   );
 }
