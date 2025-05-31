6f9580a04f73e077ae61ef573375f2da3c688841
arc-alex <ac@omnivox.dk>
2025-05-31 19:18:25 +0200

    no need for constant update of info, just fetch when looking at it

--- apps/client/src/common/hooks-query/useInfo.ts
+++ apps/client/src/common/hooks-query/useInfo.ts
@@ -1,20 +1,18 @@
 import { useQuery } from '@tanstack/react-query';
 import { GetInfo, RefetchKey } from 'ontime-types';
 
-import { queryRefetchIntervalSlow } from '../../ontimeConfig';
 import { getInfo } from '../api/session';
 import { ontimePlaceholderInfo } from '../models/Info';
 
 export default function useInfo() {
-  const { data, status, isError, refetch, isFetching } = useQuery<GetInfo>({
+  const { data, status, isError, isFetching } = useQuery<GetInfo>({
     queryKey: [RefetchKey.APP_INFO],
     queryFn: getInfo,
     placeholderData: (previousData, _previousQuery) => previousData,
     retry: 5,
     retryDelay: (attempt) => attempt * 2500,
-    refetchInterval: queryRefetchIntervalSlow,
     networkMode: 'always',
   });
 
-  return { data: data ?? ontimePlaceholderInfo, status, isError, refetch, isFetching };
+  return { data: data ?? ontimePlaceholderInfo, status, isError, isFetching };
 }
