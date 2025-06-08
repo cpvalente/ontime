e1a45112420a86a0dff8992b68f24a74279af8cd
Carlos Valente <carlosvalente@pm.me>
2025-06-08 14:46:22 +0200

    refactor: small behaviour tweaks

--- apps/client/src/views/backstage/Backstage.tsx
+++ apps/client/src/views/backstage/Backstage.tsx
@@ -111,7 +111,7 @@ export default function Backstage(props: BackstageProps) {
     <div className={`backstage ${isMirrored ? 'mirror' : ''}`} data-testid='backstage-view'>
       <ViewParamsEditor viewOptions={backstageOptions} />
       <div className='project-header'>
-        {general?.projectLogo ? <ViewLogo name={general.projectLogo} className='logo' /> : <div className='logo' />}
+        {general?.projectLogo && <ViewLogo name={general.projectLogo} className='logo' />}
         <div className='title'>{general.title}</div>
         <div className='clock-container'>
           <div className='label'>{getLocalizedString('common.time_now')}</div>
