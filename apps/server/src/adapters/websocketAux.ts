c216a84a0b119bd51b6ae554af68d7f0fd0382a6
Alex Christoffer Rasmussen <ac@omnivox.dk>
2025-06-20 12:01:55 +0200

    Revert "refactor: refetch targets is enum"
    This reverts commit e7cfb7d9d9e3bcd33ba33d5b84abd7683c53704c.

--- apps/server/src/adapters/websocketAux.ts
+++ apps/server/src/adapters/websocketAux.ts
@@ -1,10 +1,5 @@
 import { socket } from './WebsocketAdapter.js';
 
-export enum RefetchTargets {
-  Rundown = 'rundown',
-  Report = 'report',
-}
-
 /**
  * Utility function to notify clients that the REST data is stale
  * @param payload -- possible patch payload
