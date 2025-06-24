bb148e07cb6c73713ad8641ead6ca624301624a0
Carlos Valente <carlosvalente@pm.me>
2025-06-24 16:56:52 +0200

    refactor: extract button styles

--- apps/client/src/common/components/buttons/Button.tsx
+++ apps/client/src/common/components/buttons/Button.tsx
@@ -5,15 +5,20 @@ import { cx } from '../../utils/styleUtils';
 import style from './Button.module.scss';
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  variant?: 'subtle' | 'primary' | 'destructive' | 'subtle-destructive';
+  variant?: 'primary' | 'subtle' | 'subtle-white' | 'destructive' | 'subtle-destructive';
   size?: 'medium' | 'large' | 'xlarge';
+  fluid?: boolean;
 }
 
 export default function Button(props: ButtonProps) {
-  const { className, children, variant = 'subtle', size = 'medium', ...buttonProps } = props;
+  const { className, children, variant = 'subtle', size = 'medium', fluid, ...buttonProps } = props;
 
   return (
-    <button className={cx([style.baseButton, style[variant], style[size], className])} type='button' {...buttonProps}>
+    <button
+      className={cx([style.baseButton, style[variant], style[size], fluid && style.fluid, className])}
+      type='button'
+      {...buttonProps}
+    >
       {children}
     </button>
   );
