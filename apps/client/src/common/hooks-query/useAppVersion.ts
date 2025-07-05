d740ea94c96c21d6b9405a83f2ff22d72cbab0ee
arc-alex <ac@omnivox.dk>
2025-07-05 22:42:02 +0200

    refactor: useAppVersion

--- apps/client/src/common/hooks-query/useAppVersion.ts
+++ apps/client/src/common/hooks-query/useAppVersion.ts
@@ -6,28 +6,24 @@ import { isLocalhost } from '../../externals';
 import { APP_VERSION } from '../api/constants';
 import { getLatestVersion, HasUpdate } from '../api/external';
 
-const placeholder: HasUpdate & { hasUpdates: boolean } = { url: '', version: '', hasUpdates: false };
+const initialData: HasUpdate & { hasUpdates: boolean } = { url: '', version: '', hasUpdates: false };
 
 export default function useAppVersion() {
-  const {
-    data: fetchData,
-    status,
-    isFetching,
-    isError,
-    refetch,
-  } = useQuery({
+  const { data: fetchData, isError } = useQuery({
     queryKey: APP_VERSION,
     queryFn: getLatestVersion,
     placeholderData: (previousData, _previousQuery) => previousData,
+    // this is a very lazy piece of data therefore we very really ask it to refetch
     refetchOnReconnect: false,
     retry: false,
     staleTime: dayInMs,
+    initialData,
     enabled: isLocalhost,
   });
 
   const hasUpdates = fetchData?.version && !fetchData.version.includes(version);
 
-  const data = fetchData ? { ...fetchData, hasUpdates } : placeholder;
+  const data = { ...fetchData, hasUpdates };
 
-  return { data, placeholder, status, isFetching, isError, refetch };
+  return { data, isError };
 }
