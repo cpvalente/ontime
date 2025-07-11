27b8568d26c9c37d4a637a0e0ba47fb8859e903c
Carlos Valente <carlosvalente@pm.me>
2025-07-11 18:42:09 +0200

    feat: implement custom project data image

--- apps/client/src/features/app-settings/panel/project-panel/ProjectCreateForm.tsx
+++ apps/client/src/features/app-settings/panel/project-panel/ProjectCreateForm.tsx
@@ -1,16 +1,11 @@
 import { useEffect, useState } from 'react';
-import { useFieldArray, useForm } from 'react-hook-form';
-import { IoAdd, IoTrash } from 'react-icons/io5';
-import { useQueryClient } from '@tanstack/react-query';
+import { useForm } from 'react-hook-form';
 
-import { PROJECT_LIST } from '../../../../common/api/constants';
 import { createProject } from '../../../../common/api/db';
 import { maybeAxiosError } from '../../../../common/api/utils';
 import Button from '../../../../common/components/buttons/Button';
 import Input from '../../../../common/components/input/input/Input';
-import Textarea from '../../../../common/components/input/textarea/Textarea';
 import { preventEscape } from '../../../../common/utils/keyEvent';
-import { documentationUrl } from '../../../../externals';
 import * as Panel from '../../panel-utils/PanelUtils';
 
 import style from './ProjectPanel.module.scss';
@@ -31,12 +26,10 @@ export default function ProjectCreateForm(props: ProjectCreateFromProps) {
   const { onClose } = props;
 
   const [error, setError] = useState<string | null>(null);
-  const queryClient = useQueryClient();
 
   const {
     handleSubmit,
     register,
-    control,
     formState: { isSubmitting, isValid },
     setFocus,
   } = useForm<ProjectCreateFormValues>({
@@ -45,11 +38,6 @@ export default function ProjectCreateForm(props: ProjectCreateFromProps) {
     resetOptions: {
       keepDirtyValues: true,
     },
-  });
-
-  const { fields, append, remove } = useFieldArray({
-    control,
-    name: 'custom',
   });
 
   // set focus to first field
@@ -63,20 +51,12 @@ export default function ProjectCreateForm(props: ProjectCreateFromProps) {
 
       const filename = values.title ?? 'untitled';
 
-      await createProject({
-        ...values,
-        filename,
-      });
+      await createProject({ filename });
 
-      await queryClient.invalidateQueries({ queryKey: PROJECT_LIST });
       onClose();
     } catch (error) {
       setError(maybeAxiosError(error));
     }
-  };
-
-  const handleAddCustom = () => {
-    append({ title: '', value: '' });
   };
 
   return (
@@ -100,44 +80,8 @@ export default function ProjectCreateForm(props: ProjectCreateFromProps) {
       <Panel.Section className={style.innerColumn}>
         <label>
           Project title
-          <Input fluid maxLength={50} placeholder='Your project name' {...register('title')} />
+          <Input fluid placeholder='Your project name' {...register('title')} />
         </label>
-        <label>
-          Project description
-          <Input fluid maxLength={100} placeholder='Euro Love, Malmö 2024' {...register('description')} />
-        </label>
-        <label>
-          Project info
-          <Textarea fluid maxLength={150} placeholder='Wi-Fi password: 1234' resize='vertical' {...register('info')} />
-        </label>
-        <label>
-          Project QR code URL
-          <Input fluid placeholder={documentationUrl} {...register('url')} />
-        </label>
-        <Panel.Section>
-          <Panel.ListItem>
-            <Panel.Field title='Custom data' description='Add custom data for your project' />
-            <Button onClick={handleAddCustom}>
-              Add <IoAdd />
-            </Button>
-          </Panel.ListItem>
-          {fields.map((field, idx) => (
-            <div key={field.id} className={style.customDataItem}>
-              <Panel.Paragraph>{idx + 1}.</Panel.Paragraph>
-              <label>
-                Title
-                <Input placeholder={field.title} {...register(`custom.${idx}.title` as const)} />
-              </label>
-              <label>
-                Value
-                <Input placeholder={field.value} autoComplete='off' {...register(`custom.${idx}.value` as const)} />
-              </label>
-              <Button variant='ghosted' onClick={() => remove(idx)}>
-                <IoTrash />
-              </Button>
-            </div>
-          ))}
-        </Panel.Section>
       </Panel.Section>
     </Panel.Section>
   );
