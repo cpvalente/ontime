bb4f6e67abcdc59b2c2d0a38ee08e152cc6cbf1d
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2025-07-19 23:05:48 +0530

    refactor: validate all values of keys in translation payload

--- apps/server/src/api-data/assets/assets.validation.ts
+++ apps/server/src/api-data/assets/assets.validation.ts
@@ -2,4 +2,8 @@ import { body } from 'express-validator';
 import { requestValidationFunction } from '../validation-utils/validationFunction.js';
 
 export const validatePostCss = [body('css').isString().trim(), requestValidationFunction];
-export const validatePostTranslation = [body('translation').isObject(), requestValidationFunction];
+export const validatePostTranslation = [
+  body('translation').isObject(),
+  body('translation.*').isString().notEmpty(),
+  requestValidationFunction,
+];
