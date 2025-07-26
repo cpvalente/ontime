6b4d96dddd3fd7303eb7732a637e6412d69aadb7
Carlos Valente <carlosvalente@pm.me>
2025-07-26 09:46:56 +0200

    feat: extra info in backstage

--- apps/client/src/views/backstage/backstage.options.ts
+++ apps/client/src/views/backstage/backstage.options.ts
@@ -1,15 +1,23 @@
 import { useMemo } from 'react';
 import { useSearchParams } from 'react-router-dom';
-import { CustomFields, OntimeEvent } from 'ontime-types';
+import { CustomFields, OntimeEvent, ProjectData } from 'ontime-types';
 
 import { getTimeOption } from '../../common/components/view-params-editor/common.options';
 import { OptionTitle } from '../../common/components/view-params-editor/constants';
 import { ViewOption } from '../../common/components/view-params-editor/viewParams.types';
-import { makeOptionsFromCustomFields } from '../../common/components/view-params-editor/viewParams.utils';
+import {
+  makeOptionsFromCustomFields,
+  makeProjectDataOptions,
+} from '../../common/components/view-params-editor/viewParams.utils';
 import { scheduleOptions } from '../common/schedule/schedule.options';
 
-export const getBackstageOptions = (timeFormat: string, customFields: CustomFields): ViewOption[] => {
+export const getBackstageOptions = (
+  timeFormat: string,
+  customFields: CustomFields,
+  projectData: ProjectData,
+): ViewOption[] => {
   const secondaryOptions = makeOptionsFromCustomFields(customFields, [{ value: 'note', label: 'Note' }]);
+  const projectDataOptions = makeProjectDataOptions(projectData);
 
   return [
     { title: OptionTitle.ClockOptions, collapsible: true, options: [getTimeOption(timeFormat)] },
@@ -28,11 +36,26 @@ export const getBackstageOptions = (timeFormat: string, customFields: CustomFiel
       ],
     },
     scheduleOptions,
+    {
+      title: OptionTitle.ElementVisibility,
+      collapsible: true,
+      options: [
+        {
+          id: 'extra-info',
+          title: 'Extra info',
+          description: 'Select a project data source to show in the view',
+          type: 'option',
+          values: projectDataOptions,
+          defaultValue: '',
+        },
+      ],
+    },
   ];
 };
 
 type BackstageOptions = {
   secondarySource: keyof OntimeEvent | null;
+  extraInfo: string | null;
 };
 
 /**
@@ -43,6 +66,7 @@ function getOptionsFromParams(searchParams: URLSearchParams): BackstageOptions {
   // we manually make an object that matches the key above
   return {
     secondarySource: searchParams.get('secondary-src') as keyof OntimeEvent | null,
+    extraInfo: searchParams.get('extra-info'),
   };
 }
 
