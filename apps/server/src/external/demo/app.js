d6e6f5bb0f6eb010ec759915702489cd2ef78656
Carlos Valente <carlosvalente@pm.me>
2025-08-10 10:40:04 +0200

    docs: demo project is runtime data documentation

--- apps/server/src/external/demo/app.js
+++ apps/server/src/external/demo/app.js
@@ -4,37 +4,22 @@
  * You could use this as a starting point to creating your own interfaces
  */
 
-const mts = 1000; // millis to seconds
-const mtm = 1000 * 60; // millis to minutes
-const mth = 1000 * 60 * 60; // millis to hours
+// Data that the user needs to provide depending on the Ontime URL
+const isSecure = window.location.protocol === 'https:';
+const userProvidedSocketUrl = `${isSecure ? 'wss' : 'ws'}://${window.location.hostname}:${window.location.port}/ws`;
 
-const leftPad = (number) => {
-  return Math.floor(number).toString().padStart(2, '0');
-};
-
-const formatTimer = (number) => {
-  const millis = Math.abs(number);
-  const isNegative = number < 0;
-  return `${isNegative ? '-' : ''}${leftPad(millis / mth)}:${leftPad((millis % mth) / mtm)}:${leftPad(
-    (millis % mtm) / mts,
-  )}`;
-};
-
-function updateTimerElement(playback, timerValue) {
-  const timerElement = document.getElementById('timer');
-  if (playback === 'stop') {
-    timerElement.innerText = '--:--:--';
-  } else {
-    timerElement.innerText = formatTimer(timerValue);
-  }
-}
+connectSocket();
 
 let reconnectTimeout;
 const reconnectInterval = 1000;
 let reconnectAttempts = 0;
 
-const connectSocket = () => {
-  const websocket = new WebSocket(`ws://${window.location.hostname}:${window.location.port}/ws`);
+/**
+ * Connects to the websocket server
+ * @param {string} socketUrl
+ */
+function connectSocket(socketUrl = userProvidedSocketUrl) {
+  const websocket = new WebSocket(socketUrl);
 
   websocket.onopen = () => {
     clearTimeout(reconnectTimeout);
@@ -57,27 +42,97 @@ const connectSocket = () => {
   };
 
   websocket.onmessage = (event) => {
-    const data = JSON.parse(event.data);
+    // all objects from ontime are structured with tag and payload
+    const { tag, payload } = JSON.parse(event.data);
 
-    // all objects from ontime are structured with type and payload
-    const { type, payload } = data;
-
-    // we only need to read message type of ontime
-    switch (type) {
-      case 'ontime': {
-        // destructure known data from ontime
-        // see https://docs.getontime.no/api/data/runtime-data/
-        const { current, playback } = payload.timer;
-        updateTimerElement(playback, current);
-        break;
-      }
-      case 'ontime-timer': {
-        const { current, playback } = payload;
-        updateTimerElement(playback, current);
-        break;
-      }
+    /**
+     * runtime-data is sent
+     * - on connect with the full state
+     * - and then on every update with a patch
+     */
+    if (tag === 'runtime-data') {
+      handleOntimePayload(payload);
     }
   };
-};
+}
 
-connectSocket();
+let localData = {};
+/**
+ * Handles the ontime payload updates
+ * @param {object} payload - The payload object containing the updates
+ */
+function handleOntimePayload(payload) {
+  // 1. apply the patch into your local copy of the data
+  localData = { ...localData, ...payload };
+
+  // 2. update the UI with the new data
+  // ... timer data
+  if ('clock' in payload) updateDOM('clock', formatTimer(payload.clock));
+  if ('timer' in payload) updateDOM('timer', formatObject(payload.timer));
+  // ... rundown data
+  if ('rundown' in payload) updateDOM('rundown', formatObject(payload.rundown));
+  // ... runtime
+  if ('offset' in payload) updateDOM('offset', formatObject(payload.offset));
+  // ... relevant entries
+  if ('eventNow' in payload) updateDOM('eventNow', formatObject(payload.eventNow));
+  if ('eventNext' in payload) updateDOM('eventNext', formatObject(payload.eventNext));
+  if ('eventFlag' in payload) updateDOM('eventFlag', formatObject(payload.eventFlag));
+  if ('groupNow' in payload) updateDOM('groupNow', formatObject(payload.groupNow));
+  // ... messages service
+  if ('message' in payload) updateDOM('message', formatObject(payload.message));
+  // ... extra timers
+  if ('auxtimer1' in payload) updateDOM('auxtimer1', formatObject(payload.auxtimer1));
+  if ('auxtimer2' in payload) updateDOM('auxtimer2', formatObject(payload.auxtimer2));
+  if ('auxtimer3' in payload) updateDOM('auxtimer3', formatObject(payload.auxtimer3));
+}
+
+/**
+ * Updates the DOM with a given payload
+ * @param {string} field - The runtime data field
+ * @param {object} payload - The patch object for the field
+ */
+function updateDOM(field, payload) {
+  const domElement = document.getElementById(field);
+  if (domElement) {
+    domElement.innerText = payload;
+  }
+}
+
+// Time constants used for calculating times
+const millisToSeconds = 1000;
+const millisToMinutes = 1000 * 60;
+const millisToHours = 1000 * 60 * 60;
+
+/**
+ * Formats a timer value into a human-readable string
+ * @param {number} number - The timer value in milliseconds
+ * @returns {string} The formatted timer string
+ */
+function formatTimer(number) {
+  if (number == null) {
+    return '--:--:--';
+  }
+  const millis = Math.abs(number);
+  const isNegative = number < 0;
+  return `${isNegative ? '-' : ''}${leftPad(millis / millisToHours)}:${leftPad(
+    (millis % millisToHours) / millisToMinutes,
+  )}:${leftPad((millis % millisToMinutes) / millisToSeconds)}`;
+
+  /**
+   * Pads a number with leading zeros
+   * @param {number} number - The number to pad
+   * @returns {string} The padded number string
+   */
+  function leftPad(number) {
+    return Math.floor(number).toString().padStart(2, '0');
+  }
+}
+
+/**
+ * Stringifies an object into a pretty string
+ * @param {object} data - The data object to format
+ * @returns {string} The formatted data string
+ */
+function formatObject(data) {
+  return JSON.stringify(data, null, 2);
+}
