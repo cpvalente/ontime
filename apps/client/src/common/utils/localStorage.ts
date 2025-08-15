f3c01819060fdf007269f54409380db7efef34d1
Carlos Valente <carlosvalente@pm.me>
2025-08-15 20:28:36 +0200

    fix: storage key conflict across stages

--- apps/client/src/common/utils/localStorage.ts
+++ apps/client/src/common/utils/localStorage.ts
@@ -1,3 +1,5 @@
+import { baseURI } from '../../externals';
+
 export function booleanFromLocalStorage(key: string, fallback: boolean): boolean {
   const valueInStorage = localStorage.getItem(key);
   if (valueInStorage) {
@@ -6,4 +8,9 @@ export function booleanFromLocalStorage(key: string, fallback: boolean): boolean
     localStorage.setItem(key, String(fallback));
     return fallback;
   }
+}
+
+export function makeStageKey(key: string) {
+  if (baseURI) return `${baseURI}-${key}`;
+  return key;
 }
