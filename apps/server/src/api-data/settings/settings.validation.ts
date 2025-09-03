39e939fa653db50c9fb4dc52ec2683505a6f9504
Alex Christoffer Rasmussen <ac@omnivox.dk>
2025-09-03 11:20:03 -0400

    refactor: settings route (#1753)
    * refactor: settings route

--- apps/server/src/api-data/settings/settings.validation.ts
+++ apps/server/src/api-data/settings/settings.validation.ts
@@ -1,4 +1,4 @@
-import { body } from 'express-validator';
+import { body, checkExact } from 'express-validator';
 import { requestValidationFunction } from '../validation-utils/validationFunction.js';
 
 /**
@@ -6,16 +6,30 @@ import { requestValidationFunction } from '../validation-utils/validationFunctio
  */
 export const validateWelcomeDialog = [body('show').isBoolean(), requestValidationFunction];
 
+const pinValidator = (key: string) => {
+  return body(key)
+    .isString()
+    .trim()
+    .isLength({ min: 0, max: 4 })
+    .customSanitizer((input) => {
+      if (input.length === 0) {
+        return null;
+      }
+      return input;
+    });
+};
+
 /**
  * @description Validates object for POST /ontime/settings
  */
 export const validateSettings = [
-  body().notEmpty().withMessage('No object found in request'),
-  body('editorKey').isString().isLength({ min: 0, max: 4 }).optional({ nullable: true }),
-  body('operatorKey').isString().isLength({ min: 0, max: 4 }).optional({ nullable: true }),
-  body('timeFormat').isString().isIn(['12', '24']),
-  body('language').isString(),
-  body('serverPort').isPort().optional(),
+  pinValidator('editorKey'),
+  pinValidator('operatorKey'),
+  body('timeFormat').isString().isIn(['12', '24']).withMessage('Time format can only be "12" or "24"'),
+  body('language').isString().trim().notEmpty(),
+  body('serverPort').isPort().withMessage('Invalid value found for server port').toInt(),
+  checkExact(),
 
   requestValidationFunction,
 ];
+// TODO: dont allow other keys
