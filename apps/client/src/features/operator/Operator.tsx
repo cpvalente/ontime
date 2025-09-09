6f51e813b3423f4e633c2db03bf73d75c5330623
Carlos Valente <carlosvalente@pm.me>
2025-09-09 20:24:55 +0200

    refactor: improve automated following

--- apps/client/src/features/operator/Operator.tsx
+++ apps/client/src/features/operator/Operator.tsx
@@ -49,6 +49,7 @@ export default function Operator() {
     scrollRef,
     doFollow: !lockAutoScroll,
     topOffset: selectedOffset,
+    followTrigger: selectedEventId,
   });
 
   useWindowTitle('Operator');
