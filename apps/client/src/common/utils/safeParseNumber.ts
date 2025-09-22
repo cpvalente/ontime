4058231d6b9220a38fb0867ac4165e23cdfac92e
Carlos Valente <carlosvalente@pm.me>
2025-09-22 07:54:56 +0200

    refactor: remove unused code

--- apps/client/src/common/utils/safeParseNumber.ts
+++
@@ -1,8 +0,0 @@
-import { MaybeString } from 'ontime-types';
-
-export default function safeParseNumber(value: MaybeString, defaultValue: number = 0): number {
-  if (!value) return defaultValue;
-  const number = Number(value);
-  if (isNaN(number)) return defaultValue;
-  return number;
-}
