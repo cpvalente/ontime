7a1808e1fc487443a1884243037e3a062f6a5794
Alex Christoffer Rasmussen <ac@omnivox.dk>
2025-09-24 19:52:55 +0200

    Chore: Small todo about coerce boolean (#1789)
    * fix: coerce boolean types negative number is false
    
    * strict coerce

--- apps/server/src/utils/coerceType.ts
+++ apps/server/src/utils/coerceType.ts
@@ -33,8 +33,8 @@ export function coerceString(value: unknown): string {
  * @throws {Error} Throws an error if the value is null or undefined.
  */
 export function coerceBoolean(value: unknown): boolean {
-  if (value === undefined || typeof value === 'object') {
-    throw new Error('Invalid value received');
+  if (typeof value === 'boolean') {
+    return value;
   }
   if (typeof value === 'string') {
     const lowerCaseValue = value.toLocaleLowerCase();
@@ -52,7 +52,11 @@ export function coerceBoolean(value: unknown): boolean {
         throw new Error('Invalid value received');
     }
   }
-  return Boolean(value);
+  if (typeof value === 'number') {
+    if (value === 0) return false;
+    if (value === 1) return true;
+  }
+  throw new Error('Invalid value received');
 }
 
 /**
