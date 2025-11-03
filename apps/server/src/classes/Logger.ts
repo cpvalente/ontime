146f837be20d17664c2529fd9453baac88ffdf8a
arc-alex <ac@omnivox.dk>
2025-11-03 20:47:18 +0100

    remove all use of old timeNow()

--- apps/server/src/classes/Logger.ts
+++ apps/server/src/classes/Logger.ts
@@ -3,8 +3,8 @@ import { generateId, millisToString } from 'ontime-utils';
 
 import { socket } from '../adapters/WebsocketAdapter.js';
 import { consoleSubdued, consoleError } from '../utils/console.js';
-import { timeNow } from '../utils/time.js';
 import { isProduction } from '../setup/environment.js';
+import { getInstant, instantToClock } from '../utils/temporal.js';
 
 class Logger {
   private queue: Log[];
@@ -72,7 +72,7 @@ class Logger {
       level,
       origin,
       text,
-      time: millisToString(timeNow()),
+      time: millisToString(instantToClock(getInstant())),
     };
     this._push(log);
   }
