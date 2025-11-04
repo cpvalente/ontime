d362a2d26f009a63c3d0bcbc042774ffce501853
arc-alex <ac@omnivox.dk>
2025-11-04 13:30:37 +0100

    test for DST

--- apps/server/src/utils/temporal.ts
+++ apps/server/src/utils/temporal.ts
@@ -1,42 +1,40 @@
 import { Maybe } from 'ontime-types';
 import { dayInMs, MILLIS_PER_MINUTE } from 'ontime-utils';
 
-const tzOffset = new Date().getTimezoneOffset() * MILLIS_PER_MINUTE;
-
 export type Instant = number & { __brand: 'instant' };
-export type Duration = number & { __brand: 'duration' };
 
+/**
+ * get the current instant in time irrespective of timezone
+ * @returns {number}
+ */
 export const getInstant = (): Instant => Date.now() as Instant;
 
+/**
+ * convert a 24 hour clamped value into the corresponding instance
+ * @param {(number|null)} clock
+ * @returns {(number|null)}
+ */
 export function clockToInstant(clock: null): null;
 export function clockToInstant(clock: number): Instant;
 export function clockToInstant(clock: Maybe<number>): Maybe<Instant>;
 export function clockToInstant(clock: Maybe<number>): Maybe<Instant> {
   if (clock === null) return null;
   const now = getInstant();
-  const day = now - timeNow();
-  return (day + clock) as Instant;
+  const timePart = instantToClock(now);
+  const dayPart = now - timePart;
+  return (dayPart + clock) as Instant;
 }
 
+/**
+ * convert a instant into a 24 hour clamp value adjusted for timezone
+ * @param {(number|null)} instant
+ * @returns {(number|null)}
+ */
 export function instantToClock(instant: null): null;
 export function instantToClock(instant: Instant): number;
 export function instantToClock(instant: Maybe<Instant>): Maybe<number>;
 export function instantToClock(instant: Maybe<Instant>): Maybe<number> {
   if (instant === null) return null;
-  return (instant % dayInMs) - tzOffset;
-}
-
-/**
- * The old clock function
- * is here until is is no longer needed for reference
- */
-function timeNow() {
-  const now = new Date();
-
-  // extract milliseconds since midnight
-  let elapsed = now.getHours() * 3600000;
-  elapsed += now.getMinutes() * 60000;
-  elapsed += now.getSeconds() * 1000;
-  elapsed += now.getMilliseconds();
-  return elapsed;
+  const tzOffset = new Date().getTimezoneOffset() * MILLIS_PER_MINUTE;
+  return (instant - tzOffset) % dayInMs;
 }
