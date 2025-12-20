792a6f40e9b14e403dad7613cfe7c51626560526
Carlos Valente <carlosvalente@pm.me>
2025-12-20 15:07:47 +0100

    refactor: improve merge logic

--- apps/server/src/classes/data-provider/DataProvider.utils.ts
+++ apps/server/src/classes/data-provider/DataProvider.utils.ts
@@ -7,24 +7,27 @@ export function safeMerge(existing: DatabaseModel, newData: Partial<DatabaseMode
   const deepExisting = structuredClone(existing);
   const deepNewData = structuredClone(newData);
 
+  // destructure each property to simplify merging not provided ie: ...{} has no effect
   const {
     rundowns = {},
     project = {},
     settings = {},
     viewSettings = {},
-    urlPresets = deepExisting.urlPresets,
-    customFields = deepExisting.customFields,
-    automation = deepExisting.automation,
+    urlPresets = [],
+    customFields = {},
+    automation,
   } = deepNewData;
 
   return {
-    ...deepExisting,
     rundowns: { ...existing.rundowns, ...rundowns },
     project: { ...deepExisting.project, ...project },
     settings: { ...deepExisting.settings, ...settings },
     viewSettings: { ...deepExisting.viewSettings, ...viewSettings },
-    urlPresets: urlPresets ?? deepExisting.urlPresets,
-    customFields: customFields ?? deepExisting.customFields,
-    automation: { ...deepExisting.automation, ...automation },
+    // URL presets are independent and we can append them together
+    urlPresets: [...deepExisting.urlPresets, ...urlPresets],
+    // custom fields can be merged
+    customFields: { ...deepExisting.customFields, ...customFields },
+    // trigger and automation are coupled and cannot be changed individually so we replace the whole automation
+    automation: automation ?? deepExisting.automation,
   };
 }
